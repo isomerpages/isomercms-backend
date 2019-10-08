563c395a2aa7be5023973ff79db198c92cc90c04
Preston Lim <prestonlimlianjie@gmail.com>
2019-10-08 18:31:53 +0800

    Properly handle GitHub OAuth Token (#1)

---
+++ utils/jwt-utils.js
@@ -0,0 +1,18 @@
+const _ = require('lodash')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = process.env.JWT_SECRET
+const AUTH_TOKEN_EXPIRY_MS = process.env.AUTH_TOKEN_EXPIRY_DURATION_IN_MILLISECONDS.toString()
+
+const jwtUtil = {
+  verifyToken: _.wrap(
+    jwt.verify,
+    (verify, token) => verify(token, JWT_SECRET, { algorithms: ['HS256'] })
+  ),
+  signToken: _.wrap(
+    jwt.sign,
+    (sign, content) => sign(content, JWT_SECRET, { algorithm: 'HS256', expiresIn: AUTH_TOKEN_EXPIRY_MS })
+  ),
+}
+
+module.exports = jwtUtil

098881e0a7ea708bc11334dc90d3521cae8e6b49
Preston Lim <prestonlimlianjie@gmail.com>
2019-10-12 12:06:21 +0800

    add GET collections route

--- routes/collections.js
+++ routes/collections.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const jwtUtils = require('../utils/jwt-utils')
 const Bluebird = require('bluebird')
+const yaml = require('js-yaml');
+const base64 = require('base-64');
+const axios = require('axios')
+
+const GITHUB_ORG_NAME = 'isomerpages'
 
 // Import classes 
 const { File, CollectionPageType } = require('../classes/File.js')
@@ -9,7 +14,33 @@ const { File, CollectionPageType } = require('../classes/File.js')
 // List collections
 router.get('/:siteName/collections', async function(req, res, next) {
   try {
-    // TO-DO
+    const { oauthtoken } = req.cookies
+    const { access_token } = jwtUtils.verifyToken(oauthtoken)
+    const { siteName } = req.params
+
+    // validateStatus allows axios to handle a 404 HTTP status without rejecting the promise.
+    // This is necessary because GitHub returns a 404 status when the file does not exist.
+    const validateStatus = (status) => {
+      return (status >= 200 && status < 300) || status === 404
+    }
+
+    const endpoint = `https://api.github.com/repos/${GITHUB_ORG_NAME}/${siteName}/contents/_config.yml`
+    const resp = await axios.get(endpoint, {
+      validateStatus: validateStatus,
+      headers: {
+        Authorization: `token ${access_token}`,
+        "Content-Type": "application/json"
+      }
+    })
+
+    if (resp.status === 404) throw new Error ('Page does not exist')
+
+    const { content, sha } = resp.data
+    const config = yaml.safeLoad(base64.decode(content))
+    const collections = Object.keys(config.collections)
+
+    res.status(200).json({ collections })
+
   } catch (err) {
     console.log(err)
   }
@@ -48,7 +79,23 @@ router.get('/:siteName/collections/:collectionName', async function(req, res, ne
 // Delete collection
 router.delete('/:siteName/collections/:collectionName', async function(req, res, next) {
   try {
-    // TO-DO
+    // TO-DO: Verify that collection exists
+
+    // Remove collection from config file
+
+    // Get all collectionPages
+    const GitHubFile = new File(access_token, siteName)
+    const collectionPageType = new CollectionPageType(collectionName)
+    GitHubFile.setFileType(collectionPageType)
+    const collectionPages = await GitHubFile.list()
+
+    // Delete all collectionPages
+    await Bluebird.map(collectionPages, async(collectionPage) => {
+      let pageName = collectionPage.pageName
+      const { sha } = await GitHubFile.read(pageName)
+      return GitHubFile.delete(pageName, sha)
+    })
+
   } catch (err) {
     console.log(err)
   }

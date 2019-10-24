f5898de6fed2affafe0c475134c31b8c216b80bf
Taufiq Mohammed <taufiqmmhd@gmail.com>
2019-10-24 15:01:13 +0800

    feat: change error response from render to json
    An error is thrown when using `res.render()` in the error handler
    function. The error returned is `no default engine was specified and no
    extension was provided`. The solution was to use `res.json()`

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err });
 });
 
 module.exports = app;

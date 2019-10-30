535267604fa7ac4a10f3cc3f6ef9a720162b5c79
Taufiq Mohammed <taufiqmmhd@gmail.com>
2019-10-30 17:25:51 +0800

    feat: increase request body size limit to 5mb
    You can now send json payloads of upto 5mb

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const menuRouter = require('./routes/menus')
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '5mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));

b41239582762bf52493520e3586dddc710ea0899
Taufiq Mohammed <taufiqmmhd@gmail.com>
2019-11-11 14:20:51 +0800

    feat: read collection details from collections.yml
    Instead of finding the corresponding collection folder and getting the
    branching out details from there, this commit now uses the
    `_data/collections.yml` file to find the necessary collection
    information

--- routes/menuDirectory.js
+++ routes/menuDirectory.js
@@ -21,6 +21,12 @@ router.get('/:siteName/tree', async function(req, res, next) {
       const { content } = await IsomerNavFile.read('navigation.yml')
       const navItems = yaml.safeLoad(base64.decode(content)).links
 
+      // Stores content of the collections tree
+      let IsomerCollectionTreeFile = new File(access_token, siteName)
+      IsomerCollectionTreeFile.setFileType(new MenuType())
+      IsomerCollectionTreeFile.setBranchRef("cms-test")
+      const { content: collectionsContent } = await IsomerCollectionTreeFile.read('collections.yml')
+      const { collections } = yaml.safeLoad(base64.decode(collectionsContent))
       /**
        * This tokenizes the items loaded from `navigation.yml`
        * into these types:
@@ -29,103 +35,57 @@ router.get('/:siteName/tree', async function(req, res, next) {
        * `resource room` - Resource room pages
        */
 
-      let directory = navItems.map(item => {
+      let directory = navItems.map(navItem => {
         // If navigation item has a url it links to a simple page / external page
         // For now it defaults to a simple page
         // TODO know if it links to an external page
-        if (item.url) {
-          const fileName = item.title.toLowerCase().replace(" ", "-") + ".md"
+        if (navItem.url) {
+          const fileName = navItem.title.toLowerCase().replace(" ", "-") + ".md"
           return {
             type: 'page',
             name: fileName,
             path: encodeURIComponent(new PageType().getFolderName() + fileName)
           }
-        } else if (item.collection) {
-          return {
-            type: 'collection',
-            name: item.collection
-          }
-        } else if (item.resource_room) {
-          return {
-            type: 'resource room',
-            name: item.title,
-            title: item.title
-          }
-        }
-
-        return item
-      });
-
-      /**
-       * This function then loops through the directory items
-       * to find items of type `collection`, and retrieve the 
-       * relevant "Sub Collections" or "Collection Pages"
-       * `Promise.all()` was used as an async function was needed to 
-       * retrieve the relevant items and `Promse.all` resolves when all
-       * its array contents are resolved/rejected
-       */
-      directory = await Promise.all(directory.map(async item => {
-        if (item.type === 'collection') {
-          const IsomerFile = new File(access_token, siteName)
-          const collectionPageType = new CollectionPageType(item.name)
-          IsomerFile.setFileType(collectionPageType)
-          let collectionPages = await IsomerFile.list()
+        } else if (navItem.collection) {
+          // Find corresponding entry from navigation.yml and matches up with collections.yml
+          let { children } = collections.filter(({ collection }) => collection === navItem.collection)[0]
 
           /**
-           * Within the listed collection pages, we need to group them up
-           * into their respective sub collections
+           * Parses and groups up the thirdnav entries
            */
-          collectionPages = collectionPages.reduce((accumulator, page) => {
-            // Create a deep copy of the accumulated value
-            let accumulatorCopy = [...accumulator]
-            /**
-             * Files such as `2c-filename.md` will be split
-             * by the `-` and checked if it's part of a subcollection
-             * Collection pages that are split into subcollections contains a letter
-             * after their group number (i.e `c` in `2c-filename.md`)
-             * Link: https://isomer.gov.sg/documentation/navbar-and-footer/creating-3rd-level-nav/
-             */
-            const identifier = page.fileName.split("-")[0]
-            const isSubcollection = /[0-9][a-z]/.test(identifier)
+          children = children.reduce((accumulator, collectionPage) => {
+            if (collectionPage.type === "collection-page") {
+              accumulator.push(collectionPage)
+              return accumulator
+            }
 
-            /**
-             * `canCreateSubcollection` is to reflect the moment
-             * a filename is start of a new subcollection
-             * (i.e `1a-filename.md` is the start of a new subcollection
-             * while `1b-filename.md` is not)
-             */
-            const canCreateSubcollection = /[0-9]a$/.test(identifier)
-        
-            // Treat it as a normal collection page and proceed to the next item
-            if (!isSubcollection) {
-               accumulatorCopy.push({...page, type: 'leftnav', name: page.fileName}) 
-               return accumulatorCopy
+            if (accumulator.length === 0 || accumulator[accumulator.length - 1].type !== "thirdnav") {
+              accumulator.push({
+                title: collectionPage.thirdnav,
+                type: "thirdnav",
+                children: [],
+              })
             }
-        
-            // Create a subcollection object
-            if (canCreateSubcollection) {
-                accumulatorCopy.push({ type: "subcollection", children: [], name: "subcollection" })
-            }
-        
-            /**
-             * If the program gets this far, it would mean the item is
-             * meant to be part of the last subcollection being populated in `accumulatorCopy`
-             */
-            const lastSubCollectionIndex = accumulatorCopy.length - 1
-            accumulatorCopy[lastSubCollectionIndex].children.push({...page, name: page.fileName})
-            
-            return accumulatorCopy
+
+            accumulator[accumulator.length - 1].children.push(collectionPage)
+            return accumulator
           }, [])
 
-          // Return the fully branched out collection
           return {
-            type: item.type,
-            name: item.name,
-            children: collectionPages
+            type: 'collection',
+            children,
+            title: navItem.collection
+          }
+        } else if (navItem.resource_room) {
+          return {
+            type: 'resource room',
+            name: navItem.title,
+            title: navItem.title
           }
         }
-        return item
-      }))
+
+        return navItem
+      });
 
       const IsomerPageFile = new File(access_token, siteName)
       IsomerPageFile.setFileType(new PageType())

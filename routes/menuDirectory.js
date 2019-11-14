61a0a5a697736ad95132d72c183e86e73483207b
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2019-11-14 22:59:32 +0800

    Modify response to include unlinked collections and unlinked resource rooms

--- routes/menuDirectory.js
+++ routes/menuDirectory.js
@@ -8,21 +8,38 @@ const _ = require('lodash')
 const Bluebird = require('bluebird')
 
 const { PageType, File, CollectionPageType, MenuType } = require('../classes/File')
-const { deslugifyCollectionPage } = require('../utils/utils')
+const { Collection } = require('../classes/Collection')
+const { ResourceRoom } = require('../classes/ResourceRoom')
+const { collectionPageAggregator } = require('../utils/menu-utils')
+const { deslugifyCollectionName } = require('../utils/utils')
 
 // Read tree of directory
 router.get('/:siteName/tree', async function(req, res, next) {
     try {
+      // variables to keep track of whether there are simple pages or 
+      // resources in the navigation bar
+      let navHasSimplePage = false;
+      let navHasResources = false;
+      // variable to store unlinked pages
+      let unlinkedPages;
+      // variable to keep track of collections in the nav bar
+      let collections = [];
+      let unlinkedCollections = [];
+      // object to accumulate what to send back as response
+      let response = {};
+
+      // verify credentials
       const { oauthtoken } = req.cookies
       const { access_token } = jwtUtils.verifyToken(oauthtoken)
-  
       const { siteName } = req.params
   
+      // read the _data/navigation.yml file
       const IsomerNavFile = new File(access_token, siteName)
       IsomerNavFile.setFileType(new MenuType())
       const { content } = await IsomerNavFile.read('navigation.yml')
 
       const navItems = yaml.safeLoad(base64.decode(content)).links;
+
       /**
        * This tokenizes the items loaded from `navigation.yml`
        * into these types:
@@ -36,25 +53,31 @@ router.get('/:siteName/tree', async function(req, res, next) {
         // For now it defaults to a simple page
         // TODO know if it links to an external page
         if (item.url) {
+          // navigation contains simple page
+          navHasSimplePage = true;
+
           // this is the wrong way to do it, we should look at how exactly the titles are slugified
           const fileName = item.title.toLowerCase().replace(" ", "-") + ".md"
           return {
             type: 'page',
             title: item.title,
-            path: encodeURIComponent(new PageType().getFolderName() + fileName)
+            path: encodeURIComponent(new PageType().getFolderName() + fileName),
+            url: item.url,
           }
         } else if (item.collection) {
-          console.log(item)
+          // keep track of list of collections in navigation bar
+          collections.push(item.collection)
           return {
             type: 'collection',
             title: item.title,
             collection: item.collection,
           }
         } else if (item.resource_room) {
+          // navigation contains resource room
+          navHasResources = true
           return {
             type: 'resource room',
-            name: item.title,
-            title: item.title
+            title: item.title,
           }
         }
 
@@ -62,94 +85,79 @@ router.get('/:siteName/tree', async function(req, res, next) {
       });
 
       /**
-       * This function then loops through the directory items
+       * This function then loops through the directory item
        * to find items of type `collection`, and retrieve the 
        * relevant `collection-page`(s) & groups them up into
        * `thirdnav` groups when necessary
        */
       directory = await Bluebird.map(directory, async item => {
         if (item.type === 'collection') {
-          const CollectionFile = new File(access_token, siteName)
-          const collectionPageType = new CollectionPageType(item.collection)
-          CollectionFile.setFileType(collectionPageType)
-          let collectionPages = await CollectionFile.list()
-
-          /**
-           * Within the listed collection pages, we need to group them up
-           * into their respective thirdnav groups
-           */
-          collectionPages = await Bluebird.reduce(collectionPages, async (accumulator, collectionPage) => {
-            /**
-             * Files such as `2c-filename.md` will be split
-             * by the `-` and checked if it's part of a thirdnav group
-             * Collection pages that are part of a thirdnav contains a letter
-             * after their group number (i.e `c` in `2c-filename.md`)
-             * Link: https://isomer.gov.sg/documentation/navbar-and-footer/creating-3rd-level-nav/
-             */
-            const identifier = collectionPage.fileName.split('-')[0]
-            const isThirdnav = /[0-9][a-z]/.test(identifier)
-
-            /**
-             * `canCreateThirdnav` is to check if the filename indicates
-             * a need to create a new thirdnav group to store it in
-             * (i.e `1a-filename.md` is the start of a new thirdnav
-             * while `1b-filename.md` is not)
-             */
-            const canCreateThirdnav = /[0-9]a$/.test(identifier)
-        
-            // Treat it as a normal collection page and proceed to the next item
-            if (!isThirdnav) {
-              accumulator.push({
-                 path: collectionPage.path,
-                 type: 'collection-page',
-                 title: deslugifyCollectionPage(collectionPage.fileName)
-              }) 
-              return accumulator
-            }
-        
-            // Create a thirdnav object
-            if (canCreateThirdnav) {
-              // Retrieve third_nav_title from frontmatter in the thirdnav page - this is slow
-              const { content } = await CollectionFile.read(collectionPage.fileName);
-              const frontMatter = yaml.safeLoad(base64.decode(content).split('---')[1]);
-              accumulator.push({
-                title: `${frontMatter.third_nav_title}`,
-                type: "thirdnav",
-                children: []
-              })
-            }
-        
-            /**
-             * If the program gets this far, it would mean the item is a thirdnav-page and is
-             * meant to be part of the last thirdnav in `accumulator`
-             */
-            const lastSubCollectionIndex = accumulator.length - 1
-            accumulator[lastSubCollectionIndex].children.push({
-              path: collectionPage.path,
-              title: deslugifyCollectionPage(collectionPage.fileName),
-              type: 'thirdnav-page',
-            })
-            
-            return accumulator
-          }, [])
-
-          // Return the fully branched out collection
-          return {
-            type: item.type,
-            title: item.title,
-            children: collectionPages
-          }
+          return collectionPageAggregator(item, access_token, siteName)
         }
         return item
       })
 
-      const IsomerPageFile = new File(access_token, siteName)
-      IsomerPageFile.setFileType(new PageType())
-      const pages = await IsomerPageFile.list()
-      const linkedPages = directory.filter(item => item.type === 'page')
-      const unlinkedPages = _.differenceBy(pages, linkedPages, 'fileName')
+      // check whether simple pages are linked in the navigation bar
+      if (navHasSimplePage) {
+        // get all files in pages folder in the repo
+        const IsomerPageFile = new File(access_token, siteName)
+        IsomerPageFile.setFileType(new PageType())
+        const pages = await IsomerPageFile.list()
+
+        // get the list of pages which are not linked in the navigation bar
+        const linkedPages = directory.filter(item => item.type === 'page')
+        unlinkedPages = _.differenceBy(pages, linkedPages, 'fileName') // THIS IS WRONG
+      } else {
+        unlinkedPages = pages
+      }
       
-      res.status(200).json({ directory , unlinkedPages })
+      // add directory and unlinkedPages to response since they must both exist
+      Object.assign(response, {
+        directory,
+        unlinked: {
+          unlinkedPages,
+        },
+      })
+
+      // check whether resources are linked in the navigation bar
+      // if they are not linked, include resources in the unlinked section
+      if (!navHasResources) {
+        const resourceRoomName = await(new ResourceRoom(access_token, siteName)).get()
+        Object.assign(response.unlinked, {
+          resourceRoom: {
+            type: 'resource room',
+            title: resourceRoomName,
+          },
+        })
+      }
+
+
+      // get the list of collections which are not linked in the navigation bar
+      const repoCollections = await (new Collection(access_token, siteName)).list()
+      unlinkedCollections = _.differenceBy(repoCollections, collections)
+
+      // if there are any unlinked collections, generate the same data structure
+      if (unlinkedCollections.length > 0) {
+        // create an array of collection items to mimic what is received from navigation.yml
+        unlinkedCollections = unlinkedCollections.map((collection) => ({
+          type: 'collection',
+          title: deslugifyCollectionName(collection), // convert collection name into title
+          collection,
+        }))
+
+        // go through the whole process of generating a directory
+        unlinkedCollections = await Bluebird.map(unlinkedCollections, async item => (
+          collectionPageAggregator(item, access_token, siteName)
+        ))
+  
+        unlinkedCollections.forEach((collection) => {
+          Object.assign(response.unlinked, {
+            [collection.title]: collection,
+          })
+        })
+      }
+      
+      res.status(200).json(response)
     } catch (err) {
       console.log(err)
     }

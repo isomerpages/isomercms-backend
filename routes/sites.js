82d55ee6678751ea58d146c0a400de31f8636669
kwajiehao <31984694+kwajiehao@users.noreply.github.com>
2019-11-20 12:06:28 +0800

    [sites] Fix sites endpoint (#29)
    * refactor: remove step which checks for staging branch
    
    All Isomer pages should have a staging branch, so there is no need to check for this.
    
    * fix: add return statement in reduce
    
    * fix: remove "isomerpages" from repo name
    
    * fix: filter out isomer admin repos
    
    * Include last update time of repo
    
    * fix: remove isomer admin repos
    
    Since users of the CMS will not be collaborators in admin repos, we can
    comment this out for now
    
    * fix: typo in timeDiff

--- routes/sites.js
+++ routes/sites.js
@@ -6,11 +6,44 @@ const _ = require('lodash')
 const Bluebird = require('bluebird')
 
 const ISOMER_GITHUB_ORG_NAME = 'isomerpages'
+// const ISOMER_ADMIN_REPOS = [
+//   'isomercms-backend',
+//   'isomercms-frontend',
+//   'isomer-redirection',
+//   'isomerpages-template',
+//   'isomer-conversion-scripts',
+//   'isomer-wysiwyg',
+//   'isomer-slackbot',
+//   'isomer-tooling',
+//   'generate-site',
+//   'travisci-scripts',
+//   'recommender-train',
+//   'editor',
+//   'ci-test',
+//   'infra',
+//   'markdown-helper',
+// ]
 
 // validateStatus allows axios to handle a 404 HTTP status without rejecting the promise.
 // This is necessary because GitHub returns a 404 status when the file does not exist.
 const validateStatus = (status) => {
   return (status >= 200 && status < 300) || status === 404
+}
+
+// timeDiff tells us when a repo was last updated in terms of days (for e.g. 2 days ago,
+// today)
+const timeDiff = (lastUpdated) => {
+  const gapInUpdate =  new Date().getTime() - new Date(lastUpdated).getTime()
+  const numDaysAgo = Math.floor(gapInUpdate/(1000 * 60 * 60 * 24))
+  // return a message for number of days ago repo was last updated
+  switch (numDaysAgo) {
+    case 0:
+      return 'Updated today';
+    case 1: 
+      return 'Updated 1 day ago';
+    default:
+      return `Updated ${numDaysAgo} days ago` 
+  }
 }
 
 /* Returns a list of all sites (repos) that the user has access to on Isomer. */
@@ -20,36 +53,45 @@ router.get('/', async function(req, res, next) {
     const { oauthtoken } = req.cookies
     let { access_token } = jwtUtils.verifyToken(oauthtoken)
 
-    const filePath = `https://api.github.com/user/repos`
-    const resp = await axios.get(filePath, {
-      headers: {
-        Authorization: `token ${access_token}`,
-        "Content-Type": "application/json"
-      }
-    })
+    // Variable to store user repos
+    let siteNames = []
 
-    // Ensures that only sites that are Isomer and have a staging branch are shown
-    const siteNames = _.compact(await Bluebird.map(resp.data, async(site) => {
-      // The full_name is in the format of <GITHUB_ORG_NAME>/<GITHUB_REPO_NAME>
-      // isIsomerSite checks the <GITHUB_ORG_NAME> and makes sure that it matches the Isomer org name
-      const isIsomerSite = site.full_name.split('/')[0] === ISOMER_GITHUB_ORG_NAME
-      const branchEndpoint = `https://api.github.com/repos/${ISOMER_GITHUB_ORG_NAME}/${site.name}/branches/staging`
-      const branchResp = await axios.get(branchEndpoint, {
-        validateStatus: validateStatus,
+    // Variables to track pagination of user's repos in case user has more than 100
+    let pageCount = 1
+    let hasNextPage = true;
+    const filePath = `https://api.github.com/user/repos?per_page=100&page=`;
+
+    // Loop through all user repos
+    while (hasNextPage) {
+      const resp = await axios.get(filePath + pageCount, {
         headers: {
           Authorization: `token ${access_token}`,
           "Content-Type": "application/json"
         }
       })
-      const hasStagingBranch = (branchResp.status === 200)
-      
-      if (isIsomerSite && hasStagingBranch) {
-        return site.name
-      } else {
-        return undefined
-      }
-    }))
 
+      // Filter for isomer repos
+      const isomerRepos = resp.data.reduce((acc, repo) => {
+        const { updated_at, full_name } = repo
+        const fullName = full_name.split('/')
+        if (fullName[0] === ISOMER_GITHUB_ORG_NAME) {
+          return acc.concat({
+            repoName: fullName[1],
+            lastUpdated: timeDiff(updated_at),
+          })
+        }
+        return acc
+      }, [])
+
+
+      siteNames= siteNames.concat(isomerRepos)
+      hasNextPage = resp.headers.link.includes('next')
+      ++pageCount
+    }
+    
+    // Remove Isomer admin repositories from this list
+    // siteNames = _.difference(siteNames, ISOMER_ADMIN_REPOS)
+    
     res.status(200).json({ siteNames })
   } catch (err) {
     console.log(err)

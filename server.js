ce264f85c9d5e1699870a9a9edb00f6f8acce810
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-29 18:32:20 +0800

    refactor: relocate error handler to a middleware file
    This commit relocates the error handler function to its own file in the
    `middleware` folder

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cors = require('cors');
 // Env vars
 const FRONTEND_URL = process.env.FRONTEND_URL
 
+// Import error handler
+const { errorHandler } = require('./middleware/errorHandler')
+
 // Import routes
 const indexRouter = require('./routes/index')
 const authRouter = require('./routes/auth')
@@ -61,16 +64,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  console.log(`${new Date()}: ${err}`)
-
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({ error: err });
-});
+app.use(errorHandler);
 
 module.exports = app;

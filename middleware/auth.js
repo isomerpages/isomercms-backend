ccd394f518e58116cce274e757a3cfac020aab06
Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>
2020-10-29 23:51:22 +0800

    feat: adjust auth middleware function
    Some endpoints, such as the authentication endpoint with GitHub OAuth,
    do not require the user to have an access token. In such cases, we don't
    want to try to extract a cookie and access token from a user request, since
    might not have one.
    
    This commit chooses the appropriate auth function (noVerify - or on verification -
    for the login and logout endpoints, but verifyJwt for the rest of the
    endpoints). Any route that is passed in but not found in our list of endpoints
    will receive a 404 response with the message `Unauthorized for unknown route'.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,18 @@
 // Imports
-const jwt = require('jsonwebtoken')
 const express = require('express')
 const jwtUtils = require('../utils/jwt-utils')
 
 // Import errors
 const { AuthError } = require('../errors/AuthError')
+const { verify } = require('jsonwebtoken')
 
 // Instantiate router object
 const auth = express.Router()
+
+
+function noVerify (req, res, next) {
+    next('router')
+}
 
 const verifyJwt = (req, res, next) => {
     try {
@@ -23,7 +28,96 @@ const verifyJwt = (req, res, next) => {
     return next('router')
 }
 
-auth.use(verifyJwt)
+// Login and logout
+auth.get('/auth', noVerify)
+auth.get('/auth/logout', noVerify)
+
+// Index
+auth.get('/', noVerify)
+
+// Homepage
+auth.get('/sites/:siteName/homepage', verifyJwt)
+auth.post('/sites/:siteName/homepage', verifyJwt)
+
+// Collection pages
+auth.get('/sites/sites/:siteName/collections/:collectionName', verifyJwt)
+auth.post('/sites/:siteName/collections/:collectionName/pages', verifyJwt)
+auth.get('/sites/:siteName/collections/:collectionName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/collections/:collectionName/pages/:pageName', verifyJwt)
+auth.delete('/sites/:siteName/collections/:collectionName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/collections/:collectionName/pages/:pageName/rename/:newPageName', verifyJwt)
+
+// Collections
+auth.get('/sites/:siteName/collections', verifyJwt)
+auth.post('/sites/:siteName/collections', verifyJwt)
+auth.delete('/sites/:siteName/collections/:collectionName', verifyJwt)
+auth.post('/sites/:siteName/collections/:collectionName/rename/:newCollectionName', verifyJwt)
+
+// Documents
+auth.get('/sites/:siteName/documents', verifyJwt)
+auth.post('/sites/:siteName/documents', verifyJwt)
+auth.get('/sites/:siteName/documents/:documentName', verifyJwt)
+auth.post('/sites/:siteName/documents/:documentName', verifyJwt)
+auth.delete('/sites/:siteName/documents/:documentName', verifyJwt)
+auth.post('/sites/:siteName/documents/:documentName/rename/:newDocumentName', verifyJwt)
+
+// Images
+auth.get('/sites/:siteName/images', verifyJwt)
+auth.post('/sites/:siteName/images', verifyJwt)
+auth.get('/sites/:siteName/images/:imageName', verifyJwt)
+auth.post('/sites/:siteName/images/:imageName', verifyJwt)
+auth.delete('/sites/:siteName/images/:imageName', verifyJwt)
+auth.post('/sites/:siteName/images/:imageName/rename/:newImageName', verifyJwt)
+
+// Menu directory
+auth.get('/sites/:siteName/tree', verifyJwt)
+
+// Menu
+auth.get('/sites/:siteName/menus', verifyJwt)
+auth.get('/sites/:siteName/menus/:menuName', verifyJwt)
+auth.post('/sites/:siteName/menus/:menuName', verifyJwt)
+
+// Pages
+auth.get('/sites/:siteName/pages', verifyJwt)
+auth.post('/sites/:siteName/pages', verifyJwt)
+auth.get('/sites/:siteName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/pages/:pageName', verifyJwt)
+auth.delete('/sites/:siteName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/pages/:pageName/rename/:newPageName', verifyJwt)
+
+// Resource pages
+auth.get('/sites/:siteName/resources/:resourceName', verifyJwt)
+auth.post('/sites/:siteName/resources/:resourceName/pages', verifyJwt)
+auth.get('/sites/:siteName/resources/:resourceName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/resources/:resourceName/pages/:pageName', verifyJwt)
+auth.delete('/sites/:siteName/resources/:resourceName/pages/:pageName', verifyJwt)
+auth.post('/sites/:siteName/resources/:resourceName/pages/:pageName/rename/:newPageName', verifyJwt)
+
+// Resource room
+auth.get('/sites/:siteName/resource-room', verifyJwt)
+auth.post('/sites/:siteName/resource-room', verifyJwt)
+auth.post('/sites/:siteName/resource-room/:resourceRoom', verifyJwt)
+auth.delete('/sites/:siteName/resource-room', verifyJwt)
+
+// Resources
+auth.get('/sites/:siteName/resources', verifyJwt)
+auth.post('/sites/:siteName/resources', verifyJwt)
+auth.delete('/sites/:siteName/resources/:resourceName', verifyJwt)
+auth.post('/sites/:siteName/resources/:resourceName/rename/:newResourceName', verifyJwt)
+
+// Settings
+auth.get('/sites/:siteName/settings', verifyJwt)
+auth.post('/sites/:siteName/settings', verifyJwt)
+
+// Sites
+auth.get('/sites', verifyJwt)
+
+auth.use((req, res, next) => {
+    if (!req.route) {
+        return res.status(404).send('Unauthorised for unknown route')
+    }
+    return next()
+})
 
 module.exports = {
     auth,

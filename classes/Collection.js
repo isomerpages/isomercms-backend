364ce767cfb59eb6d0c8fbff0dac8db00415faa1
Alexander Lee <alexander@open.gov.sg>
2020-11-16 09:33:29 +0800

    Nit: convert indentation to spaces

--- classes/Collection.js
+++ classes/Collection.js
@@ -14,11 +14,11 @@ class Collection {
 
   async list() {
     try {
-    	const config = new Config(this.accessToken, this.siteName)
-    	const { content, sha } = await config.read()
-		const contentObject = yaml.safeLoad(base64.decode(content))
-		const collections = contentObject.collections ? Object.keys(contentObject.collections) : []
-		return collections
+      const config = new Config(this.accessToken, this.siteName)
+      const { content, sha } = await config.read()
+    const contentObject = yaml.safeLoad(base64.decode(content))
+    const collections = contentObject.collections ? Object.keys(contentObject.collections) : []
+    return collections
     } catch (err) {
       throw err
     }
@@ -26,19 +26,19 @@ class Collection {
 
   async create(collectionName) {
     try {
-    	const config = new Config(this.accessToken, this.siteName)
-    	const { content, sha } = await config.read()
-    	const contentObject = yaml.safeLoad(base64.decode(content))
+      const config = new Config(this.accessToken, this.siteName)
+      const { content, sha } = await config.read()
+      const contentObject = yaml.safeLoad(base64.decode(content))
 
-    	// TO-DO: Verify that collection doesn't already exist
+      // TO-DO: Verify that collection doesn't already exist
 
-    	contentObject.collections[`${collectionName}`] = { 
-				permalink: '/:collection/:path/:title',
-				output: true 
-			}
-    	const newContent = base64.encode(yaml.safeDump(contentObject))
+      contentObject.collections[`${collectionName}`] = { 
+        permalink: '/:collection/:path/:title',
+        output: true 
+      }
+      const newContent = base64.encode(yaml.safeDump(contentObject))
 
-    	await config.update(newContent, sha)
+      await config.update(newContent, sha)
 
     } catch (err) {
       throw err
@@ -47,30 +47,30 @@ class Collection {
 
   async delete(collectionName) {
     try {
-    	// Delete collection in config
-    	const config = new Config(this.accessToken, this.siteName)
-    	const { content, sha } = await config.read()
-    	const contentObject = yaml.safeLoad(base64.decode(content))
+      // Delete collection in config
+      const config = new Config(this.accessToken, this.siteName)
+      const { content, sha } = await config.read()
+      const contentObject = yaml.safeLoad(base64.decode(content))
 
-    	delete contentObject.collections[`${collectionName}`]
-    	const newContent = base64.encode(yaml.safeDump(contentObject))
+      delete contentObject.collections[`${collectionName}`]
+      const newContent = base64.encode(yaml.safeDump(contentObject))
 
-    	await config.update(newContent, sha)
+      await config.update(newContent, sha)
 
-	    // Get all collectionPages
-	    const IsomerFile = new File(this.accessToken, this.siteName)
-	    const collectionPageType = new CollectionPageType(collectionName)
-	    IsomerFile.setFileType(collectionPageType)
-			const collectionPages = await IsomerFile.list()
+      // Get all collectionPages
+      const IsomerFile = new File(this.accessToken, this.siteName)
+      const collectionPageType = new CollectionPageType(collectionName)
+      IsomerFile.setFileType(collectionPageType)
+      const collectionPages = await IsomerFile.list()
 
-			if (!_.isEmpty(collectionPages)) {
-				// Delete all collectionPages
-				await Bluebird.map(collectionPages, async(collectionPage) => {
-					let pageName = collectionPage.pageName
-					const { sha } = await IsomerFile.read(pageName)
-					return IsomerFile.delete(pageName, sha)
-				})
-			}
+      if (!_.isEmpty(collectionPages)) {
+        // Delete all collectionPages
+        await Bluebird.map(collectionPages, async(collectionPage) => {
+          let pageName = collectionPage.pageName
+          const { sha } = await IsomerFile.read(pageName)
+          return IsomerFile.delete(pageName, sha)
+        })
+      }
     } catch (err) {
       throw err
     }
@@ -78,42 +78,42 @@ class Collection {
 
   async rename(oldCollectionName, newCollectionName) {
     try {
-    	// Rename collection in config
-    	const config = new Config(this.accessToken, this.siteName)
-    	const { content, sha } = await config.read()
-    	const contentObject = yaml.safeLoad(base64.decode(content))
+      // Rename collection in config
+      const config = new Config(this.accessToken, this.siteName)
+      const { content, sha } = await config.read()
+      const contentObject = yaml.safeLoad(base64.decode(content))
 
-    	contentObject.collections[`${newCollectionName}`] = { 
-				permalink: '/:collection/:path/:title',
-				output: true 
-			}
-    	delete contentObject.collections[`${oldCollectionName}`]
-    	const newContent = base64.encode(yaml.safeDump(contentObject))
+      contentObject.collections[`${newCollectionName}`] = { 
+        permalink: '/:collection/:path/:title',
+        output: true 
+      }
+      delete contentObject.collections[`${oldCollectionName}`]
+      const newContent = base64.encode(yaml.safeDump(contentObject))
 
-    	await config.update(newContent, sha)
+      await config.update(newContent, sha)
 
-	    // Get all collectionPages
-	    const OldIsomerFile = new File(this.accessToken, this.siteName)
-	    const oldCollectionPageType = new CollectionPageType(oldCollectionName)
-	    OldIsomerFile.setFileType(oldCollectionPageType)
-			const collectionPages = await OldIsomerFile.list()
-			
-			// If the object is empty (there are no pages in the collection), do nothing
-			if (_.isEmpty(collectionPages)) return 
+      // Get all collectionPages
+      const OldIsomerFile = new File(this.accessToken, this.siteName)
+      const oldCollectionPageType = new CollectionPageType(oldCollectionName)
+      OldIsomerFile.setFileType(oldCollectionPageType)
+      const collectionPages = await OldIsomerFile.list()
+      
+      // If the object is empty (there are no pages in the collection), do nothing
+      if (_.isEmpty(collectionPages)) return 
 
-	    // Set up new collection File instance
-	    const NewIsomerFile = new File(this.accessToken, this.siteName)
-	    const newCollectionPageType = new CollectionPageType(newCollectionName)
-	    NewIsomerFile.setFileType(newCollectionPageType)
+      // Set up new collection File instance
+      const NewIsomerFile = new File(this.accessToken, this.siteName)
+      const newCollectionPageType = new CollectionPageType(newCollectionName)
+      NewIsomerFile.setFileType(newCollectionPageType)
 
-	    // Rename all collectionPages
-	    await Bluebird.map(collectionPages, async(collectionPage) => {
-	      let pageName = collectionPage.fileName
-				const { content, sha } = await OldIsomerFile.read(pageName)
-				await OldIsomerFile.delete(pageName, sha)
-	      return NewIsomerFile.create(pageName, content)
-			})
-			
+      // Rename all collectionPages
+      await Bluebird.map(collectionPages, async(collectionPage) => {
+        let pageName = collectionPage.fileName
+        const { content, sha } = await OldIsomerFile.read(pageName)
+        await OldIsomerFile.delete(pageName, sha)
+        return NewIsomerFile.create(pageName, content)
+      })
+      
     } catch (err) {
       throw err
     }

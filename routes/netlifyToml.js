ecc749be9bd6e7ea606b46a7d58ff560d7c66ad0
gweiying <39231249+gweiying@users.noreply.github.com>
2020-11-16 12:04:55 +0100

    Feat/retrieve csp (#70)
    * feat: add NetlifyToml class for retrieving netlify.toml file through Github API
    
    * feat: add netlifyToml route file for retrieving, decoding, and sending netlify.toml
    
    * feat: adds netlifyTomlRouter and auth

---
+++ routes/netlifyToml.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const toml = require('toml')
+
+// Import middleware
+const { attachRouteHandlerWrapper } = require('../middleware/routeHandler')
+
+// Import classes 
+const { NetlifyToml } = require('../classes/NetlifyToml')
+
+// List resources
+async function getNetlifyToml (req, res, next) {
+  const { accessToken } = req
+  const { siteName } = req.params
+
+  const netlifyTomlFile = new NetlifyToml(accessToken, siteName)
+  
+  const { content } = await netlifyTomlFile.read()
+  
+  // Convert to readable form
+  const netlifyTomlReadableContent = toml.parse(Base64.decode(content))
+  
+  // Headers is an array of objects, specifying a set of access rules for each specified path
+  // Under our current assumption, we apply the first set of access rules to all paths
+  const netlifyTomlHeaderValues = netlifyTomlReadableContent.headers[0].values
+
+  res.status(200).json({ netlifyTomlHeaderValues })
+}
+
+router.get('/:siteName/netlify-toml', attachRouteHandlerWrapper(getNetlifyToml))
+
+module.exports = router;

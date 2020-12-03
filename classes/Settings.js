530c64faed34edfaa654ba4f88525cd51e60342d
kwajiehao <31984694+kwajiehao@users.noreply.github.com>
2020-12-03 17:38:19 +0800

    Feat/add settings fields (#85)
    * feat: add shareicon and analytics fields for Settings
    
    * feat: retrieve and update agency logo in navigation.yml
    
    This commit adds additional steps for the Settings route to retrieve
    and update the navigation.yml file to update agency logo
    
    * refactor: Settings class file retrieval and update process
    
    This commit refactors the Settings class so that we can reuse a
    utility function to retrieve the necessary settings files.
    
    It also simplifies and standardizes the update process for all settings
    files involved by putting the settings objects (config, navigation, footer)
    in an array and looping over them.
    
    * feat: update homepage title together with footer title
    
    This commit resolves issue #269. If a user edits the title field,
    they will update both the footer title and the browser title so that
    the titles are consistent.
    
    This commit achieves this by updating the title attribute of the homepage
    file ONLY IF a change in the `title` field is detected.
    
    * feat: send is_government field in response
    
    This commit sends an additional is_government field when retrieving
    settings configurations. The is_government field determines whether the
    government masthead is displayed on the website.
    
    * fix: error when updating settings object
    
    * fix: retrieve settings configurations programmatically
    
    Currently, we retrive the different settings configuration from
    various files using an array. This is confusing since the settings files
    are associated with random array indices. This commit switches to use
    an object so that the operations to retrieve the files are associated
    with a key instead.
    
    * chore: replace arrays and magic indice numbers with objects
    
    Previously, we used magic numbers to reference specific index
    positions in arrays to retrieve data. This commit replaces such arrays
    with objects so that data is accessible by key instead.
    
    Co-authored-by: Jie Hao Kwa <jiehaokwa@Jies-MacBook-Pro.local>

--- classes/Settings.js
+++ classes/Settings.js
@@ -1,13 +1,71 @@
 const { Base64 } = require('js-base64')
+const _ = require('lodash')
 const yaml = require('js-yaml')
 const Bluebird = require('bluebird')
 
 // import classes
 const { Config } = require('../classes/Config.js')
-const { File, DataType } = require('../classes/File.js')
+const { File, DataType, HomepageType } = require('../classes/File.js')
 
 // Constants
 const FOOTER_PATH = 'footer.yml'
+const NAVIGATION_PATH = 'navigation.yml'
+const HOMEPAGE_INDEX_PATH = 'index.md' // Empty string
+
+const retrieveSettingsFiles = async (accessToken, siteName, shouldRetrieveHomepage) => {
+  const configResp = new Config(accessToken, siteName)
+
+  const FooterFile = new File(accessToken, siteName)
+  const dataType = new DataType()
+  FooterFile.setFileType(dataType)
+
+  const NavigationFile = new File(accessToken, siteName)
+  NavigationFile.setFileType(dataType)
+
+  const HomepageFile = new File(accessToken, siteName)
+  const homepageType = new HomepageType()
+  HomepageFile.setFileType(homepageType)
+
+  const fileRetrievalObj = {
+    config: configResp.read(),
+    footer: FooterFile.read(FOOTER_PATH),
+    navigation: NavigationFile.read(NAVIGATION_PATH),
+  }
+
+  // Retrieve homepage only if flag is set to true
+  if (shouldRetrieveHomepage) {
+    fileRetrievalObj.homepage = HomepageFile.read(HOMEPAGE_INDEX_PATH);
+  }
+
+
+  const fileContentsArr = await Bluebird.map(Object.keys(fileRetrievalObj), async (fileOpKey) => {
+    const { content, sha } = await fileRetrievalObj[fileOpKey]
+
+    // homepage requires special extraction as the content is wrapped in front matter
+    if (fileOpKey === 'homepage') {
+      const homepageContent = Base64.decode(content)
+      const homepageFrontMatterObj = yaml.safeLoad(homepageContent.split('---')[1])
+      return { type: fileOpKey, content: homepageFrontMatterObj, sha }
+    }
+
+    return { type: fileOpKey, content: yaml.safeLoad(Base64.decode(content)), sha }
+  })
+
+  // Convert to an object so that data is accessible by key
+  const fileContentsObj = {}
+  fileContentsArr.forEach((fileObj) => {
+    const { type, content, sha } = fileObj
+    fileContentsObj[type] = { content, sha }
+  })
+
+  return {
+    configResp,
+    FooterFile,
+    NavigationFile,
+    HomepageFile,
+    fileContentsObj,
+  }
+}
 
 class Settings {
   constructor(accessToken, siteName) {
@@ -16,66 +74,121 @@ class Settings {
   }
 
   async get() {
-    // retrieve _config.yml and footer.yml
-    const configResp = new Config(this.accessToken, this.siteName)
-
-    const IsomerDataFile = new File(this.accessToken, this.siteName)
-    const dataType = new DataType()
-    IsomerDataFile.setFileType(dataType)
-
-    const fileRetrievalArr = [configResp.read(), IsomerDataFile.read(FOOTER_PATH)]
-
-    const fileContentsArr = await Bluebird.map(fileRetrievalArr, async (fileOp) => {
-      const { content, sha } = await fileOp
-      return { content, sha}
-    })
+    const { fileContentsObj: {
+      config,
+      footer,
+      navigation,
+    } } = await retrieveSettingsFiles(this.accessToken, this.siteName)
 
     // convert data to object form
-    const configContent = fileContentsArr[0].content
-    const footerContent = fileContentsArr[1].content
-
-    const configReadableContent = yaml.safeLoad(Base64.decode(configContent));
-    const footerReadableContent = yaml.safeLoad(Base64.decode(footerContent));
+    const configContent = config.content;
+    const footerContent = footer.content;
+    const navigationContent = navigation.content;
 
     // retrieve only the relevant config and index fields
     const configFieldsRequired = {
-      url: configReadableContent.url,
-      title: configReadableContent.title,
-      favicon: configReadableContent.favicon,
-      resources_name: configReadableContent.resources_name,
-      colors: configReadableContent.colors,
+      url: configContent.url,
+      title: configContent.title,
+      favicon: configContent.favicon,
+      shareicon: configContent.shareicon,
+      is_government: configContent.is_government,
+      facebook_pixel: configContent['facebook-pixel'],
+      google_analytics: configContent.google_analytics,
+      resources_name: configContent.resources_name,
+      colors: configContent.colors,
     }
 
     // retrieve footer sha since we are sending the footer object wholesale
-    const footerSha = fileContentsArr[1].sha
+    const footerSha = footer.sha
 
-    return ({ configFieldsRequired, footerContent: footerReadableContent, footerSha })
+    return ({
+      configFieldsRequired,
+      footerContent,
+      navigationContent: { logo: navigationContent.logo },
+      footerSha,
+    })
   }
   
   async post(payload) {
-    // setup 
-    const configResp = new Config(this.accessToken, this.siteName)
-    const config = await configResp.read()
-    const IsomerDataFile = new File(this.accessToken, this.siteName)
-    const dataType = new DataType()
-    IsomerDataFile.setFileType(dataType)
+    const {
+      configResp,
+      FooterFile,
+      NavigationFile,
+      HomepageFile,
+      fileContentsObj: {
+        config,
+        footer,
+        navigation,
+        homepage,
+      },
+    } = await retrieveSettingsFiles(this.accessToken, this.siteName, true)
 
     // extract data
     const {
       footerSettings,
       configSettings,
-      footerSha,
+      navigationSettings,
     } = payload
 
-    // update config object
-    const configContent = yaml.safeLoad(Base64.decode(config.content));
-    Object.keys(configSettings).forEach((setting) => (configContent[setting] = configSettings[setting]));
+    // update settings objects
+    const configContent = config.content
+    const footerContent = footer.content
+    const navigationContent = navigation.content
+
+    const settingsObj = {
+      config: {
+        payload: configSettings,
+        currentData: configContent,
+      },
+      footer: {
+        payload: footerSettings,
+        currentData: footerContent,
+      },
+      navigation: {
+        payload: navigationSettings,
+        currentData: navigationContent,
+      },
+    }
+
+    const updatedSettingsObjArr = Object.keys(settingsObj).map((settingsObjKey) => {
+      const { payload, currentData } = settingsObj[settingsObjKey]
+      const clonedSettingsObj = _.cloneDeep(currentData);
+      Object.keys(payload).forEach((setting) => (clonedSettingsObj[setting] = payload[setting]));
+      return {
+        type: settingsObjKey,
+        settingsObj: clonedSettingsObj,
+      }
+    })
+
+    const updatedSettingsObj = {}
+    updatedSettingsObjArr.forEach((setting) => {
+      const { type, settingsObj } = setting
+      updatedSettingsObj[`${type}SettingsObj`] = settingsObj
+    })
+
+    const { configSettingsObj, footerSettingsObj, navigationSettingsObj } = updatedSettingsObj
 
     // update files
-    const newConfigContent = Base64.encode(yaml.safeDump(configContent))
-    const newFooterContent = Base64.encode(yaml.safeDump(footerSettings))
+    const newConfigContent = Base64.encode(yaml.safeDump(configSettingsObj))
+    const newFooterContent = Base64.encode(yaml.safeDump(footerSettingsObj))
+    const newNavigationContent = Base64.encode(yaml.safeDump(navigationSettingsObj))
+
+    // To-do: use Git Tree to speed up operations
     await configResp.update(newConfigContent, config.sha)
-    await IsomerDataFile.update(FOOTER_PATH, newFooterContent, footerSha)
+    await FooterFile.update(FOOTER_PATH, newFooterContent, footer.sha)
+    await NavigationFile.update(NAVIGATION_PATH, newNavigationContent, navigation.sha)
+
+    // Update title in homepage as well if it's changed
+    if (configContent.title !== updatedSettingsObjArr[0].title) {
+      const { content: homepageContentObj, sha } = homepage;
+      homepageContentObj.title = configSettings.title;
+      const homepageFrontMatter = yaml.safeDump(homepageContentObj);
+
+      const homepageContent = ['---\n', homepageFrontMatter, '---'].join('') ;
+      const newHomepageContent = Base64.encode(homepageContent)
+
+      await HomepageFile.update(HOMEPAGE_INDEX_PATH, newHomepageContent, sha)
+    }
     return
   }
 }

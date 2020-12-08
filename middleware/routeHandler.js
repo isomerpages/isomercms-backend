3cf4627fb6ff824d2c0d756b26724872bc5f8018
Alexander Lee <alexander@open.gov.sg>
2020-12-08 17:22:16 +0800

    Feat: add route handler which allows for rollback

--- middleware/routeHandler.js
+++ middleware/routeHandler.js
@@ -1,10 +1,28 @@
+const { getCommitAndTreeSha, revertCommit } = require('../utils/utils.js')
+
 const attachRouteHandlerWrapper = (routeHandler) => async (req, res, next) => {
-    routeHandler(req, res).catch((err) => {
-      next(err)
-    })
+  routeHandler(req, res).catch((err) => {
+    next(err)
+  })
+}
+
+const attachRollbackRouteHandlerWrapper = (routeHandler) => async (req, res, next) => {
+  const { accessToken } = req
+  const { siteName } = req.params
+  let originalCommitSha
+  try {
+    const { currentCommitSha } = await getCommitAndTreeSha(siteName, accessToken)
+    originalCommitSha = currentCommitSha
+  } catch (err) {
+    next(err)
   }
+  routeHandler(req, res).catch((err) => {
+    revertCommit(originalCommitSha, siteName, accessToken)
+    next(err)
+  })
+}
   
-  module.exports = {
-    attachRouteHandlerWrapper,
-  }
-  
+module.exports = {
+  attachRouteHandlerWrapper,
+  attachRollbackRouteHandlerWrapper,
+}

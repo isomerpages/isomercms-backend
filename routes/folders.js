ff0a815a2e1ca30c45f170c9107d5e04e6fc4c8b
jiehao <jiehao@open.gov.sg>
2021-02-16 21:42:33 +0800

    feat: add generalized endpoint for retrieving files within a folder or subfolder
    This commit adds the GET /v1/sites/:siteName/folders endpoint which
    retrieves the files and directories within the provided path directory.
    This endpoint will fail if the provided path is not a valid directory (for
    example, if you give it the path for an individual file, or if the provided
    path does not exist in the repo) and will respond with the appropriate error.
    
    The path is passed as a route query param `path` since the file/directory
    path contains the `/` character, which prevents it from being used as a
    route param.

---
+++ routes/folders.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+// Import middleware
+const { attachRouteHandlerWrapper } = require('../middleware/routeHandler')
+
+// Import classes 
+const { File, CollectionPageType } = require('../classes/File.js');
+
+// Import error
+const { BadRequestError } = require('../errors/BadRequestError')
+const { NotFoundError } = require('../errors/NotFoundError')
+
+// List pages and directories in folder
+async function listFolderContent (req, res, next) {
+    const { accessToken } = req
+    const { siteName } = req.params
+    const { path } = req.query // paths begin with _
+
+    // Validation of path
+    const pathArr = path.split('/')
+    if (path.slice(0,1) !== '_' || pathArr.length > 2 || (pathArr.length === 2 && pathArr[1].includes('.'))) {
+        throw new BadRequestError(`The provided path ${path} is not a valid directory!`)
+    }
+
+    const IsomerFile = new File(accessToken, siteName)
+    const folderPageType = new CollectionPageType(path.slice(1))
+    IsomerFile.setFileType(folderPageType)
+    const folderPages = await IsomerFile.listAll()
+
+    if (JSON.stringify(folderPages) == '{}') throw new NotFoundError(`Path ${path} was invalid!`)
+    res.status(200).json({ folderPages })
+}
+
+router.get('/:siteName/folders', attachRouteHandlerWrapper(listFolderContent))
+
+module.exports = router;

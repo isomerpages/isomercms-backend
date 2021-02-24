97a55536bfb858f6b652458f50927cc0e20ca1e8
Alexander Lee <alexleegs@gmail.com>
2021-02-24 12:07:11 +0800

    Feat: add endpoints to retrieve navigation data (#112)

---
+++ routes/navigation.js
@@ -0,0 +1,47 @@
+const { Base64 } = require('js-base64')
+const express = require('express');
+const yaml = require('js-yaml')
+const router = express.Router();
+
+// Import middleware
+const { attachRouteHandlerWrapper, attachRollbackRouteHandlerWrapper } = require('../middleware/routeHandler')
+
+// Import Classes
+const { File, DataType } = require('../classes/File.js')
+
+const NAVIGATION_PATH = 'navigation.yml'
+
+async function getNavigation(req, res, next) {
+  const { accessToken } = req
+
+  const { siteName } = req.params
+
+  const IsomerFile = new File(accessToken, siteName)
+  const dataType =  new DataType()
+  IsomerFile.setFileType(dataType)
+  const { content, sha } = await IsomerFile.read(NAVIGATION_PATH)
+
+  res.status(200).json({
+    sha: sha,
+    content: yaml.safeLoad(Base64.decode(content))
+  })
+}
+
+async function updateNavigation(req, res, next) {
+  const { accessToken } = req
+  const { siteName } = req.params
+
+  const { content, sha } = req.body
+
+  const IsomerFile = new File(accessToken, siteName)
+  const dataType =  new DataType()
+  IsomerFile.setFileType(dataType)
+  await IsomerFile.update(NAVIGATION_PATH, Base64.encode(yaml.safeDump(content)), sha)
+
+  res.status(200).send('OK')
+}
+
+router.get('/:siteName/navigation', attachRouteHandlerWrapper(getNavigation))
+router.post('/:siteName/navigation', attachRouteHandlerWrapper(updateNavigation))
+
+module.exports = router;

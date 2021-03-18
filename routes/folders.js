b0b12a7a70f9700f5da1cce86fdeebc591f82443
jiehao <jiehao@open.gov.sg>
2021-03-18 16:07:47 +0800

    feat: add new endpoint for renaming subfolders
    This commit adds a new endpoint, POST /:siteName/folders/:folderName/subfolder/:subfolderName/rename/:newSubfolderName,
    which renames a subfolder by rewriting the Git tree for the repo, as well
    as updating the collection config file for that subfolder's parent folder.

--- routes/folders.js
+++ routes/folders.js
@@ -61,7 +61,50 @@ async function deleteSubfolder (req, res, next) {
     res.status(200).send('Ok')
 }
 
+// Rename subfolder
+async function renameSubfolder (req, res, next) {
+    const { accessToken, currentCommitSha, treeSha } = req
+    const { siteName, folderName, subfolderName, newSubfolderName } = req.params
+
+    // Rename subfolder
+    const commitMessage = `Rename subfolder ${folderName}/${subfolderName} to ${folderName}/${newSubfolderName}`
+    const isRecursive = true
+    const gitTree = await getTree(siteName, accessToken, treeSha, isRecursive)
+    const baseTreeWithoutFolder = gitTree.filter(item => (
+        // keep all root-level items except for tree object of folder whose subfolder is to be deleted
+       !item.path.includes('/') && item.path !== `_${folderName}`
+    ))
+    const folderTreeWithRenamedSubfolder= gitTree.filter(item => (
+        // get all folder items
+        item.path.includes(`_${folderName}`)
+    )).filter(item => (
+        // remove tree objects of folder and subfolder to be renamed
+        item.path !== `_${folderName}` && item.path !== `_${folderName}/${subfolderName}`
+    )).map(item => {
+        // rename all subfolder items
+        if (item.path.includes(`_${folderName}/${subfolderName}`)) {
+            const pathArr = item.path.split('/')
+            return {
+                ...item,
+                path: `_${folderName}/${newSubfolderName}/${pathArr[2]}`,
+            }
+        }
+        return item
+    })
+
+
+    const newGitTree = [...baseTreeWithoutFolder, ...folderTreeWithRenamedSubfolder]
+    await sendTree(newGitTree, currentCommitSha, siteName, accessToken, commitMessage)
+
+    // // Update collection config
+    const collectionConfig = new CollectionConfig(accessToken, siteName, folderName)
+    await collectionConfig.renameSubfolderInOrder(subfolderName, newSubfolderName)
+
+    res.status(200).send('Ok')
+}
+
 router.get('/:siteName/folders/all', attachReadRouteHandlerWrapper(listAllFolderContent))
 router.delete('/:siteName/folders/:folderName/subfolder/:subfolderName', attachRollbackRouteHandlerWrapper(deleteSubfolder))
+router.post('/:siteName/folders/:folderName/subfolder/:subfolderName/rename/:newSubfolderName', attachRollbackRouteHandlerWrapper(renameSubfolder))
 
 module.exports = router;

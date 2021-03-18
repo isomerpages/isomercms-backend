abaf63d82e50a77c69e8849525f8e506971bee8a
jiehao <jiehao@open.gov.sg>
2021-03-18 15:06:54 +0800

    feat: endpoint for deleting subfolders
    This commit adds a new DELETE '/:siteName/folders/:folderName/subfolder/:subfolderName'
    endpoint which, when given a folder name and subfolder name, will delete
    that entire subfolder from the repo.

--- routes/folders.js
+++ routes/folders.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Bluebird = require('bluebird')
 
+const { getTree, sendTree } = require('../utils/utils.js')
+
 // Import middleware
-const { attachReadRouteHandlerWrapper } = require('../middleware/routeHandler')
+const { attachReadRouteHandlerWrapper, attachRollbackRouteHandlerWrapper } = require('../middleware/routeHandler')
 
 // Import classes
 const { CollectionConfig } = require('../classes/Config')
@@ -28,6 +30,32 @@ async function listAllFolderContent (req, res, next) {
     res.status(200).json({ allFolderContent })
 }
 
+// Delete subfolder
+async function deleteSubfolder (req, res, next) {
+    const { accessToken, currentCommitSha, treeSha } = req
+    const { siteName, folderName, subfolderName } = req.params
+
+    const commitMessage = `Delete subfolder ${folderName}/${subfolderName}`
+    const isRecursive = true
+    const gitTree = await getTree(siteName, accessToken, treeSha, isRecursive)
+    const baseTreeWithoutFolder = gitTree.filter(item => (
+        // keep all root-level items except for tree object of folder whose subfolder is to be deleted
+       !item.path.includes('/') && item.path !== `_${folderName}`
+    ))
+    const folderTreeWithoutSubfolder = gitTree.filter(item => (
+        // get all folder items, except for the tree object of the folder itself (note the trailing /)
+        item.path.includes(`_${folderName}/`)
+    )).filter(item => (
+        // exclude all subfolder items, including the tree object of the subfolder
+        !item.path.includes(`_${folderName}/${subfolderName}`)
+    ))
+
+    const newGitTree = [...baseTreeWithoutFolder, ...folderTreeWithoutSubfolder]
+    await sendTree(newGitTree, currentCommitSha, siteName, accessToken, commitMessage)
+    res.status(200).send('Ok')
+}
+
 router.get('/:siteName/folders/all', attachReadRouteHandlerWrapper(listAllFolderContent))
+router.delete('/:siteName/folders/:folderName/subfolder/:subfolderName', attachRollbackRouteHandlerWrapper(deleteSubfolder))
 
 module.exports = router;

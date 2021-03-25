c1864d8001756504c459a68384bf0c71f17ad6c3
Alexander Lee <alexleegs@gmail.com>
2021-03-25 13:18:01 +0800

    Feat: create placeholder for empty subfolders (#137)
    * Feat: remove automatic deletion of folders and resource pages
    
    This commit removes the check and automatic deletion of folders and resource categories. Since we now want to support the existence of empty folders, it would be inconsistent behaviour if folders/resource categories were deleted when their last item was.
    
    * Feat: add new Subfolder class
    
    * Fix: creation of placeholder files when creating new subfolder through moving files
    
    * Feat: implement check for existence of resource category when listing resource pages
    
    * Fix: await subfolder creation
    
    * Fix: await subfolder creation in pages

---
+++ classes/Subfolder.js
@@ -0,0 +1,46 @@
+const { CollectionConfig } = require('./Config.js')
+const { File, CollectionPageType } = require('./File.js')
+const { Directory, FolderType } = require('./Directory.js')
+
+class Subfolder {
+  constructor(accessToken, siteName, collectionName) {
+    this.accessToken = accessToken
+    this.siteName = siteName
+    this.collectionName = collectionName
+  }
+
+  async list() {
+    const IsomerDirectory = new Directory(this.accessToken, this.siteName)
+    const folderType = new FolderType(`_${this.collectionName}`)
+    IsomerDirectory.setDirType(folderType)
+    const repoRootContent = await IsomerDirectory.list()
+
+    const allSubfolders = repoRootContent.reduce((acc, curr) => {
+        if (curr.type === 'dir') {
+          const pathTokens = curr.path.split('/')
+          acc.push(pathTokens.slice(1).join('/'))
+        }
+        return acc
+    }, [])
+    return allSubfolders
+  }
+
+  async create(subfolderName) {
+    try {
+      // Update collection.yml
+      const collectionConfig = new CollectionConfig(this.accessToken, this.siteName, this.collectionName)
+      await collectionConfig.addItemToOrder(`${subfolderName}/.keep`)
+
+      // Create placeholder file
+      const IsomerFile = new File(this.accessToken, this.siteName)
+      const dataType = new CollectionPageType(this.collectionName)
+      IsomerFile.setFileType(dataType)
+      await IsomerFile.create(`${subfolderName}/.keep`, '')
+
+    } catch (err) {
+      throw err
+    }
+  }
+}
+
+module.exports = { Subfolder }

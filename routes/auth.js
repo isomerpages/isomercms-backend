61ba6abbba7f45ccbc70fb453ce5660d0d1ee00c
Preston Lim <prestonlimlianjie@gmail.com>
2021-04-23 17:28:55 +0800

    chore: redirect to /sites on frontend after login

--- routes/auth.js
+++ routes/auth.js
@@ -66,7 +66,7 @@ async function githubAuth (req, res, next) {
 
   res.cookie(COOKIE_NAME, token, cookieSettings)
 
-  res.redirect(`${FRONTEND_URL}/auth#isomercms-${userId}`)
+  res.redirect(`${FRONTEND_URL}/sites`)
 }
 
 async function logout(req, res) {

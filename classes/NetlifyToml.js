79e43a62e19a338719843fa3c3e3a1c15690681d
Alexander Lee <alexander@open.gov.sg>
2021-06-07 16:43:17 +0800

    Lint: fix classes

--- classes/NetlifyToml.js
+++ classes/NetlifyToml.js
@@ -14,25 +14,21 @@ class NetlifyToml {
   }
 
   async read() {
-    try {
-      const endpoint = `https://api.github.com/repos/${GITHUB_BUILD_ORG_NAME}/${GITHUB_BUILD_REPO_NAME}/contents/netlify.toml`
+    const endpoint = `https://api.github.com/repos/${GITHUB_BUILD_ORG_NAME}/${GITHUB_BUILD_REPO_NAME}/contents/netlify.toml`
 
-      const resp = await axios.get(endpoint, {
-        validateStatus,
-        headers: {
-          Authorization: `token ${this.accessToken}`,
-          "Content-Type": "application/json",
-        },
-      })
+    const resp = await axios.get(endpoint, {
+      validateStatus,
+      headers: {
+        Authorization: `token ${this.accessToken}`,
+        "Content-Type": "application/json",
+      },
+    })
 
-      if (resp.status === 404)
-        throw new NotFoundError("netlify.toml file does not exist")
+    if (resp.status === 404)
+      throw new NotFoundError("netlify.toml file does not exist")
 
-      const { content, sha } = resp.data
-      return { content, sha }
-    } catch (err) {
-      throw err
-    }
+    const { content, sha } = resp.data
+    return { content, sha }
   }
 }
 

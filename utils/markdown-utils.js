071e5e7fffd1b5cecae2d6066754156207b1e499
Alexander Lee <alexander@open.gov.sg>
2021-06-21 13:03:11 +0800

    Feat: add markdown util

---
+++ utils/markdown-utils.js
@@ -0,0 +1,19 @@
+const yaml = require("yaml")
+
+const retrieveDataFromMarkdown = (fileContent) => {
+  // eslint-disable-next-line no-unused-vars
+  const [unused, encodedFrontMatter, pageContent] = fileContent.split("---")
+  const frontMatter = yaml.parse(encodedFrontMatter)
+  return { frontMatter, pageContent }
+}
+
+const convertDataToMarkdown = (frontMatter, pageContent) => {
+  const newFrontMatter = yaml.stringify(frontMatter)
+  const newContent = ["---\n", newFrontMatter, "---", pageContent].join("")
+  return newContent
+}
+
+module.exports = {
+  retrieveDataFromMarkdown,
+  convertDataToMarkdown,
+}

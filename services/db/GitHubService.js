2cad34514704cfc3b8aa683a40070d84abf22cdd
Alexander Lee <alexander@open.gov.sg>
2021-06-21 13:00:40 +0800

    Feat: set up base github and directory services

---
+++ services/db/GitHubService.js
@@ -0,0 +1,268 @@
+const axios = require("axios")
+
+const validateStatus = require("@utils/axios-utils")
+
+const BRANCH_REF = "staging"
+const { GITHUB_ORG_NAME } = process.env
+
+const {
+  ConflictError,
+  inputNameConflictErrorMsg,
+} = require("@errors/ConflictError")
+const { NotFoundError } = require("@errors/NotFoundError")
+
+const axiosInstance = axios.create({
+  baseURL: `https://api.github.com/repos/${GITHUB_ORG_NAME}/`,
+})
+
+axiosInstance.interceptors.request.use((config) => ({
+  ...config,
+  headers: {
+    ...config.headers,
+    "Content-Type": "application/json",
+  },
+}))
+
+const getFilePath = ({ siteName, fileName, dir }) =>
+  `${siteName}/contents/${dir ? `${dir}/${fileName}` : fileName}`
+
+const getFolderPath = ({ siteName, dir }) => `${siteName}/contents/${dir}`
+
+const Create = async (
+  { accessToken, siteName },
+  { content, fileName, dir }
+) => {
+  try {
+    const endpoint = getFilePath({ siteName, fileName, dir })
+    const encodedContent = Base64.encode(content)
+
+    const params = {
+      message: `Create file: ${fileName}`,
+      content: encodedContent,
+      branch: BRANCH_REF,
+    }
+
+    const resp = await axiosInstance.put(endpoint, params, {
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    })
+
+    return { sha: resp.data.content.sha }
+  } catch (err) {
+    const { status } = err.response
+    if (status === 422 || status === 409)
+      throw new ConflictError(inputNameConflictErrorMsg(fileName))
+    throw err.response
+  }
+}
+
+const Read = async ({ accessToken, siteName }, { fileName, dir }) => {
+  const endpoint = getFilePath({ siteName, fileName, dir })
+  console.log(endpoint)
+
+  const params = {
+    ref: BRANCH_REF,
+  }
+
+  const resp = await axiosInstance.get(endpoint, {
+    validateStatus,
+    params,
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+  })
+  if (resp.status === 404) throw new NotFoundError("File does not exist")
+
+  const { content: encodedContent, sha } = resp.data
+  const content = Base64.decode(encodedContent)
+
+  return { content, sha }
+}
+
+const ReadDirectory = async ({ accessToken, siteName }, { dir }) => {
+  const endpoint = getFolderPath({ siteName, dir })
+
+  const params = {
+    ref: BRANCH_REF,
+  }
+
+  const resp = await axiosInstance.get(endpoint, {
+    validateStatus,
+    params,
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+  })
+  if (resp.status === 404) throw new NotFoundError("File does not exist")
+
+  return resp.data
+}
+
+const Update = async (
+  { accessToken, siteName },
+  { fileContent, sha, fileName, dir }
+) => {
+  try {
+    const endpoint = getFilePath({ siteName, fileName, dir })
+    const encodedNewContent = Base64.encode(fileContent)
+
+    let fileSha = sha
+    if (!sha) {
+      const { sha: retrievedSha } = await Read({ accessToken, fileName, dir })
+      fileSha = retrievedSha
+    }
+
+    const params = {
+      message: `Update file: ${fileName}`,
+      content: encodedNewContent,
+      branch: BRANCH_REF,
+      sha: fileSha,
+    }
+
+    const resp = await axiosInstance.put(endpoint, params, {
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    })
+
+    return { newSha: resp.data.commit.sha }
+  } catch (err) {
+    const { status } = err.response
+    if (status === 404) throw new NotFoundError("File does not exist")
+    throw err
+  }
+}
+
+const Delete = async ({ accessToken, siteName }, { sha, fileName, dir }) => {
+  try {
+    const endpoint = getFilePath({ siteName, fileName, dir })
+
+    let fileSha = sha
+    if (!sha) {
+      const { sha: retrievedSha } = await Read({ accessToken, fileName, dir })
+      fileSha = retrievedSha
+    }
+
+    const params = {
+      message: `Delete file: ${fileName}`,
+      branch: BRANCH_REF,
+      sha: fileSha,
+    }
+
+    console.log(params)
+    console.log(endpoint)
+
+    await axiosInstance.delete(endpoint, {
+      params,
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+    })
+  } catch (err) {
+    const { status } = err.response
+    if (status === 404) throw new NotFoundError("File does not exist")
+    throw err
+  }
+}
+
+const GetRepoState = async ({ accessToken, siteName }) => {
+  const endpoint = `${siteName}/commits`
+  const headers = {
+    Authorization: `token ${accessToken}`,
+  }
+  const params = {
+    ref: BRANCH_REF,
+  }
+  // Get the commits of the repo
+  const { data: commits } = await axios.get(endpoint, {
+    params,
+    headers,
+  })
+  // Get the tree sha of the latest commit
+  const {
+    commit: {
+      tree: { sha: treeSha },
+    },
+  } = commits[0]
+  const currentCommitSha = commits[0].sha
+
+  return { treeSha, currentCommitSha }
+}
+
+const GetTree = async ({ accessToken, siteName, treeSha }, { isRecursive }) => {
+  const url = `${siteName}/git/trees/${treeSha}`
+
+  const params = {
+    ref: BRANCH_REF,
+  }
+
+  if (isRecursive) params.recursive = true
+
+  const {
+    data: { tree: gitTree },
+  } = await axiosInstance.get(url, {
+    params,
+    headers: { Authorization: `token ${accessToken}` },
+  })
+
+  return gitTree
+}
+
+const UpdateTree = async (
+  { accessToken, currentCommitSha, siteName },
+  { gitTree, message }
+) => {
+  const url = `${siteName}/git/trees`
+
+  const headers = {
+    Authorization: `token ${accessToken}`,
+  }
+
+  const resp = await axiosInstance.post(url, { tree: gitTree }, { headers })
+
+  const {
+    data: { sha: newTreeSha },
+  } = resp
+
+  const commitEndpoint = `${siteName}/git/commits`
+
+  const newCommitResp = await axiosInstance.post(
+    commitEndpoint,
+    {
+      message: message || `isomerCMS updated ${siteName} state`,
+      tree: newTreeSha,
+      parents: [currentCommitSha],
+    },
+    { headers }
+  )
+
+  const newCommitSha = newCommitResp.data.sha
+
+  return newCommitSha
+}
+
+const UpdateRepoState = async ({ accessToken, siteName }, { commitSha }) => {
+  const refEndpoint = `${siteName}/git/refs/heads/${BRANCH_REF}`
+  const headers = {
+    Authorization: `token ${accessToken}`,
+  }
+
+  await axiosInstance.patch(
+    refEndpoint,
+    { sha: commitSha, force: true },
+    { headers }
+  )
+}
+
+module.exports = {
+  Create,
+  Read,
+  ReadDirectory,
+  Update,
+  Delete,
+  GetRepoState,
+  GetTree,
+  UpdateTree,
+  UpdateRepoState,
+}
---
+++ services/directoryServices/BaseDirectoryService.js
@@ -0,0 +1,76 @@
+const _ = require("lodash")
+
+const GitHubService = require("../db/GitHubService")
+
+// Job is to deal with directory level operations to and from GitHub
+
+const List = async (reqDetails, { directoryName }) => {
+  const directoryData = await GitHubService.ReadDirectory(reqDetails, {
+    dir: directoryName,
+  })
+
+  const filesOrDirs = directoryData.map((fileOrDir) => {
+    const { name, path, sha, size, content, type } = fileOrDir
+    return {
+      name,
+      path,
+      sha,
+      size,
+      content,
+      type,
+    }
+  })
+
+  return _.compact(filesOrDirs)
+}
+
+const Rename = async (
+  reqDetails,
+  { oldDirectoryName, newDirectoryName, message }
+) => {
+  const gitTree = await GitHubService.GetTree(reqDetails, { isRecursive: true })
+
+  const newGitTree = []
+
+  gitTree.forEach((item) => {
+    if (item.path === oldDirectoryName) {
+      newGitTree.push({
+        ...item,
+        path: newDirectoryName,
+      })
+    } else if (
+      item.type !== "tree" &&
+      !item.path.includes(`${oldDirectoryName}/`)
+    ) {
+      // We don't include any other trees - we reconstruct them by adding all their individual files instead
+      // Files which are children of the renamed tree are covered by adding the renamed tree
+      newGitTree.push(item)
+    }
+  })
+
+  const newCommitSha = await GitHubService.UpdateTree(reqDetails, {
+    gitTree: newGitTree,
+    message,
+  })
+  await GitHubService.UpdateRepoState(reqDetails, { commitSha: newCommitSha })
+}
+
+const Delete = async (reqDetails, { directoryName, message }) => {
+  const gitTree = await GitHubService.GetTree(reqDetails, { isRecursive: true })
+
+  const newGitTree = gitTree.filter(
+    (item) => !(item.path.includes(`${directoryName}/`) || item.type !== "tree")
+  )
+
+  const newCommitSha = await GitHubService.UpdateTree(reqDetails, {
+    gitTree: newGitTree,
+    message,
+  })
+  await GitHubService.UpdateRepoState(reqDetails, { commitSha: newCommitSha })
+}
+
+module.exports = {
+  List,
+  Rename,
+  Delete,
+}

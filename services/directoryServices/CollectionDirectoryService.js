5592feb3b13c5c4dfc0f5a68dc8b6b8464b2b15b
Alexander Lee <alexander@open.gov.sg>
2021-06-21 13:03:30 +0800

    Feat: add collection directory service

---
+++ services/directoryServices/CollectionDirectoryService.js
@@ -0,0 +1,87 @@
+const {
+  ConflictError,
+  protectedFolderConflictErrorMsg,
+} = require("@errors/ConflictError")
+
+const CollectionYmlService = require("../fileServices/YmlFileServices/CollectionYmlService")
+const NavYmlService = require("../fileServices/YmlFileServices/NavYmlService")
+
+const BaseDirectoryService = require("./BaseDirectoryService")
+
+const ISOMER_TEMPLATE_DIRS = ["_data", "_includes", "_site", "_layouts"]
+const ISOMER_TEMPLATE_PROTECTED_DIRS = [
+  "data",
+  "includes",
+  "site",
+  "layouts",
+  "files",
+  "images",
+  "misc",
+  "pages",
+]
+
+const ListAllCollections = async (reqDetails) => {
+  const filesOrDirs = await BaseDirectoryService.List(reqDetails, {
+    directoryName: "",
+  })
+  return filesOrDirs.reduce((acc, curr) => {
+    if (
+      curr.type === "dir" &&
+      !ISOMER_TEMPLATE_DIRS.includes(curr.name) &&
+      curr.name.slice(0, 1) === "_"
+    )
+      acc.push(curr.path.slice(1))
+    return acc
+  }, [])
+}
+
+const ListFiles = async (reqDetails, { directoryName }) =>
+  CollectionYmlService.ListContents(reqDetails, {
+    collectionName: directoryName,
+  })
+
+const Create = async (reqDetails, { directoryName, orderArray }) => {
+  if (ISOMER_TEMPLATE_PROTECTED_DIRS.includes(directoryName))
+    throw new ConflictError(protectedFolderConflictErrorMsg(directoryName))
+  await CollectionYmlService.Create(reqDetails, {
+    collectionName: directoryName,
+    orderArray,
+  })
+  // TODO: move files in order array if necessary
+}
+
+const Rename = async (reqDetails, { oldDirectoryName, newDirectoryName }) => {
+  if (ISOMER_TEMPLATE_PROTECTED_DIRS.includes(newDirectoryName))
+    throw new ConflictError(protectedFolderConflictErrorMsg(newDirectoryName))
+  await BaseDirectoryService.Rename(reqDetails, {
+    oldDirectoryName: `_${oldDirectoryName}`,
+    newDirectoryName: `_${newDirectoryName}`,
+    message: `Renaming collection ${oldDirectoryName} to ${newDirectoryName}`,
+  })
+  await CollectionYmlService.RenameCollectionInOrder(reqDetails, {
+    oldCollectionName: oldDirectoryName,
+    newCollectionName: newDirectoryName,
+  })
+  await NavYmlService.RenameCollectionInNav(reqDetails, {
+    oldCollectionName: oldDirectoryName,
+    newCollectionName: newDirectoryName,
+  })
+}
+
+const Delete = async (reqDetails, { directoryName }) => {
+  await BaseDirectoryService.Delete(reqDetails, {
+    directoryName,
+    message: `Deleting collection ${directoryName}`,
+  })
+  await NavYmlService.DeleteCollectionInNav(reqDetails, {
+    collectionName: directoryName,
+  })
+}
+
+module.exports = {
+  ListAllCollections,
+  ListFiles,
+  Create,
+  Rename,
+  Delete,
+}

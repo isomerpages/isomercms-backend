8cdbb35014c3c4e8a40133d127c3c1883d57f895
Alexander Lee <alexander@open.gov.sg>
2021-06-24 15:57:19 +0800

    Feat: add Mover service

---
+++ services/MoverService.js
@@ -0,0 +1,71 @@
+const CollectionPageService = require("@services/fileServices/MdPageServices/CollectionPageService")
+const ThirdNavPageService = require("@services/fileServices/MdPageServices/ThirdNavPageService")
+const UnlinkedPageService = require("@services/fileServices/MdPageServices/UnlinkedPageService")
+
+const MovePage = async (
+  reqDetails,
+  {
+    fileName,
+    oldFileDirectory,
+    oldFileThirdNav,
+    newFileDirectory,
+    newFileThirdNav,
+  }
+) => {
+  let fileContent
+  if (oldFileThirdNav) {
+    const { content, sha } = await ThirdNavPageService.Read(reqDetails, {
+      fileName,
+      collectionName: oldFileDirectory,
+      thirdNavTitle: oldFileThirdNav,
+    })
+    fileContent = content
+    await ThirdNavPageService.Delete(reqDetails, {
+      fileName,
+      collectionName: oldFileDirectory,
+      thirdNavTitle: oldFileThirdNav,
+      sha,
+    })
+  } else if (oldFileDirectory) {
+    const { content, sha } = await CollectionPageService.Read(reqDetails, {
+      fileName,
+      collectionName: oldFileDirectory,
+    })
+    fileContent = content
+    await CollectionPageService.Delete(reqDetails, {
+      fileName,
+      collectionName: oldFileDirectory,
+      sha,
+    })
+  } else {
+    const { content, sha } = await UnlinkedPageService.Read(reqDetails, {
+      fileName,
+    })
+    fileContent = content
+    await UnlinkedPageService.Delete(reqDetails, { fileName, sha })
+  }
+
+  if (newFileThirdNav) {
+    await ThirdNavPageService.Create(reqDetails, {
+      fileName,
+      collectionName: newFileDirectory,
+      thirdNavTitle: newFileThirdNav,
+      content: fileContent,
+    })
+  } else if (newFileDirectory) {
+    await CollectionPageService.Create(reqDetails, {
+      fileName,
+      collectionName: newFileDirectory,
+      content: fileContent,
+    })
+  } else {
+    await UnlinkedPageService.Create(reqDetails, {
+      fileName,
+      content: fileContent,
+    })
+  }
+}
+
+module.exports = {
+  MovePage,
+}

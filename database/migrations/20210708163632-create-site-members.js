a57cb275b24d9c8efda354386ec4e828f9dab072
Lam Kee Wei <keewei.lam@gmail.com>
2021-07-09 02:35:39 +0800

    feat: add database migrations to create tables

---
+++ database/migrations/20210708163632-create-site-members.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("site_members", {
+      user_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.BIGINT,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      site_id: {
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+          model: "sites",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.ENUM("owner", "user"),
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("site_members")
+  },
+}

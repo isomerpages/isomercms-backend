2714f3da82eb16744f95c6e1e1898a4788946a34
Alexander Lee <alexander@open.gov.sg>
2021-07-09 11:40:29 +0800

    Feat: add schema validation in route

--- newroutes/RequestSchema.js
+++ newroutes/RequestSchema.js
@@ -1,23 +1,29 @@
-import Joi from "joi"
+const Joi = require("joi")
 
 const FrontMatterSchema = Joi.object({
   title: Joi.string().required(),
   permalink: Joi.string().required(),
 })
 
-export const CreatePageRequestSchema = Joi.object().keys({
+const CreatePageRequestSchema = Joi.object().keys({
   frontMatter: FrontMatterSchema,
   pageBody: Joi.string().required(),
   newFileName: Joi.string().required(),
 })
 
-export const UpdatePageRequestSchema = Joi.object().keys({
+const UpdatePageRequestSchema = Joi.object().keys({
   frontMatter: FrontMatterSchema,
   pageBody: Joi.string().required(),
   sha: Joi.string().required(),
   newFileName: Joi.string().required(),
 })
 
-export const DeletePageRequestSchema = Joi.object().keys({
+const DeletePageRequestSchema = Joi.object().keys({
   sha: Joi.string().required(),
 })
+
+module.exports = {
+  CreatePageRequestSchema,
+  UpdatePageRequestSchema,
+  DeletePageRequestSchema,
+}

585bf67bf01c6468a20c7cb967b99c642fc6fdae
Alexander Lee <alexander@open.gov.sg>
2021-07-27 15:24:01 +0800

    Refactor: router into class

--- newroutes/collectionPages.js
+++ newroutes/collectionPages.js
@@ -7,128 +7,143 @@ const {
   attachRollbackRouteHandlerWrapper,
 } = require("@middleware/routeHandler")
 
-// Import classes
-const CollectionController = require("@controllers/CollectionController")
-
 const {
   CreatePageRequestSchema,
   UpdatePageRequestSchema,
   DeletePageRequestSchema,
 } = require("./RequestSchema")
 
-const router = express.Router()
+class CollectionPagesRouter {
+  constructor({ collectionController }) {
+    this.CollectionController = collectionController
 
-// Create new page in collection
-async function createCollectionPage(req, res) {
-  const { accessToken } = req
+    const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(this))
+    // Bind all methods
+    methods
+      .filter((method) => method !== "constructor")
+      .forEach((method) => {
+        this[method] = this[method].bind(this)
+      })
+  }
 
-  const { siteName, collectionName, subcollectionName } = req.params
-  const { error } = CreatePageRequestSchema.validate(req.body)
-  if (error) throw error
-  const { newFileName, pageBody, frontMatter } = req.body
-  const createResp = await CollectionController.CreatePage(
-    { siteName, accessToken },
-    {
-      fileName: newFileName,
-      collectionName,
-      content: pageBody,
-      frontMatter,
-      thirdNavTitle: subcollectionName,
-    }
-  )
+  // Create new page in collection
+  async createCollectionPage(req, res) {
+    const { accessToken } = req
 
-  return res.status(200).json(createResp)
+    const { siteName, collectionName, subcollectionName } = req.params
+    const { error } = CreatePageRequestSchema.validate(req.body)
+    if (error) throw error
+    const { newFileName, pageBody, frontMatter } = req.body
+    const createResp = await this.CollectionController.CreatePage(
+      { siteName, accessToken },
+      {
+        fileName: newFileName,
+        collectionName,
+        content: pageBody,
+        frontMatter,
+        thirdNavTitle: subcollectionName,
+      }
+    )
+
+    return res.status(200).json(createResp)
+  }
+
+  // Read page in collection
+  async readCollectionPage(req, res) {
+    const { accessToken } = req
+
+    const { siteName, pageName, collectionName, subcollectionName } = req.params
+    const { sha, content } = await this.CollectionController.ReadPage(
+      { siteName, accessToken },
+      { fileName: pageName, collectionName, thirdNavTitle: subcollectionName }
+    )
+
+    return res.status(200).json({ collectionName, pageName, sha, content })
+  }
+
+  // Update page in collection
+  async updateCollectionPage(req, res) {
+    const { accessToken } = req
+
+    const { siteName, pageName, collectionName, subcollectionName } = req.params
+    const { error } = UpdatePageRequestSchema.validate(req.body)
+    if (error) throw error
+    const { frontMatter, pageBody, sha, newFileName } = req.body
+    const updateResp = await this.CollectionController.UpdatePage(
+      { siteName, accessToken },
+      {
+        fileName: pageName,
+        newFileName,
+        collectionName,
+        thirdNavTitle: subcollectionName,
+        content: pageBody,
+        frontMatter,
+        sha,
+      }
+    )
+
+    return res.status(200).json(updateResp)
+  }
+
+  // Delete page in collection
+  async deleteCollectionPage(req, res) {
+    const { accessToken } = req
+
+    const { siteName, pageName, collectionName, subcollectionName } = req.params
+    const { error } = DeletePageRequestSchema.validate(req.body)
+    if (error) throw error
+    const { sha } = req.body
+    await this.CollectionController.DeletePage(
+      { siteName, accessToken },
+      {
+        fileName: pageName,
+        collectionName,
+        thirdNavTitle: subcollectionName,
+        sha,
+      }
+    )
+
+    return res.status(200).send("OK")
+  }
+
+  getRouter() {
+    const router = express.Router()
+
+    router.post(
+      "/:siteName/collections/:collectionName/pages",
+      attachRollbackRouteHandlerWrapper(this.createCollectionPage)
+    )
+    router.post(
+      "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages",
+      attachRollbackRouteHandlerWrapper(this.createCollectionPage)
+    )
+    router.get(
+      "/:siteName/collections/:collectionName/pages/:pageName",
+      attachReadRouteHandlerWrapper(this.readCollectionPage)
+    )
+    router.get(
+      "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
+      attachReadRouteHandlerWrapper(this.readCollectionPage)
+    )
+    router.post(
+      "/:siteName/collections/:collectionName/pages/:pageName",
+      attachWriteRouteHandlerWrapper(this.updateCollectionPage)
+    )
+    router.post(
+      "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
+      attachWriteRouteHandlerWrapper(this.updateCollectionPage)
+    )
+    router.delete(
+      "/:siteName/collections/:collectionName/pages/:pageName",
+      attachRollbackRouteHandlerWrapper(this.deleteCollectionPage)
+    )
+    router.delete(
+      "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
+      attachRollbackRouteHandlerWrapper(this.deleteCollectionPage)
+    )
+
+    return router
+  }
 }
 
-// Read page in collection
-async function readCollectionPage(req, res) {
-  const { accessToken } = req
-
-  const { siteName, pageName, collectionName, subcollectionName } = req.params
-  const { sha, content } = await CollectionController.ReadPage(
-    { siteName, accessToken },
-    { fileName: pageName, collectionName, thirdNavTitle: subcollectionName }
-  )
-
-  return res.status(200).json({ collectionName, pageName, sha, content })
-}
-
-// Update page in collection
-async function updateCollectionPage(req, res) {
-  const { accessToken } = req
-
-  const { siteName, pageName, collectionName, subcollectionName } = req.params
-  const { error } = UpdatePageRequestSchema.validate(req.body)
-  if (error) throw error
-  const { frontMatter, pageBody, sha, newFileName } = req.body
-  const updateResp = await CollectionController.UpdatePage(
-    { siteName, accessToken },
-    {
-      fileName: pageName,
-      newFileName,
-      collectionName,
-      thirdNavTitle: subcollectionName,
-      content: pageBody,
-      frontMatter,
-      sha,
-    }
-  )
-
-  return res.status(200).json(updateResp)
-}
-
-// Delete page in collection
-async function deleteCollectionPage(req, res) {
-  const { accessToken } = req
-
-  const { siteName, pageName, collectionName, subcollectionName } = req.params
-  const { error } = DeletePageRequestSchema.validate(req.body)
-  if (error) throw error
-  const { sha } = req.body
-  await CollectionController.DeletePage(
-    { siteName, accessToken },
-    {
-      fileName: pageName,
-      collectionName,
-      thirdNavTitle: subcollectionName,
-      sha,
-    }
-  )
-
-  return res.status(200).send("OK")
-}
-
-router.post(
-  "/:siteName/collections/:collectionName/pages",
-  attachRollbackRouteHandlerWrapper(createCollectionPage)
-)
-router.post(
-  "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages",
-  attachRollbackRouteHandlerWrapper(createCollectionPage)
-)
-router.get(
-  "/:siteName/collections/:collectionName/pages/:pageName",
-  attachReadRouteHandlerWrapper(readCollectionPage)
-)
-router.get(
-  "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
-  attachReadRouteHandlerWrapper(readCollectionPage)
-)
-router.post(
-  "/:siteName/collections/:collectionName/pages/:pageName",
-  attachWriteRouteHandlerWrapper(updateCollectionPage)
-)
-router.post(
-  "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
-  attachWriteRouteHandlerWrapper(updateCollectionPage)
-)
-router.delete(
-  "/:siteName/collections/:collectionName/pages/:pageName",
-  attachRollbackRouteHandlerWrapper(deleteCollectionPage)
-)
-router.delete(
-  "/:siteName/collections/:collectionName/subcollections/:subcollectionName/pages/:pageName",
-  attachRollbackRouteHandlerWrapper(deleteCollectionPage)
-)
-
-module.exports = router
+module.exports = { CollectionPagesRouter }

e22a2362f11a6da03fe4c8496a928ff45eb49efc
Lam Kee Wei <keewei.lam@gmail.com>
2021-07-29 15:55:26 +0800

    feat: add AuthService

---
+++ services/AuthService.js
@@ -0,0 +1,27 @@
+const axios = require("axios")
+
+const ISOMER_GITHUB_ORG_NAME = process.env.GITHUB_ORG_NAME
+
+class AuthService {
+  async hasAccessToSite(siteName, userId, accessToken) {
+    const endpoint = `https://api.github.com/repos/${ISOMER_GITHUB_ORG_NAME}/${siteName}/collaborators/${userId}`
+
+    try {
+      await axios.get(endpoint, {
+        headers: {
+          Authorization: `token ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      })
+      return true
+    } catch (err) {
+      const { status } = err.response
+      if (status === 404 || status === 403) {
+        return false
+      }
+      throw err
+    }
+  }
+}
+
+module.exports = new AuthService()

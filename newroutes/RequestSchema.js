e04ab8fb8760c05bda69a0ef0031e6cda16b0ca2
Alexander Lee <alexander@open.gov.sg>
2021-08-11 10:06:12 +0800

    Fix: update schema to allow blank new name

--- newroutes/RequestSchema.js
+++ newroutes/RequestSchema.js
@@ -6,19 +6,19 @@ const FrontMatterSchema = Joi.object({
 })
 
 const ContentSchema = Joi.object({
-  frontMatter: FrontMatterSchema,
+  frontMatter: FrontMatterSchema.required(),
   pageBody: Joi.string().allow(""),
 })
 
 const CreatePageRequestSchema = Joi.object().keys({
-  content: ContentSchema,
+  content: ContentSchema.required(),
   newFileName: Joi.string().required(),
 })
 
 const UpdatePageRequestSchema = Joi.object().keys({
-  content: ContentSchema,
+  content: ContentSchema.required(),
   sha: Joi.string().required(),
-  newFileName: Joi.string().required(),
+  newFileName: Joi.string(),
 })
 
 const DeletePageRequestSchema = Joi.object().keys({

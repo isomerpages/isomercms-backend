ff0cd6cfa31ed9abc67eb66c7000e7036f7585be
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-09-02 01:39:25 +0800

    test: NavYmlService unit tests
    This commit adds the unit tests for the "read" and "update" methods
    of the NavYmlService. It also adds the necessary fixtures for
    the navigation file.

--- fixtures/navigation.js
+++ fixtures/navigation.js
@@ -1,3 +1,15 @@
+const rawNavigationContent = `logo: /images/favicon-isomer.ico
+links:
+  - title: Contact Us
+    url: /contact-us/
+  - title: Test
+    url: /Test/
+  - title: Resources
+    resource_room: true
+  - title: Test Folder Title No Pages
+    collection: test-folder-title-no-pages
+`
+
 const navigationContent = {
   logo: "/images/favicon-isomer.ico",
   links: [
@@ -21,4 +33,5 @@ module.exports = {
   navigationContent,
   navigationSha,
   navigationResponse,
+  rawNavigationContent,
 }

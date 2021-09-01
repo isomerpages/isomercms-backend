66d3ac33d27fd3a08d81eaf6bca97d11e6d02c1c
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-09-02 00:17:13 +0800

    test: HomepagePageService unit tests
    This commit adds the unit tests for the "read" and "update" methods
    of the HomepagePageService. It also adds the necessary fixtures for
    the homepage.

--- fixtures/homepage.js
+++ fixtures/homepage.js
@@ -1,3 +1,36 @@
+const rawHomepageContent = `---
+layout: homepage
+title: abcdefg
+description: Brief site description here
+image: /images/isomer-logo.svg
+permalink: /
+notification: Here's a notification bar you can use!
+sections:
+  - hero:
+      title: Hero titlZZZZ
+      subtitle: Hero subtitle
+      background: /images/hero-banner.png
+      button: Contact Us
+      url: /contact-us/
+      key_highlights:
+        - title: Highlight A
+          description: Important highlight A is important
+          url: https://google.com
+        - title: Highlight B
+          description: Important highlight B is equally important
+          url: https://gmail.com
+        - title: Page A
+          description: Page A is important too
+          url: /privacy/
+  - infobar:
+      title: Infobar title
+      subtitle: Subtitle
+      description: About a sentence worth of description here
+      button: Button text
+      url: /faq/
+---
+`
+
 const homepageContent = {
   frontMatter: {
     layout: "homepage",
@@ -44,7 +77,7 @@ const homepageContent = {
       },
     ],
   },
-  pageBody: "\n\n\n\n\n\n\n\n\n\n\n\n",
+  pageBody: "\n",
 }
 
 const homepageSha = "homepageSha"
@@ -52,4 +85,5 @@ const homepageSha = "homepageSha"
 module.exports = {
   homepageContent,
   homepageSha,
+  rawHomepageContent,
 }

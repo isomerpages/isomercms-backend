619fcddcab3b1d775998345d0e994e1fcd6ed3f6
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-09-02 01:35:46 +0800

    test: FooterYmlService unit tests
    This commit adds the unit tests for the "read" and "update" methods
    of the FooterYmlService. It also adds the necessary fixtures for
    the footer file.

--- fixtures/footer.js
+++ fixtures/footer.js
@@ -1,3 +1,15 @@
+const rawFooterContent = `show_reach: true
+feedback: www.feedbacktest.com/hihi
+faq: /faqpagetest/hihi
+social_media:
+  facebook: https://www.facebook.com/testfbnow
+  twitter: https://www.twitter.com/testtwitternow
+  youtube: https://www.youtube.com/testyoutubenow
+  instagram: https://www.instagram.com/testinstanow
+  linkedin: https://www.linkedin.com/company/testagencynow
+contact_us: astast
+`
+
 const footerContent = {
   show_reach: true,
   feedback: "www.feedbacktest.com/hihi",
@@ -22,4 +34,5 @@ module.exports = {
   footerContent,
   footerSha,
   footerResponse,
+  rawFooterContent,
 }

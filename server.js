06d8031ccb52f7f53d6780ccba821342344e78ea
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2021-09-08 16:14:09 +0800

    chore: add health check endpoint

--- server.js
+++ server.js
@@ -132,6 +132,8 @@ app.use("/v1/sites", netlifyTomlRouter)
 app.use("/v2/sites", collectionPagesV2Router.getRouter())
 app.use("/v2/sites", unlinkedPagesRouter.getRouter())
 
+app.use("/v2/ping", (req, res, next) => res.status(200).send("Ok"))
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404))

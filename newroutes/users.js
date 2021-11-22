3fb07dba5bd404bbd05647092389cc32a9d32f5c
Lam Kee Wei <keewei.lam@gmail.com>
2021-11-22 12:41:38 +0800

    feat: update err message for non-govt emails

--- newroutes/users.js
+++ newroutes/users.js
@@ -22,13 +22,15 @@ class UsersRouter {
 
     try {
       if (!(await this.usersService.canSendEmailOtp(email))) {
-        throw new Error(`Invalid email ${email}`)
+        throw new Error(
+          `The email you have entered is not a government-issued email. Please contact your website owner for assistance.`
+        )
       }
       await this.usersService.sendEmailOtp(email)
       return res.sendStatus(200)
     } catch (err) {
       logger.error(err.message)
-      throw new BadRequestError("Unable to send OTP")
+      throw new BadRequestError(err.message)
     }
   }
 

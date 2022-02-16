1d67cd2afddafcc2db7317bddf2bf560f24dcf38
Alexander Lee <alexander@open.gov.sg>
2022-02-16 16:35:31 +0800

    Style: early return

--- services/middlewareServices/AuthMiddlewareService.js
+++ services/middlewareServices/AuthMiddlewareService.js
@@ -16,84 +16,72 @@ class AuthMiddlewareService {
   verifyE2E({ cookies, url }) {
     const { isomercmsE2E } = cookies
     const urlTokens = url.split("/") // urls take the form "/v1/sites/<repo>/<path>""
-    let isValidE2E
 
-    if (isomercmsE2E) {
-      if (isomercmsE2E !== E2E_TEST_SECRET)
-        throw new AuthError("Bad credentials")
+    if (!isomercmsE2E) return false
 
-      // Throw an error if accessing a repo other than e2e-test-repo
-      // Otherwise, allow access only to paths available to all users
-      if (!GENERAL_ACCESS_PATHS.includes(url)) {
-        if (urlTokens.length >= 3) {
-          const repo = urlTokens[3]
-          if (repo !== E2E_TEST_REPO)
-            throw new AuthError(
-              `E2E tests can only access the ${E2E_TEST_REPO} repo`
-            )
-        } else {
-          throw new BadRequestError("Invalid path")
-        }
-      }
+    if (isomercmsE2E !== E2E_TEST_SECRET) throw new AuthError("Bad credentials")
 
-      isValidE2E = true
-    }
+    if (urlTokens.length < 3) throw new BadRequestError("Invalid path")
 
-    return isValidE2E
+    // General access paths are allowed
+    if (GENERAL_ACCESS_PATHS.includes(url)) return true
+
+    // Throw an error if accessing a repo other than e2e-test-repo
+    const repo = urlTokens[3]
+    if (repo !== E2E_TEST_REPO)
+      throw new AuthError(`E2E tests can only access the ${E2E_TEST_REPO} repo`)
+
+    return true
   }
 
   verifyJwt({ cookies, url }) {
     const { isomercms } = cookies
     const isValidE2E = this.verifyE2E({ cookies, url })
 
-    let accessToken
-    let userId
     if (isValidE2E) {
-      accessToken = E2E_TEST_GH_TOKEN
-      userId = E2E_TEST_USER
-    } else {
-      try {
-        const {
-          access_token: retrievedToken,
-          user_id: retrievedId,
-        } = jwtUtils.verifyToken(isomercms)
-        accessToken = jwtUtils.decryptToken(retrievedToken)
-        userId = retrievedId
-      } catch (err) {
-        logger.error("Authentication error")
-        if (err.name === "TokenExpiredError") {
-          throw new AuthError("JWT token has expired")
-        }
-        if (err.name === "JsonWebTokenError") {
-          throw new AuthError(err.message)
-        }
-        throw new Error(err)
-      }
+      const accessToken = E2E_TEST_GH_TOKEN
+      const userId = E2E_TEST_USER
+      return { accessToken, userId }
     }
-    return { accessToken, userId }
+    try {
+      const {
+        access_token: retrievedToken,
+        user_id: retrievedId,
+      } = jwtUtils.verifyToken(isomercms)
+      const accessToken = jwtUtils.decryptToken(retrievedToken)
+      const userId = retrievedId
+      return { accessToken, userId }
+    } catch (err) {
+      logger.error("Authentication error")
+      if (err.name === "TokenExpiredError") {
+        throw new AuthError(`JWT token has expired`)
+      }
+      if (err.name === "JsonWebTokenError") {
+        throw new AuthError(
+          `Encountered an auth error at ${url}: ${err.message}`
+        )
+      }
+      throw err
+    }
   }
 
   whoamiAuth({ cookies, url }) {
     const isValidE2E = this.verifyE2E({ cookies, url })
 
-    let accessToken
-    let userId
     if (isValidE2E) {
-      accessToken = E2E_TEST_GH_TOKEN
-      userId = E2E_TEST_USER
-    } else {
-      let retrievedToken
-      try {
-        const { isomercms } = cookies
-        const { access_token: verifiedToken } = jwtUtils.verifyToken(isomercms)
-        retrievedToken = jwtUtils.decryptToken(verifiedToken)
-      } catch (err) {
-        retrievedToken = undefined
-      } finally {
-        accessToken = retrievedToken
-      }
+      const accessToken = E2E_TEST_GH_TOKEN
+      const userId = E2E_TEST_USER
+      return { accessToken, userId }
     }
-    return { accessToken, userId }
+    let retrievedToken
+    try {
+      const { isomercms } = cookies
+      const { access_token: verifiedToken } = jwtUtils.verifyToken(isomercms)
+      retrievedToken = jwtUtils.decryptToken(verifiedToken)
+    } catch (err) {
+      retrievedToken = undefined
+    }
+    return { accessToken: retrievedToken, userId: undefined }
   }
 }
 

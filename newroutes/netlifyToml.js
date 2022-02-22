176545c948d9bee0d01781d63f9e76761d2aceb6
Alexander Lee <alexander@open.gov.sg>
2022-02-22 16:47:28 +0800

    Feat: add netlifyToml router

---
+++ newroutes/netlifyToml.js
@@ -0,0 +1,36 @@
+const autoBind = require("auto-bind")
+const express = require("express")
+
+const { attachReadRouteHandlerWrapper } = require("@middleware/routeHandler")
+
+class NetlifyTomlRouter {
+  constructor({ netlifyTomlService }) {
+    this.netlifyTomlService = netlifyTomlService
+    // We need to bind all methods because we don't invoke them from the class directly
+    autoBind(this)
+  }
+
+  // Read netlify.toml file
+  async readNetlifyToml(req, res) {
+    const { accessToken } = req
+
+    const netlifyTomlHeaderValues = await this.netlifyTomlService.read({
+      accessToken,
+    })
+
+    return res.status(200).json({ netlifyTomlHeaderValues })
+  }
+
+  getRouter() {
+    const router = express.Router()
+
+    router.get(
+      "/:siteName/netlify-toml",
+      attachReadRouteHandlerWrapper(this.readNetlifyToml)
+    )
+
+    return router
+  }
+}
+
+module.exports = { NetlifyTomlRouter }

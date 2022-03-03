672162457f739e47d086a31bc1f47e62fdaaf1be
seaerchin <jiachin@open.gov.sg>
2022-03-03 11:42:46 +0800

    test(tokenstore): added tests

---
+++ services/identity/__tests__/TokenStore.spec.ts
@@ -0,0 +1,48 @@
+import {
+  secretsManagerClient,
+  GetSecretValueCommand,
+} from "@root/__mocks__/@aws-sdk/client-secrets-manager"
+
+import _TokenStore from "../TokenStore"
+
+const apiTokenName = "some token"
+const TokenStore = new _TokenStore()
+
+describe("Token Store", () => {
+  beforeEach(() => {
+    // Clear all instances and calls to constructor and all methods:
+    secretsManagerClient.send.mockClear()
+  })
+
+  it("should return the api token when the parameters are valid", async () => {
+    // Arrange
+    const expected = "some api"
+    const awsCommand = new GetSecretValueCommand({
+      SecretId: apiTokenName,
+    })
+    secretsManagerClient.send.mockResolvedValueOnce({ SecretString: expected })
+
+    // Act
+    const actual = await TokenStore.getToken(apiTokenName)
+
+    // Assert
+    expect(actual).toBe(expected)
+    expect(secretsManagerClient.send).toHaveBeenCalledWith(awsCommand)
+  })
+
+  it("should return the error when the secrets client fails to retrieve credentials", () => {
+    // Arrange
+    const expected = Error("oh noes")
+    const awsCommand = new GetSecretValueCommand({
+      SecretId: apiTokenName,
+    })
+    secretsManagerClient.send.mockRejectedValueOnce(expected)
+
+    // Act
+    const actual = TokenStore.getToken(apiTokenName)
+
+    // Assert
+    expect(actual).rejects.toBe(expected)
+    expect(secretsManagerClient.send).toHaveBeenCalledWith(awsCommand)
+  })
+})

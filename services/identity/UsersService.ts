c337f085ee8bff7341a89d8a9d77a3f9e95f30a4
seaerchin <jiachin@open.gov.sg>
2022-03-08 11:36:12 +0800

    refactor(userservice): convert to ts
    refactor(userservice): update to ts

--- services/identity/UsersService.ts
+++ services/identity/UsersService.ts
@@ -1,56 +1,83 @@
-const { sequelize } = require("@database/models")
+import { Attributes } from "sequelize"
+
+import sequelize from "@database/index"
+import { User } from "@database/models"
+
+import MailClient from "./MailClient"
+import SmsClient from "./SmsClient"
+import TotpGenerator from "./TotpGenerator"
 
 // Allowed domains is a semicolon separate list of domains (e.g. .gov.sg, @agency.com.sg, etc)
 // that are allowed to login.
 const { DOMAIN_WHITELIST } = process.env
 
+interface UsersServiceProps {
+  otp: TotpGenerator
+  mailer: MailClient
+  smsClient: SmsClient
+  repository: Attributes<User>
+}
+
 class UsersService {
-  constructor({ otp, mailer, smsClient, repository }) {
-    this.repository = repository
+  // NOTE: Explicitly specifying using keyed properties to ensure
+  // that the types are synced.
+  private readonly otp: UsersServiceProps["otp"]
+
+  private readonly mailer: UsersServiceProps["mailer"]
+
+  private readonly smsClient: UsersServiceProps["smsClient"]
+
+  private readonly repository: UsersServiceProps["repository"]
+
+  private readonly whitelistDomains: string[]
+
+  constructor({ otp, mailer, smsClient, repository }: UsersServiceProps) {
     this.otp = otp
     this.mailer = mailer
     this.smsClient = smsClient
+    this.repository = repository
 
     this.whitelistDomains = (DOMAIN_WHITELIST || ".gov.sg")
       .split(";")
       .map((domain) => domain.toLowerCase().trim())
   }
 
-  async findByEmail(email) {
+  async findByEmail(email: string) {
     return this.repository.findOne({ where: { email } })
   }
 
-  async findByGitHubId(githubId) {
+  async findByGitHubId(githubId: string) {
     return this.repository.findOne({ where: { githubId } })
   }
 
-  async updateUserByGitHubId(githubId, user) {
+  async updateUserByGitHubId(githubId: string, user: string) {
     await this.repository.update(user, { where: { githubId } })
   }
 
-  async findOrCreate(githubId) {
+  async findOrCreate(githubId: string | undefined) {
     const [user] = await this.repository.findOrCreate({
       where: { githubId },
     })
     return user
   }
 
-  async login(githubId) {
+  async login(githubId: string) {
     return sequelize.transaction(async (transaction) => {
-      const user = await this.findOrCreate(githubId, { transaction })
+      // NOTE: The service's findOrCreate is not being used here as this requires an explicit transaction
+      const user = await this.repository.findOrCreate(githubId, { transaction })
       user.lastLoggedIn = new Date()
       return user.save({ transaction })
     })
   }
 
-  async canSendEmailOtp(email) {
+  async canSendEmailOtp(email: string) {
     const hasMatchDomain =
       this.whitelistDomains.filter((domain) => email.endsWith(domain)).length >
       0
     return hasMatchDomain
   }
 
-  async sendEmailOtp(email) {
+  async sendEmailOtp(email: string) {
     const otp = this.otp.generate(email)
     const expiry = this.otp.getExpiryMinutes()
 
@@ -60,7 +87,7 @@ class UsersService {
     await this.mailer.sendMail(email, html)
   }
 
-  async sendSmsOtp(mobileNumber) {
+  async sendSmsOtp(mobileNumber: string) {
     const otp = this.otp.generate(mobileNumber)
     const expiry = this.otp.getExpiryMinutes()
 
@@ -68,9 +95,9 @@ class UsersService {
     await this.smsClient.sendSms(mobileNumber, message)
   }
 
-  verifyOtp(value, otp) {
+  verifyOtp(value: string, otp: string) {
     return this.otp.verify(value, otp)
   }
 }
 
-module.exports = UsersService
+export default UsersService

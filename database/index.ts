d5caed7019d057ac6ef38d5dddf377b9490052f1
seaerchin <jiachin@open.gov.sg>
2022-03-10 11:34:48 +0800

    refactor(database): defer sequelization initialization to caller
    this allows us to pass in base models at run time rather than statically deciding at compile time.
    this is useful when we want to test services that require a db but we wish to mock the db

--- database/index.ts
+++ database/index.ts
@@ -2,10 +2,16 @@ import { Sequelize, SequelizeOptions } from "sequelize-typescript"
 
 import sequelizeConfig from "@database/config.js"
 
-import { Site, SiteMember, User } from "./models"
-
-// Sequelize init
-const sequelize = new Sequelize(sequelizeConfig as SequelizeOptions)
-sequelize.addModels([User, Site, SiteMember])
-
-export default sequelize
+/**
+ * The index argument is because the Parameters<T> type returns the parameters
+ * as a tuple type, so we need to index into it.
+ *
+ * This method is to allow for ease of testing, so that we can dynamically inject
+ * fake db models into sequelize, rather than depending upon concrete instances
+ * in our unit tests.
+ */
+export default (models: Parameters<Sequelize["addModels"]>[0]) => {
+  const sequelize = new Sequelize(sequelizeConfig as SequelizeOptions)
+  sequelize.addModels(models)
+  return sequelize
+}

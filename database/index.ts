16b410c07c155d8f0c3fbebe5413babf07798dfa
seaerchin <jiachin@open.gov.sg>
2022-03-11 14:03:29 +0800

    fix(databse/index): add typecast

--- database/index.ts
+++ database/index.ts
@@ -1,11 +1,11 @@
-import { Sequelize } from "sequelize-typescript"
+import { Sequelize, SequelizeOptions } from "sequelize-typescript"
 
 import sequelizeConfig from "@database/config"
 
 import { Site, SiteMember, User } from "./models"
 
 // Sequelize init
-const sequelize = new Sequelize(sequelizeConfig)
+const sequelize = new Sequelize(sequelizeConfig as SequelizeOptions)
 sequelize.addModels([User, Site, SiteMember])
 
 export default sequelize

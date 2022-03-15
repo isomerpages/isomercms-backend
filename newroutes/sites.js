7d79ddaf14d2b3fc0c55897d35788c111b6bc080
Alexander Lee <alexander@open.gov.sg>
2022-03-15 15:28:56 +0800

    Refactor/sites (#341)
    * Feat: add sitesService
    
    * Feat: add sites router
    
    * Feat: add site endpoints
    
    * Tests: add tests
    
    * Fix: use pushed_at instead of updated_at
    
    * Style: add spacing to tests
    
    * Style: object destructuring
    
    * Style: return type and map
    
    * Fix: return last updated time instead of string representing time and update tests
    
    * Feat: throw 404 error if no staging url found
    
    * Style: destructuring response
    
    * Feat: add access check in github service
    
    * Fix: tests
    
    * Fix: test comment
    
    * Rebase: use auth verify middleware in sites router
    
    * Fix: test for config priority

---
+++ newroutes/sites.js
@@ -0,0 +1,84 @@
+const autoBind = require("auto-bind")
+const express = require("express")
+
+// Import middleware
+const { attachReadRouteHandlerWrapper } = require("@middleware/routeHandler")
+
+const { authMiddleware } = require("@root/newmiddleware/index")
+
+class SitesRouter {
+  constructor({ sitesService }) {
+    this.sitesService = sitesService
+    // We need to bind all methods because we don't invoke them from the class directly
+    autoBind(this)
+  }
+
+  async getSites(req, res) {
+    const { accessToken } = req
+    const siteNames = await this.sitesService.getSites({ accessToken })
+    return res.status(200).json({ siteNames })
+  }
+
+  async checkHasAccess(req, res) {
+    const {
+      accessToken,
+      userId,
+      params: { siteName },
+    } = req
+
+    await this.sitesService.checkHasAccess(
+      {
+        accessToken,
+        siteName,
+      },
+      { userId }
+    )
+    return res.status(200).send("OK")
+  }
+
+  async getLastUpdated(req, res) {
+    const {
+      accessToken,
+      params: { siteName },
+    } = req
+    const lastUpdated = await this.sitesService.getLastUpdated({
+      accessToken,
+      siteName,
+    })
+    return res.status(200).json({ lastUpdated })
+  }
+
+  async getStagingUrl(req, res) {
+    const {
+      accessToken,
+      params: { siteName },
+    } = req
+
+    const stagingUrl = await this.sitesService.getStagingUrl({
+      accessToken,
+      siteName,
+    })
+    return res.status(200).json({ stagingUrl })
+  }
+
+  getRouter() {
+    const router = express.Router()
+
+    router.use(authMiddleware.verifyJwt)
+
+    router.get("/", attachReadRouteHandlerWrapper(this.getSites))
+    router.get("/:siteName", attachReadRouteHandlerWrapper(this.checkHasAccess))
+    router.get(
+      "/:siteName/lastUpdated",
+      attachReadRouteHandlerWrapper(this.getLastUpdated)
+    )
+    router.get(
+      "/:siteName/stagingUrl",
+      attachReadRouteHandlerWrapper(this.getStagingUrl)
+    )
+
+    return router
+  }
+}
+
+module.exports = { SitesRouter }

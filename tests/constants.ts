c8ed43433edd3191357ddfe7a1fdbd98aa026195
seaerchin <jiachin@open.gov.sg>
2022-03-23 16:01:08 +0800

    chore(constants): add new bearertokenheader

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,10 +1,19 @@
 export const mockRecipient = "hello@world.com"
 export const mockBody = "somebody"
+
+export const mockAccessToken = "some token"
+
 export const mockHeaders = {
   headers: {
-    Authorization: `Bearer ${process.env.POSTMAN_API_KEY}`,
+    Authorization: `token ${mockAccessToken}`,
+    "Content-Type": "application/json",
   },
 }
 export const mockSiteName = "hello world"
 export const mockUserId = "some user id"
-export const mockAccessToken = "some token"
+
+export const mockBearerTokenHeaders = {
+  headers: {
+    Authorization: `Bearer ${process.env.POSTMAN_API_KEY}`,
+  },
+}

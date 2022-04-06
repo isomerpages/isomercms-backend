52b0894b91320ce54408e28232cc1bc12b407447
seaerchin <44049504+seaerchin@users.noreply.github.com>
2022-04-06 14:21:33 +0800

    refactor(mailclient): pass api key explicitly (#419)
    * refactor(mailclient): use di
    
    * test(mailclient): update test for new api
    
    * refactor(identity): update mailclient init
    
    * chore(identity): update error message for mailclient to show on non local dev

--- services/identity/__tests__/MailClient.spec.ts
+++ services/identity/__tests__/MailClient.spec.ts
@@ -10,7 +10,7 @@ import _MailClient from "../MailClient"
 
 const mockEndpoint = "https://api.postman.gov.sg/v1/transactional/email/send"
 
-const MailClient = new _MailClient()
+const MailClient = new _MailClient(process.env.POSTMAN_API_KEY!)
 
 const generateEmail = (recipient: string, body: string) => ({
   subject: "One-Time Password (OTP) for IsomerCMS",
@@ -21,20 +21,6 @@ const generateEmail = (recipient: string, body: string) => ({
 })
 
 describe("Mail Client", () => {
-  const OLD_ENV = process.env
-
-  beforeEach(() => {
-    // Clears the cache so imports in tests uses a fresh copy
-    jest.resetModules()
-    // Make a copy of existing environment
-    process.env = { ...OLD_ENV }
-  })
-
-  afterAll(() => {
-    // Restore old environment
-    process.env = OLD_ENV
-  })
-
   afterEach(() => mockAxios.reset())
 
   it("should return the result successfully when all parameters are valid", async () => {
@@ -51,27 +37,6 @@ describe("Mail Client", () => {
       generateEmail(mockRecipient, mockBody),
       mockBearerTokenHeaders
     )
-  })
-
-  it("should throw an error on initialization when the env var is not set", async () => {
-    // Arrange
-    // Store the API key and set it later so that other tests are not affected
-    const curApiKey = process.env.POSTMAN_API_KEY
-    process.env.POSTMAN_API_KEY = ""
-    // NOTE: This is because of typescript transpiling down to raw js
-    // Export default compiles down to module.exports.default, which is also
-    // done by babel.
-    // Read more here: https://www.typescriptlang.org/tsconfig#allowSyntheticDefaultImports
-    const _MailClientWithoutKey = (await import("../MailClient")).default
-
-    // Act
-    // NOTE: We require a new instance because the old one would already have the API key bound
-    const actual = () => new _MailClientWithoutKey()
-
-    // Assert
-    expect(actual).toThrowError("Postman.gov.sg API key cannot be empty")
-    process.env.POSTMAN_API_KEY = curApiKey
-    expect(process.env.POSTMAN_API_KEY).toBe(curApiKey)
   })
 
   it("should return an error when a network error occurs", async () => {

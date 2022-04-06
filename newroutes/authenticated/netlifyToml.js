6cdc97bfb1a11de67ee01907b806d1aaf17bc5f8
Alexander Lee <alexander@open.gov.sg>
2022-04-06 09:34:01 +0800

    Refactor/netlify toml (#357)
    * Feat: add NetlifyTomlService
    
    * Feat: add netlifyToml router
    
    * Test: add new fixture for netlifyToml
    
    * Test: add tests for netlifyToml service and router
    
    * Fix: remove sitename from netlify-toml endpoint
    
    * Nit: add line space
    
    * Chore: include spec in name of test file and minor comment changes
    
    * Rebase: use auth verify middleware in router
    
    * Nit: add more details to comment
    
    * Fix: move routers to appropriate subrouters and swap to res.locals
    
    * Fix: axios mock in test for netlifytoml service

---
+++ newroutes/authenticated/netlifyToml.js
@@ -0,0 +1,33 @@
+const autoBind = require("auto-bind")
+const express = require("express")
+
+const { attachReadRouteHandlerWrapper } = require("@middleware/routeHandler")
+
+class NetlifyTomlRouter {
+  constructor({ netlifyTomlService }) {
+    this.netlifyTomlService = netlifyTomlService
+    // We need to bind all methods because we don't invoke them from the class directly
+    autoBind(this)
+  }
+
+  // Read netlify.toml file
+  async readNetlifyToml(req, res) {
+    const { accessToken } = res.locals
+
+    const netlifyTomlHeaderValues = await this.netlifyTomlService.read({
+      accessToken,
+    })
+
+    return res.status(200).json({ netlifyTomlHeaderValues })
+  }
+
+  getRouter() {
+    const router = express.Router({ mergeParams: true })
+
+    router.get("/", attachReadRouteHandlerWrapper(this.readNetlifyToml))
+
+    return router
+  }
+}
+
+module.exports = { NetlifyTomlRouter }

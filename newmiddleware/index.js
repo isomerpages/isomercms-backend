0251b85ea04b1fe9398ae82c6ab7cd67b5fb48c1
Alexander Lee <alexander@open.gov.sg>
2022-06-23 18:01:33 +0800

    Feat: initialise and export formSGHandler

--- newmiddleware/index.js
+++ newmiddleware/index.js
@@ -1,3 +1,7 @@
+import FormSGService from "@root/services/middlewareServices/FormSGService"
+
+import FormSGMiddleware from "./formsg"
+
 const {
   AuthMiddlewareService,
 } = require("@services/middlewareServices/AuthMiddlewareService")
@@ -12,6 +16,25 @@ const getAuthMiddleware = ({ identityAuthService }) => {
   return authMiddleware
 }
 
+const formSGService = new FormSGService()
+const formSGMiddleware = new FormSGMiddleware({ formSGService })
+
+/**
+ * Handles FormSG response authentication and decryption. 
+ * 
+ * To be inserted before routes requiring FormSG response data - e.g. 
+ * router.post(
+      "/",
+      attachFormSGHandler(<formKey>),
+      actualRoute
+    )
+ *
+ * Retrieve form data from res.locals.submission.
+ */
+const attachFormSGHandler = (formKey) =>
+  formSGMiddleware.authenticateAndDecrypt({ formKey })
+
 module.exports = {
   getAuthMiddleware,
+  attachFormSGHandler,
 }

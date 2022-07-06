48068d9beca670547277667589f519685cfb0144
Preston Lim <prestonlimlianjie@gmail.com>
2022-07-06 12:14:33 +0800

    feat: add site creation tables (#451)
    * feat: add repos and deployments tables
    
    * feat: modify users and sites tables
    
    * feat: set key tables to paranoid
    
    * feat: add migrations
    
    * fix: import issues
    
    * build: set alias for constants folder
    
    * fix: hard delete user records in tests
    
    * fix: This is necessary because we set the Users table to be paranoid (so Sequelize does a soft delete), but the githubId column has a unique constraint.
    
    * fix: make deletedAt optional
    
    * chore: use aliases in database folder
    
    * fix: Introduces `tsconfig-paths/register` to allow globalSetup/Teardown to use aliases. Reference: https://stackoverflow.com/a/67729228
    
    * fix: enum name issues
    
    * fix: user has many created sites
    
    * fix: remove unnecessary onDelete cascade
    
    * fix: snake_case for migrations
    
    * fix: updated_at and created_at
    
    * fix: set default val for JobStatus to running
    
    * feat: This is so that we have a default SiteStatus of "INIT" and JobStatus "RUNNING" when the site record is first created
    
    * feat: site creation flow (#456)
    
    * build(deps): add deps for Amplify and GitHub
    
    * build(env-vars): add SYSTEM_GITHUB_TOKEN
    
    * feat: add create + update methods to SitesService
    
    * feat: Deployments and Repos services
    
    * feat: InfraService
    
    * feat: use InfraService in route
    
    * feat: trigger Amplify build
    
    * refactor(deployments): split client from service
    
    * chore: The original DeploymentsService contained a lot of boilerplate code/interfaces/types that were very specific to AWS Amplify - this commit refactors all of that into a new DeploymentClient file, similar to SmsClient and MailClient
    
    * Feat/add formsg middleware (#458)
    
    * Chore: install formsg-sdk
    
    * Feat: add FormSGService
    
    * Feat: add formsgMiddleware
    
    * Feat: initialise and export formSGHandler
    
    * Refactor: insert formsg sdk as dependency
    
    * Chore: rename formsg service
    
    * Chore: convert newmiddleware/index to typescript
    
    * Fix: shift FormSGMiddleware method into newmiddleware/index
    
    * Chore: remove unnecessary conditional, mark unused variable, swap ordering
    
    * Chore: remove unused middleware
    
    * Tests: add tests
    
    * Fix: check specifically for null
    
    * Chore: rename to FormsProcessingService
    
    * Chore: swap to import
    
    * Nit: swap return type
    
    * Chore: rename test file
    
    * Chore: rename interface
    
    * Fix: remove extra try catch block
    
    * Nit: change casing
    
    * Feat: add new error type
    
    * Test: add test case for unsuccessful decrypt
    
    * Chore: update autherror messages
    
    * Chore: add postUri to autherror message
    
    * Chore: add postUri to UnprocessableError
    
    * Refactor/generalised mail client (#457)
    
    * Refactor: move subject into sendMail argument
    
    * Test: update tests
    
    * Fix: failing getSites test
    
    * Fix: update test env vars
    
    * Feat/site creation integrate (#459)
    
    * feat: /formsg/creat-site hook for site creation form
    
    * feat: Send email on site create error and success
    
    * feat: set amplify urls in _config.yml
    
    * feat: store hosting_id in deployments table
    
    * chore: address PR comments
    
    * chore: Refactor InfraService and ddress remaining PR comments.
    
    * chore: add AWS_REGION to .env-example
    
    Co-authored-by: Richard C. Davis <richard@open.gov.sg>
    Co-authored-by: Alexander Lee <alexander@open.gov.sg>

---
+++ services/identity/DeploymentClient.ts
@@ -0,0 +1,90 @@
+import {
+  AmplifyClient,
+  CreateAppCommand,
+  CreateAppCommandInput,
+  CreateAppCommandOutput,
+  CreateBranchCommand,
+  CreateBranchCommandInput,
+  CreateBranchCommandOutput,
+  Stage,
+} from "@aws-sdk/client-amplify"
+import { errAsync, ResultAsync } from "neverthrow"
+
+import { AmplifyError } from "@root/types/index"
+
+const { AWS_REGION, SYSTEM_GITHUB_TOKEN } = process.env
+
+const AMPLIFY_BUILD_SPEC = `
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - bundle install
+    build:
+      commands:
+        - curl https://raw.githubusercontent.com/opengovsg/isomer-build/amplify/build.sh | bash
+  artifacts:
+    baseDirectory: _site
+    files:
+      - '**/*'
+  cache:
+    paths: []
+`
+
+const wrap = (promise: Promise<unknown>) =>
+  ResultAsync.fromPromise(
+    promise,
+    (e) => new AmplifyError(`Publish to Amplify failed: ${e}`)
+  )
+
+class DeploymentClient {
+  private readonly amplifyClient: InstanceType<typeof AmplifyClient>
+
+  constructor() {
+    this.amplifyClient = new AmplifyClient({
+      region: AWS_REGION,
+    })
+  }
+
+  sendCreateApp = (options: CreateAppCommandInput) =>
+    wrap(this.amplifyClient.send(new CreateAppCommand(options))) as ResultAsync<
+      CreateAppCommandOutput,
+      AmplifyError
+    >
+
+  sendCreateBranch = (options: CreateBranchCommandInput) =>
+    wrap(
+      this.amplifyClient.send(new CreateBranchCommand(options))
+    ) as ResultAsync<CreateBranchCommandOutput, AmplifyError>
+
+  generateCreateAppInput = (
+    repoName: string,
+    repoUrl: string
+  ): CreateAppCommandInput => ({
+    name: repoName,
+    accessToken: SYSTEM_GITHUB_TOKEN,
+    repository: repoUrl,
+    buildSpec: AMPLIFY_BUILD_SPEC,
+    environmentVariables: {
+      JEKYLL_ENV: "development",
+    },
+    customRules: [{ source: "/<*>", target: "/404.html", status: "404-200" }],
+  })
+
+  generateCreateBranchInput = (
+    appId: string,
+    branchName: "master" | "staging"
+  ): CreateBranchCommandInput => ({
+    appId,
+    framework: "Jekyll",
+    branchName,
+    stage: branchName === "master" ? Stage.PRODUCTION : Stage.DEVELOPMENT,
+    enableAutoBuild: true,
+    environmentVariables: {
+      JEKYLL_ENV: branchName === "master" ? "production" : "staging",
+    },
+  })
+}
+
+export default DeploymentClient

710a60936a66c4a7809c2408895391c4cec6b9d8
Alexander Lee <alexander@open.gov.sg>
2022-07-21 10:01:54 +0800

    Feat: add SessionData class

---
+++ src/classes/SessionData.ts
@@ -0,0 +1,80 @@
+export interface SessionDataProps {
+  githubId?: string
+  accessToken?: string
+  isomerUserId: string
+  email?: string
+}
+
+class SessionData {
+  private readonly githubId?: SessionDataProps["githubId"]
+
+  private readonly accessToken?: SessionDataProps["accessToken"]
+
+  private readonly isomerUserId: SessionDataProps["isomerUserId"]
+
+  private readonly email?: SessionDataProps["email"]
+
+  private readonly isEmailUser: boolean
+
+  private currentCommitSha?: string
+
+  private treeSha?: string
+
+  constructor({
+    githubId,
+    accessToken,
+    isomerUserId,
+    email,
+  }: SessionDataProps) {
+    this.githubId = githubId
+    this.accessToken = accessToken
+    this.isomerUserId = isomerUserId
+    this.email = email
+    this.isEmailUser = !githubId
+  }
+
+  getId() {
+    if (this.isEmailUser) return this.isomerUserId
+    return this.githubId
+  }
+
+  getGithubId() {
+    return this.githubId
+  }
+
+  getAccessToken() {
+    return this.accessToken
+  }
+
+  getIsomerUserId() {
+    return this.isomerUserId
+  }
+
+  getEmail() {
+    return this.email
+  }
+
+  getIsEmailUser() {
+    return this.isEmailUser
+  }
+
+  addGithubState({
+    currentCommitSha,
+    treeSha,
+  }: {
+    currentCommitSha: string
+    treeSha: string
+  }) {
+    this.currentCommitSha = currentCommitSha
+    this.treeSha = treeSha
+  }
+
+  getGithubState() {
+    return {
+      currentCommitSha: this.currentCommitSha,
+      treeSha: this.treeSha,
+    }
+  }
+}
+
+export default SessionData

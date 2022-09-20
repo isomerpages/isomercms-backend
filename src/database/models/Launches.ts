286e795e3042af37397b502f5a2de3e5404b92f4
Kishore <e0323826@u.nus.edu>
2022-09-20 16:14:41 +0800

    feat(launches table): migrations + table for launches

---
+++ src/database/models/Launches.ts
@@ -0,0 +1,61 @@
+import {
+  DataType,
+  Column,
+  Model,
+  Table,
+  CreatedAt,
+  UpdatedAt,
+  ForeignKey,
+} from "sequelize-typescript"
+
+import { Site } from "@database/models/Site"
+import { User } from "@database/models/User"
+
+@Table({ tableName: "launches", paranoid: true })
+export class Launches extends Model {
+  @Column({
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+    type: DataType.BIGINT,
+  })
+  id!: number
+
+  @ForeignKey(() => User)
+  @Column
+  userId!: string
+
+  @ForeignKey(() => Site)
+  @Column
+  siteId!: string
+
+  @Column({
+    allowNull: false,
+    type: DataType.TEXT,
+  })
+  primaryDomainSource!: string
+
+  @Column({
+    allowNull: false,
+    type: DataType.TEXT,
+  })
+  primaryDomainTarget!: string
+
+  @Column({
+    allowNull: false,
+    type: DataType.TEXT,
+  })
+  domainValidationSource!: string
+
+  @Column({
+    allowNull: false,
+    type: DataType.TEXT,
+  })
+  domainValidationTarget!: string
+
+  @CreatedAt
+  createdAt!: Date
+
+  @UpdatedAt
+  updatedAt!: Date
+}

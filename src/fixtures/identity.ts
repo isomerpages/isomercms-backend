22b28a7beb04df45570657da5d2a218fa8f665e2
Preston Lim <prestonlimlianjie@gmail.com>
2022-09-23 12:52:00 +0800

    chore: add types for collaborator mocks

--- src/fixtures/identity.ts
+++ src/fixtures/identity.ts
@@ -1,3 +1,7 @@
+import { Attributes } from "sequelize/types"
+
+import { User, SiteMember } from "@database/models"
+
 export const mockRecipient = "hello@world.com"
 export const mockSubject = "mock subject"
 export const mockBody = "somebody"
@@ -19,74 +23,86 @@ export const mockBearerTokenHeaders = {
   },
 }
 
-const mockCollaboratorContributor1 = {
-  id: "1",
+const mockCollaboratorContributor1: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 1,
   email: "test1@test.gov.sg",
   githubId: "test1",
   contactNumber: "12331231",
-  lastLoggedIn: "2022-07-30T07:41:09.661Z",
-  createdAt: "2022-04-04T07:25:41.013Z",
-  updatedAt: "2022-07-30T07:41:09.662Z",
-  deletedAt: null,
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
   SiteMember: {
-    userId: "1",
+    userId: 1,
     siteId: "16",
     role: "CONTRIBUTOR",
-    createdAt: "2022-07-29T03:50:49.145Z",
-    updatedAt: "2022-07-29T03:50:49.145Z",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
   },
+  sites: [],
 }
 
-const mockCollaboratorAdmin1 = {
-  id: "2",
+const mockCollaboratorAdmin1: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 2,
   email: "test2@test.gov.sg",
   githubId: "test2",
   contactNumber: "12331232",
-  lastLoggedIn: "2022-07-30T07:41:09.661Z",
-  createdAt: "2022-04-04T07:25:41.013Z",
-  updatedAt: "2022-07-30T07:41:09.662Z",
-  deletedAt: null,
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
   SiteMember: {
-    userId: "2",
+    userId: 2,
     siteId: "16",
     role: "ADMIN",
-    createdAt: "2022-07-29T03:50:49.145Z",
-    updatedAt: "2022-07-29T03:50:49.145Z",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
   },
+  sites: [],
 }
-const mockCollaboratorAdmin2 = {
-  id: "3",
+const mockCollaboratorAdmin2: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 3,
   email: "test3@test.gov.sg",
   githubId: "test3",
   contactNumber: "12331233",
-  lastLoggedIn: "2022-06-30T07:41:09.661Z",
-  createdAt: "2022-04-04T07:25:41.013Z",
-  updatedAt: "2022-07-30T07:41:09.662Z",
-  deletedAt: null,
+  lastLoggedIn: new Date("2022-06-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
   SiteMember: {
-    userId: "3",
+    userId: 3,
     siteId: "16",
     role: "ADMIN",
-    createdAt: "2022-07-29T03:50:49.145Z",
-    updatedAt: "2022-07-29T03:50:49.145Z",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
   },
+  sites: [],
 }
-const mockCollaboratorContributor2 = {
-  id: "4",
+const mockCollaboratorContributor2: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 4,
   email: "test4@test.gov.sg",
   githubId: "test4",
   contactNumber: "12331234",
-  lastLoggedIn: "2022-07-30T07:41:09.661Z",
-  createdAt: "2022-04-04T07:25:41.013Z",
-  updatedAt: "2022-07-30T07:41:09.662Z",
-  deletedAt: null,
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
   SiteMember: {
-    userId: "4",
+    userId: 4,
     siteId: "16",
     role: "CONTRIBUTOR",
-    createdAt: "2022-07-29T03:50:49.145Z",
-    updatedAt: "2022-07-29T03:50:49.145Z",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
   },
+  sites: [],
 }
 
 export const unsortedMockCollaboratorsList = [

d42327e207de3609ec1b3a03fd900bfa78185ddd
Kishore <e0323826@u.nus.edu>
2022-09-29 09:30:58 +0800

    fix(redirections table): fix typo in migrations during table drop

--- src/database/migrations/20220920084207-create-redirects.js
+++ src/database/migrations/20220920084207-create-redirects.js
@@ -44,6 +44,6 @@ module.exports = {
     })
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("redirects")
+    await queryInterface.dropTable("redirections")
   },
 }

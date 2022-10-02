7ebe184c527eece4f1bc0bcf70b0d6ea086bfd33
Preston Lim <prestonlimlianjie@gmail.com>
2022-10-02 23:57:47 +0800

    feat: wire it up with serverless.yml

---
+++ handler.ts
@@ -0,0 +1,4 @@
+// Handler file for serverless
+// TODO: Fix directory alias issues with serverless typescript
+export { hello } from "./src/serverlessfns/hello"
+export { hithere } from "./src/serverlessfns/hithere"

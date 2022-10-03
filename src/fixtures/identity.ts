2397637fda9b387fcbff55ed11fdf5e687b0f8ff
Preston Lim <prestonlimlianjie@gmail.com>
2022-10-04 00:42:21 +0800

    test: add tests for CollaboratorsService

--- src/fixtures/identity.ts
+++ src/fixtures/identity.ts
@@ -1,3 +1,7 @@
+import { Attributes } from "sequelize/types"
+
+import { User, SiteMember } from "@database/models"
+
 export const mockRecipient = "hello@world.com"
 export const mockSubject = "mock subject"
 export const mockBody = "somebody"
@@ -17,4 +21,120 @@ export const mockBearerTokenHeaders = {
   headers: {
     Authorization: `Bearer ${process.env.POSTMAN_API_KEY}`,
   },
+}
+
+const mockCollaboratorContributor1: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 1,
+  email: "test1@test.gov.sg",
+  githubId: "test1",
+  contactNumber: "12331231",
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
+  SiteMember: {
+    userId: 1,
+    siteId: "16",
+    role: "CONTRIBUTOR",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
+  },
+  sites: [],
+}
+
+const mockCollaboratorAdmin1: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 2,
+  email: "test2@test.gov.sg",
+  githubId: "test2",
+  contactNumber: "12331232",
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
+  SiteMember: {
+    userId: 2,
+    siteId: "16",
+    role: "ADMIN",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
+  },
+  sites: [],
+}
+const mockCollaboratorAdmin2: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 3,
+  email: "test3@test.gov.sg",
+  githubId: "test3",
+  contactNumber: "12331233",
+  lastLoggedIn: new Date("2022-06-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
+  SiteMember: {
+    userId: 3,
+    siteId: "16",
+    role: "ADMIN",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
+  },
+  sites: [],
+}
+const mockCollaboratorContributor2: Attributes<User> & {
+  SiteMember: Attributes<SiteMember>
+} = {
+  id: 4,
+  email: "test4@test.gov.sg",
+  githubId: "test4",
+  contactNumber: "12331234",
+  lastLoggedIn: new Date("2022-07-30T07:41:09.661Z"),
+  createdAt: new Date("2022-04-04T07:25:41.013Z"),
+  updatedAt: new Date("2022-07-30T07:41:09.662Z"),
+  deletedAt: undefined,
+  SiteMember: {
+    userId: 4,
+    siteId: "16",
+    role: "CONTRIBUTOR",
+    createdAt: new Date("2022-07-29T03:50:49.145Z"),
+    updatedAt: new Date("2022-07-29T03:50:49.145Z"),
+  },
+  sites: [],
+}
+
+export const unsortedMockCollaboratorsList = [
+  mockCollaboratorContributor1,
+  mockCollaboratorAdmin1,
+  mockCollaboratorAdmin2,
+  mockCollaboratorContributor2,
+]
+
+export const expectedSortedMockCollaboratorsList = [
+  mockCollaboratorAdmin1,
+  mockCollaboratorAdmin2,
+  mockCollaboratorContributor1,
+  mockCollaboratorContributor2,
+]
+
+export const mockSiteOrmResponseWithAllCollaborators = {
+  id: 1,
+  name: "",
+  site_members: unsortedMockCollaboratorsList,
+}
+export const mockSiteOrmResponseWithOneAdminCollaborator = {
+  id: 1,
+  name: "",
+  site_members: [mockCollaboratorAdmin1],
+}
+export const mockSiteOrmResponseWithOneContributorCollaborator = {
+  id: 1,
+  name: "",
+  site_members: [mockCollaboratorContributor2],
+}
+export const mockSiteOrmResponseWithNoCollaborators = {
+  id: 1,
+  site_members: "",
 }

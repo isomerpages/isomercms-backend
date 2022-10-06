80746f6db962a99877d5d93817403f836497c5c2
Kishore <e0323826@u.nus.edu>
2022-10-06 11:24:17 +0800

    feat(site launch): [wip] updating db after successful launch

--- src/services/identity/QueueClient.ts
+++ src/services/identity/QueueClient.ts
@@ -113,7 +113,7 @@ export default class QueueClient {
               this.createDeleteMessageParams(receiptHandle),
               (err) => {
                 if (err) {
-                  logger.error(err, err.stack)
+                  logger.error(err)
                   throw err
                 }
               }

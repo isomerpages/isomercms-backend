7ec01c5ad571739d81bcc62dc67b6d631b28ae03
Kishore <e0323826@u.nus.edu>
2022-10-07 15:46:22 +0800

    refactor(QueueClient): remove hardcoded queue url

--- src/services/identity/QueueClient.ts
+++ src/services/identity/QueueClient.ts
@@ -38,7 +38,7 @@ export default class QueueClient {
 
   receiveMessage = async (): Promise<SQS.MessageList> => {
     const params: SQS.ReceiveMessageRequest = {
-      QueueUrl: this.incomingQueueUrl,
+      QueueUrl: this.outgoingQueueUrl,
       AttributeNames: ["All"],
       MaxNumberOfMessages: 10,
     }

d318cabd2cdd00d48a82dec7b4f5b6bea253108e
Kishore <e0323826@u.nus.edu>
2022-10-10 16:27:38 +0800

    refactor(queue service): change code style to prevent inner functions

--- src/services/infra/InfraService.ts
+++ src/services/infra/InfraService.ts
@@ -364,8 +364,8 @@ export default class InfraService {
     setInterval(this.siteUpdate, 30000)
   }
 
-  pollQueue = async () => {
-    const siteUpdate = async () => {
+  siteUpdate = async () => {
+    try {
       const messages = await this.queueService.pollMessages()
       if (messages) {
         messages.forEach(async (message) => {
@@ -380,13 +380,12 @@ export default class InfraService {
           }
         })
       }
+    } catch (error) {
+      logger.error(error)
     }
+  }
 
-    try {
-      setInterval(siteUpdate, 6000)
-    } catch (err) {
-      logger.error(err)
-      this.pollQueue() // fault tolerence - poller should not stop polling
-    }
+  pollQueue = async () => {
+    setInterval(this.siteUpdate, 6000)
   }
 }

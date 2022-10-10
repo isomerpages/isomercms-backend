5c30c4d63cf3a4cbd08dae0bf6ae390bb7b18904
Kishore <e0323826@u.nus.edu>
2022-10-10 23:52:47 +0800

    feat(site launch): ability to update redirections table

--- src/services/infra/InfraService.ts
+++ src/services/infra/InfraService.ts
@@ -275,6 +275,7 @@ export default class InfraService {
       const redirectionDomainList = dnsInfo.domainAssociation?.subDomains?.filter(
         (subDomain) => subDomain.subDomainSetting?.prefix
       )
+      const redirectionDomainSource = `${redirectionDomainList?.[0].subDomainSetting?.prefix}.${primaryDomain}`
 
       /**
        * Amplify only stores the prefix.
@@ -290,6 +291,7 @@ export default class InfraService {
         primaryDomainTarget,
         domainValidationSource,
         domainValidationTarget,
+        redirectionDomainSource,
       }
 
       if (redirectionDomainList?.length) {

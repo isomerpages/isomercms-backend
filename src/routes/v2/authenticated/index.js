cd2dd6c746d5816f08b2783a1c5c34689f7b861f
seaerchin <jiachin@open.gov.sg>
2022-10-11 19:08:25 +0800

    refactor(authenticated): shift review router dpes to init function

--- src/routes/v2/authenticated/index.js
+++ src/routes/v2/authenticated/index.js
@@ -1,3 +1,5 @@
+import { attachSiteHandler } from "@root/middleware"
+
 const express = require("express")
 
 const {
@@ -36,6 +38,11 @@ const getAuthenticatedSubrouter = ({
   authenticatedSubrouter.use(
     "/sites/:siteName/collaborators",
     collaboratorsRouter.getRouter()
+  )
+  sitesV2Router.use(
+    "/:siteName/review",
+    attachSiteHandler,
+    reviewRouter.getRouter()
   )
   authenticatedSubrouter.use("/sites", sitesV2Router.getRouter())
   authenticatedSubrouter.use("/user", usersRouter.getRouter())

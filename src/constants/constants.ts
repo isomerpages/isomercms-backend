74098cbddb5d703349dcdc20af3f9ab804abd7d9
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2022-10-12 13:24:21 +0800

    ref(services): migrate SitesService to TypeScript (#512)
    * ref(fixtures): convert repoInfo to typescript
    
    * ref(services): migrate SitesService to typescript
    
    * tests: update unit and integration tests for SitesService
    
    * ref(sites): migrate sites router to typescript
    
    * fix: revert back to using SessionData
    
    * fix: remove use of Bluebird and unused getSiteToken function
    
    * fix: use more accurate type
    
    * chore: remove unused variable
    
    * refactor(tests): migrate generic axios instance to __mocks__
    
    * tests: use mockAxios directly instead of preparing an instance

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -18,3 +18,27 @@ export enum CollaboratorRoles {
 export const E2E_ISOMER_ID = "-1"
 export const E2E_TEST_EMAIL = "test@e2e"
 export const E2E_TEST_CONTACT = "12345678"
+
+export const GH_MAX_REPO_COUNT = 100
+export const ISOMERPAGES_REPO_PAGE_COUNT =
+  (process.env.ISOMERPAGES_REPO_PAGE_COUNT &&
+    parseInt(process.env.ISOMERPAGES_REPO_PAGE_COUNT, 10)) ||
+  3
+export const ISOMER_GITHUB_ORG_NAME = process.env.GITHUB_ORG_NAME
+export const ISOMER_ADMIN_REPOS = [
+  "isomercms-backend",
+  "isomercms-frontend",
+  "isomer-redirection",
+  "isomerpages-template",
+  "isomer-conversion-scripts",
+  "isomer-wysiwyg",
+  "isomer-slackbot",
+  "isomer-tooling",
+  "generate-site",
+  "travisci-scripts",
+  "recommender-train",
+  "editor",
+  "ci-test",
+  "infra",
+  "markdown-helper",
+]

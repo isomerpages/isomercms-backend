c6c354c42b6fdcefaa32960879411734bbd6e7e7
seaerchin <44049504+seaerchin@users.noreply.github.com>
2022-10-13 12:45:08 +0800

    feat(ut): rr views (#530)
    * feat: add model and migration for review_request_views table
    
    * chore: rename migration
    
    * fix: add missing association with ReviewRequest model
    
    * fix: revert adding association in ReviewRequest model
    
    Co-authored-by: Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>

---
+++ src/database/models/ReviewRequestView.ts
@@ -0,0 +1,53 @@
+import {
+  ForeignKey,
+  DataType,
+  Column,
+  Model,
+  Table,
+  BelongsTo,
+  PrimaryKey,
+} from "sequelize-typescript"
+
+import { ReviewRequest } from "@database/models/ReviewRequest"
+import { Site } from "@database/models/Site"
+import { User } from "@database/models/User"
+
+@Table({ tableName: "review_request_views" })
+// eslint-disable-next-line import/prefer-default-export
+export class ReviewRequestView extends Model {
+  @ForeignKey(() => ReviewRequest)
+  @PrimaryKey
+  reviewRequestId!: number
+
+  @BelongsTo(() => ReviewRequest, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  reviewRequest!: ReviewRequest
+
+  @ForeignKey(() => Site)
+  @PrimaryKey
+  siteId!: number
+
+  @BelongsTo(() => Site, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  site!: Site
+
+  @ForeignKey(() => User)
+  @PrimaryKey
+  userId!: number
+
+  @BelongsTo(() => User, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  user!: User
+
+  @Column({
+    allowNull: true,
+    type: DataType.DATE,
+  })
+  lastViewedAt!: Date
+}

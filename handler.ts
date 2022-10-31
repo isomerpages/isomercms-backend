86e72a5ca5c42efd5ea114dd1ab1834edca6801f
Kishore <e0323826@u.nus.edu>
2022-10-31 12:06:06 +0800

    fix(serverless deployment): created new npm project within backend
    This is to prevent the loading of all node modules for the site launch
    microservice

--- handler.ts
+++
@@ -1,8 +0,0 @@
-// Handler file for serverless
-// TODO: Fix directory alias issues with serverless typescript
-export { generalDomainValidation } from "./microservices/site-launch/lambda-functions/general-domain-validation"
-export { primaryDomainValidation } from "./microservices/site-launch/lambda-functions/primary-domain-validation"
-export { successNotification } from "./microservices/site-launch/lambda-functions/success-notification"
-export { failureNotification } from "./microservices/site-launch/lambda-functions/failure-notification"
-export { stepFunctionsTrigger } from "./microservices/site-launch/lambda-functions/step-functions-trigger"
-export { redirectionDomainValidation } from "./microservices/site-launch/lambda-functions/redirection-domain-validation"

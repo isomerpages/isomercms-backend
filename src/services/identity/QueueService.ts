cc53fa9284e6aa6065f9c0d59847d3ded65fa37b
Kishore <e0323826@u.nus.edu>
2022-11-02 17:02:23 +0800

    fix(queue service ): parse the response from lambdas back properly

--- src/services/identity/QueueService.ts
+++ src/services/identity/QueueService.ts
@@ -40,28 +40,51 @@ export default class QueueService {
     try {
       messages?.forEach((message) => {
         console.log(JSON.stringify(message))
+
+        if (!message.Body) return
+
+        let parsedMessage
+
         /**
-         * Message from SQS wraps the message body with a 'Records'. eg.
-         * {
-         *   ...
-         *   Body {
-         *      Records: [{
-         *        ...
-         *         body: <message Body Content>
-         *        ...
-         *      }]
-         *   }
-         *  ...
-         * }
-         * Thus, to get the final message in `body`, there is a need to have multiple
-         * layers of JSON.parse().
+         * The shape of messages are different as they can come from two different lambdas (success and failure)
+         * The code below parses the it according to the type of message it comes from
          */
 
-        if (message.Body) {
-          messageBodies.push(
-            JSON.parse(JSON.parse(message.Body).Records[0].body)
-          )
+        // from error lambda
+        if (JSON.parse(message.Body).Records) {
+          /**
+           * Message from SQS wraps the message body with a 'Records'. eg.
+           * {
+           *   ...
+           *   Body {
+           *      Records: [{
+           *        ...
+           *         body: <message Body Content>
+           *        ...
+           *      }]
+           *   }
+           *  ...
+           * }
+           * Thus, to get the final message in `body`, there is a need to have multiple
+           * layers of JSON.parse().
+           */
+
+          parsedMessage = JSON.parse(JSON.parse(message.Body).Records[0].body)
+        } else {
+          /**
+           * Message shape is as such:
+           * {
+           *  ...
+           *  "Body":  <messagebody>
+           * }
+           *
+           */
+
+          parsedMessage = JSON.parse(message.Body)
+          console.log(parsedMessage)
         }
+
+        messageBodies.push(parsedMessage)
       })
     } catch (error) {
       logger.error(`error while parsing: ${messages}`)

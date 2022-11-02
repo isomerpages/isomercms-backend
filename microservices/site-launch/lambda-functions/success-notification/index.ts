4db3a035c0c75cc3df73a0b419b1f91d13220a4c
Kishore <e0323826@u.nus.edu>
2022-11-02 20:23:01 +0800

    fix(success noti lambda): fix output params shape

--- microservices/site-launch/lambda-functions/success-notification/index.ts
+++ microservices/site-launch/lambda-functions/success-notification/index.ts
@@ -1,26 +1,67 @@
+/* eslint-disable import/prefer-default-export */
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
 import { SQS } from "aws-sdk"
 
 const { INCOMING_QUEUE_URL, AWS_REGION } = process.env
 
+export enum SITE_LAUNCH_LAMBDA_STATUS {
+  SUCCESS = "success",
+  FAILURE = "failure",
+}
+
+export enum SITE_LAUNCH_LAMBDA_TYPE {
+  GENERAL_DOMAIN_VALIDATION = "general-domain-validation",
+  PRIMARY_DOMAIN_VALIDATION = "primary-domain-validation",
+  REDIRECTION_DOMAIN_VALIDATION = "redirection-domain-validation",
+}
+
+export interface MessageBody {
+  repoName: string
+  appId: string
+  primaryDomainSource: string
+  primaryDomainTarget: string
+  domainValidationSource: string
+  domainValidationTarget: string
+  requestorEmail: string
+  agencyEmail: string
+  githubRedirectionUrl?: string
+  redirectionDomain?: [
+    {
+      source: string
+      target: string
+      type: string
+    }
+  ]
+  success?: boolean
+  siteLaunchError?: string
+}
+export interface inputParams {
+  lambdaType: SITE_LAUNCH_LAMBDA_TYPE
+  status: SITE_LAUNCH_LAMBDA_STATUS
+  message: MessageBody
+}
+
 export const successNotification = async (
-  event: APIGatewayProxyEvent
+  event: inputParams[]
 ): Promise<APIGatewayProxyResult> => {
   console.log(event)
+
+  /**
+   * In success, all three lambdas will be return a success.
+   * Therefore, to get the message shape, we can just take the message body from one lambda.
+   */
+
+  const messageBody = event[0].message
 
   const sqs = new SQS({ region: AWS_REGION })
   const messageParams = {
     QueueUrl: INCOMING_QUEUE_URL || "",
-    MessageBody: JSON.stringify({ ...event, success: true }),
+    MessageBody: JSON.stringify({ ...messageBody, success: true }),
   }
 
-  sqs.sendMessage(messageParams, (err, data) => {
-    if (err) {
-      console.log("Error", err)
-    } else {
-      console.log("Success", data.MessageId)
-    }
-  })
+  const result = await sqs.sendMessage(messageParams).promise()
+
+  console.log(result)
 
   return {
     statusCode: 200,

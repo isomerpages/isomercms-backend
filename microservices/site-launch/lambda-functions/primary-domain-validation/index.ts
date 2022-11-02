94563f2473496b7af663bd1274cdd124c1463549
Kishore <e0323826@u.nus.edu>
2022-11-02 20:19:20 +0800

    refactor(primary domain validation lambda ): standarise message body for lambda

--- microservices/site-launch/lambda-functions/primary-domain-validation/index.ts
+++ microservices/site-launch/lambda-functions/primary-domain-validation/index.ts
@@ -10,27 +10,42 @@ import type {
   GetDomainAssociationCommandOutput,
 } from "@aws-sdk/client-amplify"
 
+
 import logger from "../../shared/logger"
 import {
   SITE_LAUNCH_LAMBDA_TYPE,
   SITE_LAUNCH_LAMBDA_STATUS,
 } from "../../shared/types" // TODO: Change to aliased imports
 
-interface PrimaryDomainValidationLambdaParams {
+export interface MessageBody {
+  repoName: string
   appId: string
   primaryDomainSource: string
   primaryDomainTarget: string
+  domainValidationSource: string
+  domainValidationTarget: string
+  requestorEmail: string
+  agencyEmail: string
+  githubRedirectionUrl?: string
+  redirectionDomain?: [
+    {
+      source: string
+      target: string
+      type: string
+    }
+  ]
+  success?: boolean
+  siteLaunchError?: string
 }
 
 interface PrimaryDomainValidationLambdaResponse {
   lambdaType: SITE_LAUNCH_LAMBDA_TYPE
   status: SITE_LAUNCH_LAMBDA_STATUS
-  appId: string
-  primaryDomain: string
+  message: MessageBody
 }
 
 export const primaryDomainValidation = async (
-  event: PrimaryDomainValidationLambdaParams
+  event: MessageBody
 ): Promise<PrimaryDomainValidationLambdaResponse> => {
   logger.info(event)
 
@@ -68,23 +83,23 @@ export const primaryDomainValidation = async (
       `Amplify app with id ${appId} and domain ${primaryDomainSource} successfully completed primary domain validation step with status ${domainAssociationStatus}`
     )
 
-    // Check if the primary DNS record was set correctly. This is necessary because Amplify doesn't actually check if the
-    // primary domain record has been pointed correctly.
-    const cnameRecords = await dns.promises.resolveCname(primaryDomainSource)
-    if (!cnameRecords.includes(cloudfrontDomain)) {
-      throw new Error(
-        `Website administrator has not set up the primary domain ${primaryDomainSource} to point to the correct Cloudfront domain name`
-      )
-    }
+    // todo figure out how to load 'dns' module and then uncomment the codes below
+    // // Check if the primary DNS record was set correctly. This is necessary because Amplify doesn't actually check if the
+    // // primary domain record has been pointed correctly.
+    // const cnameRecords = await dns.promises.resolveCname(primaryDomainSource)
+    // if (!cnameRecords.includes(cloudfrontDomain)) {
+    //   throw new Error(
+    //     `Website administrator has not set up the primary domain ${primaryDomainSource} to point to the correct Cloudfront domain name`
+    //   )
+    // }
 
-    console.log(
-      `Website administrator has successfully set up the primary domain ${primaryDomainSource} to point to the correct Cloudfront domain name`
-    )
+    // console.log(
+    //   `Website administrator has successfully set up the primary domain ${primaryDomainSource} to point to the correct Cloudfront domain name`
+    // )
     return {
       lambdaType: SITE_LAUNCH_LAMBDA_TYPE.PRIMARY_DOMAIN_VALIDATION,
       status: SITE_LAUNCH_LAMBDA_STATUS.SUCCESS,
-      appId,
-      primaryDomain: primaryDomainSource,
+      message: event,
     }
   } catch (error) {
     logger.error(error)

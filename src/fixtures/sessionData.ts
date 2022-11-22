d9c6159e52103b90124ad06ec2ca24f81c07902b
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2022-11-22 13:24:24 +0800

    ref(fixtures): allow generating router for different user types (#574)
    * ref(fixtures): allow generating router for different user types
    
    * fix: switch to specifying the siteName directly
    
    * feat: allow adding a default user session

--- src/fixtures/sessionData.ts
+++ src/fixtures/sessionData.ts
@@ -2,6 +2,17 @@ import GithubSessionData from "@root/classes/GithubSessionData"
 import UserSessionData from "@root/classes/UserSessionData"
 import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
 
+import {
+  MOCK_USER_EMAIL_ONE,
+  MOCK_USER_EMAIL_TWO,
+  MOCK_USER_EMAIL_THREE,
+  MOCK_USER_EMAIL_FOUR,
+  MOCK_USER_ID_ONE,
+  MOCK_USER_ID_TWO,
+  MOCK_USER_ID_THREE,
+  MOCK_USER_ID_FOUR,
+} from "./users"
+
 export const mockAccessToken = "mockAccessToken"
 export const mockGithubId = "mockGithubId"
 export const mockIsomerUserId = "1"
@@ -42,4 +53,21 @@ export const mockSessionDataEmailUserWithSite = new UserWithSiteSessionData({
   isomerUserId: mockIsomerUserId,
   email: mockEmail,
   siteName: mockSiteName,
+})
+
+export const MOCK_USER_SESSION_DATA_ONE = new UserSessionData({
+  isomerUserId: String(MOCK_USER_ID_ONE),
+  email: MOCK_USER_EMAIL_ONE,
+})
+export const MOCK_USER_SESSION_DATA_TWO = new UserSessionData({
+  isomerUserId: String(MOCK_USER_ID_TWO),
+  email: MOCK_USER_EMAIL_TWO,
+})
+export const MOCK_USER_SESSION_DATA_THREE = new UserSessionData({
+  isomerUserId: String(MOCK_USER_ID_THREE),
+  email: MOCK_USER_EMAIL_THREE,
+})
+export const MOCK_USER_SESSION_DATA_FOUR = new UserSessionData({
+  isomerUserId: String(MOCK_USER_ID_FOUR),
+  email: MOCK_USER_EMAIL_FOUR,
 })

b24f3456108c672d9eb30787adf310fd1bba00b3
Kishore <e0323826@u.nus.edu>
2022-12-05 16:41:28 +0800

    fix(site launch microservice): fix imports and simplify winston logger

--- microservices/site-launch/shared/logger.ts
+++ microservices/site-launch/shared/logger.ts
@@ -1,45 +1,17 @@
-import { CloudWatchLogsClientConfig } from "@aws-sdk/client-cloudwatch-logs"
+import { createLogger, format } from "winston"
+import WinstonCloudWatch from "winston-cloudwatch"
 
-import { CloudWatchLogs } from "aws-sdk"
-import winston from "winston"
-import TransportStream from "winston-transport"
-
-// taken from https://github.com/lazywithclass/winston-cloudwatch/blob/master/typescript/winston-cloudwatch.d.ts
-// Declare optional exports
-type LogObject = winston.LogEntry
-
-interface CloudwatchTransportOptions {
-  name?: string
-  cloudWatchLogs?: CloudWatchLogs
-  level?: string
-  ensureLogGroup?: boolean
-  logGroupName?: string | (() => string)
-  logStreamName?: string | (() => string)
-  awsAccessKeyId?: string
-  awsSecretKey?: string
-  awsRegion?: string
-  awsOptions?: CloudWatchLogsClientConfig
-  jsonMessage?: boolean
-  messageFormatter?: (logObject: LogObject) => string
-  uploadRate?: number
-  errorHandler?: (err: Error) => void
-  silent?: boolean
-  retentionInDays?: number
-}
-
-// Declare the default WinstonCloudwatch class
-declare class WinstonCloudwatch extends TransportStream {
-  constructor(options?: CloudwatchTransportOptions)
-}
-
-const logger = winston.add(
-  new WinstonCloudwatch({
-    name: "site-launch",
-    logGroupName: "site-launch-log-group",
-    logStreamName: "site-launch",
-    awsRegion: "ap-southeast-1",
-    level: "info",
-  })
-)
+export const logger = createLogger({
+  level: "debug",
+  format: format.json(),
+  transports: [
+    new WinstonCloudWatch({
+      level: "error",
+      logGroupName: "groupName",
+      logStreamName: "errors",
+      awsRegion: "eu-west-3",
+    }),
+  ],
+})
 
 export default logger

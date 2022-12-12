1f128d864d6a7b3fa2e89c41a0415e079f529551
seaerchin <jiachin@open.gov.sg>
2022-12-12 11:20:45 +0800

    chore(databaseerror): removed extra properties

--- src/errors/DatabaseError.ts
+++ src/errors/DatabaseError.ts
@@ -2,9 +2,6 @@ import { BaseIsomerError } from "./BaseError"
 
 export default class DatabaseError extends BaseIsomerError {
   constructor(message = "Unable to retrieve data from database") {
-    super()
-    Error.captureStackTrace(this, this.constructor)
-    this.name = this.constructor.name
-    this.message = message
+    super(500, message)
   }
 }

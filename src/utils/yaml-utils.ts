6962058620f19bb4e0c46c5acfb3a1ed6201fbaf
Preston Lim <prestonlimlianjie@gmail.com>
2022-12-19 15:30:43 +0800

    chore: specify return types in yaml helpers

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -1,8 +1,8 @@
 import DOMPurify from "isomorphic-dompurify"
 import yaml from "yaml"
 
-export const sanitizedYamlParse = (unparsedContent: string) =>
+export const sanitizedYamlParse = (unparsedContent: string): object =>
   yaml.parse(DOMPurify.sanitize(unparsedContent))
 
-export const sanitizedYamlStringify = (prestringifiedContent: object) =>
+export const sanitizedYamlStringify = (prestringifiedContent: object): string =>
   DOMPurify.sanitize(yaml.stringify(prestringifiedContent))

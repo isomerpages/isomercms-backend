b05cff5828cd4173e68b98f081a90f7ff2ad2eb3
Kishore <e0323826@u.nus.edu>
2022-12-27 19:00:36 +0800

    fix(logger): change region + name of log group

--- microservices/site-launch/shared/logger.ts
+++ microservices/site-launch/shared/logger.ts
@@ -1,15 +1,16 @@
 import { createLogger, format } from "winston"
 import WinstonCloudWatch from "winston-cloudwatch"
 
-export const logger = createLogger({
-  level: "debug",
+const logger = createLogger({
+  level: "info",
   format: format.json(),
   transports: [
     new WinstonCloudWatch({
-      level: "error",
-      logGroupName: "groupName",
+      level: "info",
+      logGroupName: "SiteLaunch",
       logStreamName: "errors",
-      awsRegion: "eu-west-3",
+      awsRegion: "ap-southeast-1",
+      name: "SiteLaunch_Microservice",
     }),
   ],
 })

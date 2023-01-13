50d21982cffa0db19e2fc4ec9f30a71f1f690135
Preston Lim <prestonlimlianjie@gmail.com>
2023-01-13 11:53:25 +0800

    fix: make sanitizeYamlParse return type more specific

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -1,8 +1,11 @@
 import DOMPurify from "isomorphic-dompurify"
 import yaml from "yaml"
 
-export const sanitizedYamlParse = (unparsedContent: string): object =>
-  yaml.parse(DOMPurify.sanitize(unparsedContent))
+// Note: `yaml.parse()` and `yaml.stringify()` should not be used anywhere
+// else in the codebase.
+export const sanitizedYamlParse = (
+  unparsedContent: string
+): Record<string, unknown> => yaml.parse(DOMPurify.sanitize(unparsedContent))
 
 export const sanitizedYamlStringify = (prestringifiedContent: object): string =>
   DOMPurify.sanitize(yaml.stringify(prestringifiedContent))

dcf262236c50e7b9f5337a165f1af4fb8058b416
seaerchin <jiachin@open.gov.sg>
2023-02-13 14:50:01 +0800

    refactor(ratelimiter): read from env var

--- src/services/utilServices/RateLimiter.ts
+++ src/services/utilServices/RateLimiter.ts
@@ -1,5 +1,7 @@
 import rateLimit from "express-rate-limit"
 
+const DEFAULT_AUTH_TOKEN_EXPIRY_MILLISECONDS = 900000
+
 // NOTE: Refer here for more information regarding the implementation:
 // https://github.com/express-rate-limit/express-rate-limit
 // Also, note that our production environment has 2 instances
@@ -9,7 +11,13 @@ import rateLimit from "express-rate-limit"
 // but not on the other, leading to inconsistent behaviour.
 // eslint-disable-next-line import/prefer-default-export
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs:
+    parseInt(
+      process.env.AUTH_TOKEN_EXPIRY_DURATION_IN_MILLISECONDS ||
+        DEFAULT_AUTH_TOKEN_EXPIRY_MILLISECONDS.toString(),
+      10
+    ) /
+    (1000 * 60),
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers

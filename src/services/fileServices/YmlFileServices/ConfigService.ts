75bd13dc12cfcc5dd86c6db183765416dbeb8cbb
seaerchin <jiachin@open.gov.sg>
2023-03-22 15:15:46 +0800

    chore(configService): add precondition for pathinfo

--- src/services/fileServices/YmlFileServices/ConfigService.ts
+++ src/services/fileServices/YmlFileServices/ConfigService.ts
@@ -8,27 +8,28 @@ import {
 } from "@root/types/configYml"
 import { PathInfo } from "@root/types/pages"
 import { Brand } from "@root/types/util"
-import { extractPathInfo } from "@root/utils/files"
 
 // eslint-disable-next-line import/prefer-default-export
 export class ConfigService {
-  isConfigFile = (fileName: string): Result<ConfigFileName, ConfigParseError> =>
+  isConfigFile = (
+    pathInfo: PathInfo
+  ): Result<ConfigFileName, ConfigParseError> =>
     // NOTE: A file can be a config file in a few scenarios:
     // 1. The file's extension is `.yml` (given by jekyll)
     // 2. The file's extension is `.html` + it's rooted in a custom folder
     // The second condition could be made more specific (resource room)
     // but we'd then have an additional dep + require a github api call
     // so we will rely on a less specific condition for now.
-    extractPathInfo(fileName).andThen((pathInfo) => {
+    {
       if (pathInfo.name.endsWith(".yml"))
         return ok(this.extractYmlConfigFileName(pathInfo))
 
       return this.isHtmlConfigFile(pathInfo).map((htmlPathInfo) =>
         this.extractHtmlConfigFileName(htmlPathInfo)
       )
-    })
+    }
 
-  extractYmlConfigFileName = ({
+  private extractYmlConfigFileName = ({
     name: rawName,
     path,
   }: PathInfo): YmlConfigFileName => {
@@ -40,7 +41,7 @@ export class ConfigService {
     }
   }
 
-  extractHtmlConfigFileName = ({
+  private extractHtmlConfigFileName = ({
     name: rawName,
     path,
   }: PathInfo): HtmlConfigFileName => {
@@ -53,7 +54,7 @@ export class ConfigService {
     }
   }
 
-  isHtmlConfigFile = (
+  private isHtmlConfigFile = (
     pathInfo: PathInfo
   ): Result<PathInfo, ConfigParseError> => {
     const isHtmlConfig =
@@ -68,7 +69,7 @@ export class ConfigService {
         .every((hasUnderscore) => !hasUnderscore)
 
     if (isHtmlConfig) return ok(pathInfo)
-    const rawPath = pathInfo.path.isOk() ? pathInfo.path.value : ""
+    const rawPath = pathInfo.path.unwrapOr([""]).join("/")
     return err(new ConfigParseError(`${rawPath}/${pathInfo.name}`))
   }
 }

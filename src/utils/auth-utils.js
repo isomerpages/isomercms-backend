2df7746d193a610d8c653f6cb3293c967505a8bb
Alexander Lee <alexander@open.gov.sg>
2023-03-27 11:24:02 +0800

    Fix: change isSecure to variable instead of func (#664)

--- src/utils/auth-utils.js
+++ src/utils/auth-utils.js
@@ -2,9 +2,7 @@ const { config } = require("@config/config")
 
 const NODE_ENV = config.get("env")
 
-function isSecure() {
-  return NODE_ENV !== "dev" && NODE_ENV !== "test"
-}
+const isSecure = NODE_ENV !== "dev" && NODE_ENV !== "test"
 
 module.exports = {
   isSecure,

de4c75635c3513752b02946ac50633397fc3df81
seaerchin <jiachin@open.gov.sg>
2023-04-05 12:20:34 +0800

    fix(utils): change order of ops and rec sanitization

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -1,12 +1,29 @@
+import _ from "lodash"
 import yaml from "yaml"
 
 import { sanitizer } from "@services/utilServices/Sanitizer"
+
+type YamlRecord = {
+  [key: string]: YamlRecord | string
+}
+
+const sanitizeYamlRecord = (yamlRec: YamlRecord): YamlRecord =>
+  _(yamlRec)
+    .mapValues((val) => {
+      if (typeof val === "string") return sanitizer.sanitize(val)
+      return sanitizeYamlRecord(val)
+    })
+    .value()
 
 // Note: `yaml.parse()` and `yaml.stringify()` should not be used anywhere
 // else in the codebase.
 export const sanitizedYamlParse = (
   unparsedContent: string
-): Record<string, unknown> => yaml.parse(sanitizer.sanitize(unparsedContent))
+): Record<string, unknown> => {
+  const parsedContent = yaml.parse(unparsedContent) as YamlRecord
+
+  return sanitizeYamlRecord(parsedContent)
+}
 
 export const sanitizedYamlStringify = (prestringifiedContent: object): string =>
   sanitizer.sanitize(yaml.stringify(prestringifiedContent))

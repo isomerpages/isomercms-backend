2654e3be7c9b702138b184b0791eb43b137cd2ac
seaerchin <jiachin@open.gov.sg>
2023-04-05 16:30:20 +0800

    fix(yaml): add array type

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -4,13 +4,16 @@ import yaml from "yaml"
 import { sanitizer } from "@services/utilServices/Sanitizer"
 
 type YamlRecord = {
-  [key: string]: YamlRecord | string
+  [key: string]: YamlRecord | string | YamlRecord[]
 }
+
+const isArr = (t: unknown): t is Array<unknown> => !!(t as unknown[]).length
 
 const sanitizeYamlRecord = (yamlRec: YamlRecord): YamlRecord =>
   _(yamlRec)
     .mapValues((val) => {
       if (typeof val === "string") return sanitizer.sanitize(val)
+      if (isArr(val)) return val.map(sanitizeYamlRecord)
       return sanitizeYamlRecord(val)
     })
     .value()
@@ -21,7 +24,6 @@ export const sanitizedYamlParse = (
   unparsedContent: string
 ): Record<string, unknown> => {
   const parsedContent = yaml.parse(unparsedContent) as YamlRecord
-
   return sanitizeYamlRecord(parsedContent)
 }
 

40da111867a56c8e294b6a4bd3d33e9675526714
seaerchin <44049504+seaerchin@users.noreply.github.com>
2023-04-06 15:09:23 +0800

    fix(utils): sanitize empty string + trim (#686)

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -8,10 +8,16 @@ export const sanitizedYamlParse = (
   unparsedContent: string
 ): Record<string, unknown> =>
   yaml.parse(unparsedContent, (key, value) =>
-    typeof value === "string" ? sanitizer.sanitize(value) : value
+    typeof value === "string" && !!value
+      ? // NOTE: We call `trim` here because post-sanitization,
+        // there could be an extra space.
+        // For example: `logo: path <script />`,
+        // which will be sanitized with a trailing space.
+        sanitizer.sanitize(value).trim()
+      : value
   )
 
 export const sanitizedYamlStringify = (prestringifiedContent: object): string =>
   yaml.stringify(prestringifiedContent, (key, value) =>
-    typeof value === "string" ? sanitizer.sanitize(value) : value
+    typeof value === "string" && !!value ? sanitizer.sanitize(value) : value
   )

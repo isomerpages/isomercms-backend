0dabe566c5aedec12291f5701714b56550990ad3
seaerchin <jiachin@open.gov.sg>
2023-04-19 17:05:52 +0800

    fix(markdown-utils): change sanitization order

--- src/utils/markdown-utils.js
+++ src/utils/markdown-utils.js
@@ -12,11 +12,17 @@ const getTrailingSlashWithPermalink = (permalink) =>
 
 const retrieveDataFromMarkdown = (fileContent) => {
   // eslint-disable-next-line no-unused-vars
-  const [unused, encodedFrontMatter, ...pageContent] = sanitizer
-    .sanitize(fileContent)
-    .split("---")
+  const [unused, encodedFrontMatter, ...pageContent] = fileContent.split("---")
+  // NOTE: We separate the sanitization into 2 steps.
+  // This is because DOMPurify does URL encoding when it detects html in the string.
+  // For example, `<b>&something</b>` will get escaped to `<b>&amp;something</b>`.
+  // To prevent this behaviour from affecting our frontmatter, we do the sanitization separately
+  // on the frontmatter and the content
   const frontMatter = sanitizedYamlParse(encodedFrontMatter)
-  return { frontMatter, pageContent: pageContent.join("---").trim() }
+  return {
+    frontMatter,
+    pageContent: sanitizer.sanitize(pageContent.join("---")).trim(),
+  }
 }
 
 const isResourceFileOrLink = (frontMatter) => {

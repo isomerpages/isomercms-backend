7e317a77d5238c6f9f1ce57bcf4055f4cd531275
Alexander Lee <alexander@open.gov.sg>
2023-05-17 15:34:00 +0800

    Refactor: move url retrieval into separate method

--- src/utils/media-utils.ts
+++ src/utils/media-utils.ts
@@ -8,8 +8,22 @@ import { getAccessToken } from "@root/utils/token-retrieval-utils"
 
 const GITHUB_ORG_NAME = config.get("github.orgName")
 
-export const isMediaFileOutput = (t: MediaFileOutput): t is MediaFileOutput =>
-  (t as MediaFileOutput).sha !== undefined
+const getEncodedFilePathAsUriComponent = (
+  siteName: string,
+  filePath: string,
+  accessToken?: string
+) => {
+  const isSvg = filePath.endsWith(".svg")
+  const encodedFilePath = filePath
+    .split("/")
+    .map((v) => encodeURIComponent(v))
+    .join("/")
+  return `https://${
+    accessToken ? `${accessToken}@` : ""
+  }raw.githubusercontent.com/${GITHUB_ORG_NAME}/${siteName}/staging/${encodedFilePath}${
+    isSvg ? "?sanitize=true" : ""
+  }`
+}
 
 export const getMediaFileInfo = async ({
   file,
@@ -18,11 +32,7 @@ export const getMediaFileInfo = async ({
   mediaType,
   isPrivate,
 }: MediaFileInput): Promise<MediaFileOutput> => {
-  const fileData = {
-    mediaUrl: `https://raw.githubusercontent.com/${GITHUB_ORG_NAME}/${siteName}/staging/${file.path
-      .split("/")
-      .map((v) => encodeURIComponent(v))
-      .join("/")}${file.path.endsWith(".svg") ? "?sanitize=true" : ""}`,
+  const baseFileData = {
     name: file.name,
     sha: file.sha,
     mediaPath: `${directoryName}/${file.name}`,
@@ -33,19 +43,29 @@ export const getMediaFileInfo = async ({
       // Generate data url
       const accessToken = await getAccessToken()
       // Accessing images in this way avoids token usage
-      const endpoint = `https://${accessToken}@raw.githubusercontent.com/${GITHUB_ORG_NAME}/${siteName}/staging/${file.path
-        .split("/")
-        .map((v) => encodeURIComponent(v))
-        .join("/")}${file.path.endsWith(".svg") ? "?sanitize=true" : ""}`
+      const endpoint = getEncodedFilePathAsUriComponent(
+        siteName,
+        file.path,
+        accessToken
+      )
       const resp = await axios.get(endpoint, { responseType: "arraybuffer" })
       const data = Buffer.from(resp.data, "binary").toString("base64")
-      const dataUrl = `data:${resp.headers["content-type"]};base64,${data}`
-      fileData.mediaUrl = dataUrl
+      const dataUri = `data:${resp.headers["content-type"]};base64,${data}`
+      return {
+        ...baseFileData,
+        mediaUrl: dataUri,
+      }
     } catch (err) {
       // If an error occurs while retrieving the image, we log but continue to return generic values
       logger.error(err)
-      fileData.mediaUrl = ""
+      return {
+        ...baseFileData,
+        mediaUrl: "",
+      }
     }
   }
-  return fileData
+  return {
+    ...baseFileData,
+    mediaUrl: getEncodedFilePathAsUriComponent(siteName, file.path),
+  }
 }

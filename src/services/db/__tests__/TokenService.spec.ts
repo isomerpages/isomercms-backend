769c0eeadc9926b48b9ce1d25835072a1cacba6e
caando <zhangjikun907475@gmail.com>
2023-05-29 14:33:03 +0800

    feat(token): add selectReservedToken and parseResponseTokenData tests

--- src/services/db/__tests__/TokenService.spec.ts
+++ src/services/db/__tests__/TokenService.spec.ts
@@ -1,6 +1,9 @@
 import { expect, jest } from "@jest/globals"
-import { ok } from "neverthrow"
+import { AxiosResponse } from "axios"
+import { ok, err } from "neverthrow"
 import { ModelStatic } from "sequelize"
+
+import TokenParsingError from "@errors/TokenParsingError"
 
 import { AccessToken } from "@database/models"
 import {
@@ -9,12 +12,11 @@ import {
   NoTokenData,
   NoResetTime,
   selectActiveToken,
+  selectReservedToken,
+  parseResponseTokenData,
+  GITHUB_TOKEN_REMAINING_HEADER,
+  GITHUB_TOKEN_RESET_HEADER,
 } from "@services/db/TokenService"
-
-const mockTokenDB = jest.mocked<ModelStatic<AccessToken>>(({
-  findAll: jest.fn(),
-  findOne: jest.fn(),
-} as unknown) as ModelStatic<AccessToken>)
 
 describe("Token Service", () => {
   beforeEach(() => {
@@ -26,19 +28,19 @@ describe("Token Service", () => {
       {
         id: 1,
         tokenString: "token_1",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       },
       {
         id: 2,
         tokenString: "token_2",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       },
       {
         id: 3,
         tokenString: "token_3",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       },
     ])
@@ -48,7 +50,7 @@ describe("Token Service", () => {
       const expected = ok({
         id: 1,
         tokenString: "token_1",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       })
       const input = mockActiveTokens()
@@ -70,7 +72,7 @@ describe("Token Service", () => {
       const expected = ok({
         id: 2,
         tokenString: "token_2",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: ok(earlyFutureResetTime),
       })
       const input = mockActiveTokens()
@@ -94,7 +96,7 @@ describe("Token Service", () => {
       const expected = ok({
         id: 1,
         tokenString: "token_1",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: ok(earlyFutureResetTime),
       })
       const input = mockActiveTokens()
@@ -116,7 +118,7 @@ describe("Token Service", () => {
       const expectedReturn = ok({
         id: 1,
         tokenString: "token_1",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       })
       const input = mockActiveTokens()
@@ -141,7 +143,7 @@ describe("Token Service", () => {
       const expectedReturn = ok({
         id: 3,
         tokenString: "token_3",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: ok(futureResetTime),
       })
       const input = mockActiveTokens()
@@ -164,7 +166,7 @@ describe("Token Service", () => {
       const expected = ok({
         id: 2,
         tokenString: "token_2",
-        remainingRequests: 5000,
+        remainingRequests: GITHUB_TOKEN_LIMIT,
         resetTime: NoResetTime,
       })
       const input = mockActiveTokens()
@@ -223,6 +225,236 @@ describe("Token Service", () => {
 
       // Act
       const actual = selectActiveToken(input)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe("selectReservedToken", () => {
+    let mockAccessTokenQuery: AccessToken | null = jest.mocked<AccessToken>(({
+      update: jest.fn(),
+      id: 1,
+      token: "new_token",
+      resetTime: null,
+      isReserved: true,
+    } as unknown) as AccessToken)
+
+    const mockTokenDB = jest.mocked<ModelStatic<AccessToken>>(({
+      findAll: jest.fn(),
+      findOne: jest.fn(async () => mockAccessTokenQuery),
+    } as unknown) as ModelStatic<AccessToken>)
+
+    jest.useFakeTimers()
+
+    it("should return the existing reserved token", async () => {
+      // Arrange
+      const existingToken = ok({
+        id: 2,
+        tokenString: "existing_token",
+        remainingRequests: GITHUB_TOKEN_LIMIT,
+        resetTime: NoResetTime,
+      })
+      const expected = ok(
+        ok({
+          id: 2,
+          tokenString: "existing_token",
+          remainingRequests: GITHUB_TOKEN_LIMIT,
+          resetTime: NoResetTime,
+        })
+      )
+
+      // Act
+      const actual = await selectReservedToken(existingToken, mockTokenDB)
+
+      // Assert
+      expect(actual).toEqual(expected)
+      jest.advanceTimersByTime(61 * 60 * 1000)
+      expect(mockAccessTokenQuery?.update).toHaveBeenCalledTimes(0)
+    })
+
+    it("should return the a new reserved token data from db", async () => {
+      // Arrange
+      const existingToken = ok({
+        id: 2,
+        tokenString: "existing_token",
+        remainingRequests: GITHUB_TOKEN_LIMIT - GITHUB_TOKEN_THRESHOLD,
+        resetTime: NoResetTime,
+      })
+      const expected = ok(
+        ok({
+          id: 1,
+          tokenString: "new_token",
+          remainingRequests: GITHUB_TOKEN_LIMIT,
+          resetTime: NoResetTime,
+        })
+      )
+
+      // Act
+      const actual = await selectReservedToken(existingToken, mockTokenDB)
+
+      // Assert
+      expect(actual).toEqual(expected)
+      jest.advanceTimersByTime(59 * 60 * 1000)
+      expect(mockAccessTokenQuery?.update).toHaveBeenCalledTimes(1)
+
+      jest.advanceTimersByTime(2 * 60 * 1000)
+      expect(mockAccessTokenQuery?.update).toHaveBeenCalledTimes(2)
+    })
+
+    it("should return no tokens with no database error", async () => {
+      // Arrange
+      const existingToken = ok({
+        id: 2,
+        tokenString: "existing_token",
+        remainingRequests: GITHUB_TOKEN_LIMIT - GITHUB_TOKEN_THRESHOLD,
+        resetTime: NoResetTime,
+      })
+      const expected = ok(NoTokenData)
+
+      mockAccessTokenQuery = null
+      const future = new Date()
+      future.setSeconds(future.getSeconds() + 60)
+
+      // Act
+      const actual = await selectReservedToken(existingToken, mockTokenDB)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe("parseResponseTokenData", () => {
+    const sampleGithubToken = "not_real_token_abcdefghijklmnopqrstuvwxy"
+
+    const mockAxiosResponse = jest.fn(
+      (resetTime: number) =>
+        ({
+          config: {
+            headers: {
+              Authorization: `token ${sampleGithubToken}`,
+            },
+          },
+          headers: {
+            [GITHUB_TOKEN_REMAINING_HEADER]: "1234",
+            [GITHUB_TOKEN_RESET_HEADER]: resetTime.toString(),
+          },
+          data: null,
+          status: 200,
+          statusText: "OK",
+        } as AxiosResponse)
+    )
+
+    it("should return the parsed information", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+
+      const response = mockAxiosResponse(resetTime)
+
+      const expected = ok({
+        token: sampleGithubToken,
+        remainingRequests: 1234,
+        resetTime,
+      })
+
+      // Act
+      const actual = parseResponseTokenData(response)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+
+    it("should raise an error based on invalid token type", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+      const response = mockAxiosResponse(resetTime)
+      if (response.config && response.config.headers) {
+        response.config.headers.Authorization = true
+      }
+
+      const expected = err(new TokenParsingError(response))
+
+      // Act
+      const actual = parseResponseTokenData(response)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+
+    it("should raise an error based on invalid token length", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+      const response = mockAxiosResponse(resetTime)
+      if (response.config && response.config.headers) {
+        response.config.headers.Authorization = `token ${sampleGithubToken.slice(
+          0,
+          -1
+        )}`
+      }
+
+      const expected = err(new TokenParsingError(response))
+
+      // Act
+      const actual = parseResponseTokenData(response)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+
+    it("should raise an error based on invalid token prefix", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+      const response = mockAxiosResponse(resetTime)
+      if (response.config && response.config.headers) {
+        response.config.headers.Authorization = `tokan ${sampleGithubToken}`
+      }
+
+      const expected = err(new TokenParsingError(response))
+
+      // Act
+      const actual = parseResponseTokenData(response)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+
+    it("should raise an error based on invalid remaining uses", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+      const response = mockAxiosResponse(resetTime)
+      response.headers[GITHUB_TOKEN_REMAINING_HEADER] = "abc"
+
+      const expected = err(new TokenParsingError(response))
+
+      // Act
+      const actual = parseResponseTokenData(response)
+
+      // Assert
+      expect(actual).toEqual(expected)
+    })
+
+    it("should raise an error based on invalid reset time", async () => {
+      // Arrange
+      const now = new Date()
+      const resetTime =
+        now.getTime() / 1000 + (now.getTimezoneOffset() * 60) / 1000 + 10
+      const response = mockAxiosResponse(resetTime)
+      response.headers[GITHUB_TOKEN_RESET_HEADER] = "abc"
+
+      const expected = err(new TokenParsingError(response))
+
+      // Act
+      const actual = parseResponseTokenData(response)
 
       // Assert
       expect(actual).toEqual(expected)

2cc4bc0fba5da912439d21575e58910c2c581f32
seaerchin <jiachin@open.gov.sg>
2023-05-31 12:22:23 +0800

    refactor(console.logger): shift colours into obj

--- src/logger/console.logger.ts
+++ src/logger/console.logger.ts
@@ -1,15 +1,21 @@
 import { Logger } from "./logger.types"
 
+const COLORS = {
+  RED: "\x1b[1;31m%s\x1b[0m",
+  GREEN: "\x1b[32m%s\x1b[0m",
+  YELLOW: "\x1b[33m%s\x1b[0m",
+} as const
+
 // eslint-disable-next-line import/prefer-default-export
 export const consoleLogger: Logger = {
   info: (message: string | Record<string, unknown>): void => {
     // NOTE: This adds RGB to our console logs
-    console.log("\x1b[32m%s\x1b[0m", `[INFO]: ${message}`)
+    console.log(COLORS.GREEN, `[INFO]: ${message}`)
   },
   warn: (message: string | Record<string, unknown>): void => {
-    console.warn("\x1b[33m%s\x1b[0m", `[WARN]: ${message}`)
+    console.warn(COLORS.YELLOW, `[WARN]: ${message}`)
   },
   error: (message: string | Record<string, unknown>): void => {
-    console.error("\x1b[1;31m%s\x1b[0m", `[ERROR]: ${message}`)
+    console.error(COLORS.RED, `[ERROR]: ${message}`)
   },
 }

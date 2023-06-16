fda4514181887fa1ccdb9d5a079bca59f2294c63
caando <zhangjikun907475@gmail.com>
2023-06-16 15:32:21 +0800

    fix(email): log mail status after delay

--- src/services/utilServices/MailClient.ts
+++ src/services/utilServices/MailClient.ts
@@ -5,6 +5,13 @@ import { config } from "@config/config"
 import logger from "@logger/logger"
 
 const POSTMAN_API_URL = "https://api.postman.gov.sg/v1"
+const MAIL_VERIFICATION_DELAY = 60000 // 60 seconds
+
+type MailData = {
+  id: string
+  recipient: string
+  status: string
+}
 
 class MailClient {
   POSTMAN_API_KEY: string
@@ -28,14 +35,55 @@ class MailClient {
     }
 
     try {
-      await axios.post(endpoint, email, {
+      const sendMailResponse = await axios.post(endpoint, email, {
         headers: {
           Authorization: `Bearer ${this.POSTMAN_API_KEY}`,
         },
       })
+      this.verifyMail(sendMailResponse.data)
     } catch (err) {
       logger.error(`Error occurred when sending email to ${recipient}: ${err}`)
       throw new Error("Failed to send email.")
+    }
+  }
+
+  async verifyMail(sendMailData: MailData): Promise<void> {
+    const endpoint = `${POSTMAN_API_URL}/transactional/email/${sendMailData.id}`
+    await new Promise((res) => setTimeout(res, MAIL_VERIFICATION_DELAY))
+    const verifyMailResponse = await axios.get(endpoint, {
+      headers: {
+        Authorization: `Bearer ${this.POSTMAN_API_KEY}`,
+      },
+    })
+    this.logEmailStatus(verifyMailResponse.data)
+  }
+
+  logEmailStatus(verifyMailData: MailData) {
+    const mailStatus = verifyMailData.status
+    switch (mailStatus) {
+      case "UNSENT":
+        logger.error(
+          `Email to ${verifyMailData.recipient} not accepted: ${verifyMailData}`
+        )
+        break
+      case "ACCEPTED":
+        logger.error(
+          `Email to ${verifyMailData.recipient} not sent: ${verifyMailData}`
+        )
+        break
+      case "SENT":
+        logger.error(
+          `Email to ${verifyMailData.recipient} not delivered: ${verifyMailData}`
+        )
+        break
+      case "BOUNCED":
+        logger.error(
+          `Email to ${verifyMailData.recipient} rejected by recipient's mail server: ${verifyMailData}`
+        )
+        break
+      default:
+        logger.info(`Email delivered to ${verifyMailData.recipient}`)
+        break
     }
   }
 }

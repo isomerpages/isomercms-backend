00dc1e78f4df696e38a873429d27a7f777601918
seaerchin <jiachin@open.gov.sg>
2023-06-19 19:17:46 +0800

    refactor(errors.spec): update

--- src/errors/__tests__/errors.spec.ts
+++ src/errors/__tests__/errors.spec.ts
@@ -1,5 +1,4 @@
 import { AuthError } from "../AuthError"
-import { IsomerInternalError } from "../IsomerError"
 
 describe("errors", () => {
   it("should conform to the expected interface even when it is not the base error", () => {
@@ -7,8 +6,8 @@ describe("errors", () => {
     const err = new AuthError()
 
     // Assert
-    expect(err).toBe<IsomerInternalError>({ code: "AuthError", message: "" })
     expect(err.code).toBe("AuthError")
-    expect(err.message).toBeDefined()
+    expect(err.meta).toBeEmptyObject()
+    expect(err.message).toBeString()
   })
 })

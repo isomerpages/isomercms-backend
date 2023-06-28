bd1c453779083c6e5cdde0710ec591e6e0e7e426
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-06-28 08:17:02 +0800

    style(site Info): add required types

--- src/types/siteInfo.ts
+++ src/types/siteInfo.ts
@@ -1,3 +1,5 @@
+import { RedirectionTypes } from "@root/constants"
+
 import { ProdPermalink, StagingPermalink } from "./pages"
 
 export type SiteInfo = {
@@ -7,4 +9,21 @@ export type SiteInfo = {
   publishedBy: string
   stagingUrl: StagingPermalink
   siteUrl: ProdPermalink
+}
+
+export interface DNSRecord {
+  source: string
+  type: keyof typeof RedirectionTypes
+  target: string
+}
+
+export interface DnsResultsForSite {
+  dnsRecords: DNSRecord[]
+  siteUrl: string
+}
+
+export interface SiteLaunchDto {
+  siteStatus: "LAUNCHED" | "NOT_LAUNCHED" | "LAUNCHING"
+  dnsRecords?: DNSRecord[] // only present iff siteStatus is LAUNCHED
+  siteUrl?: string
 }

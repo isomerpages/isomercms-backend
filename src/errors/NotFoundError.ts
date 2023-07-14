047446b63f99c0119bedab43e4db4f6bb5ff44e6
seaerchin <jiachin@open.gov.sg>
2023-07-14 17:47:50 +0800

    fix(errors): add messages where required

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -2,7 +2,7 @@ import { BaseIsomerError } from "@errors/BaseError"
 
 // eslint-disable-next-line import/prefer-default-export
 export class NotFoundError extends BaseIsomerError {
-  constructor(message: string) {
+  constructor(message = "The requested resource was not found") {
     super({ status: 404, code: "NotFoundError", message })
   }
 }

50a650d6839a51068e269c926ef21bfed0218d05
seaerchin <jiachin@open.gov.sg>
2023-07-26 13:24:22 +0800

    fix(router): add router

---
+++ src/routes/v2/authenticated/metrics.ts
@@ -0,0 +1,61 @@
+import express from "express"
+
+import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
+import { AuthorizationMiddleware } from "@root/middleware/authorization"
+import MailClient, { mailer } from "@root/services/utilServices/MailClient"
+import { RequestHandler } from "@root/types"
+import { FeedbackDto } from "@root/types/dto/feedback"
+import { StatsService, statsService } from "@services/infra/StatsService"
+
+const getFeedbackHtml = ({
+  rating,
+  feedback,
+  userType,
+  email,
+}: FeedbackDto) => `
+    <h1>Feedback</h1>
+    <p><b>UserType: </b>${userType}</p>
+    <p><b>Email: </b>${email}</p>
+    <p><b>Rating: </b>${rating}</p>
+    <p><b>Feedback: </b>${feedback}</p>
+    
+`
+
+export interface MetricsRouterProps {
+  statsService: StatsService
+
+  mailClient: MailClient
+  authorizationMiddleware: AuthorizationMiddleware
+}
+
+export class MetricsRouter {
+  private readonly authorizationMiddleware
+
+  constructor({ authorizationMiddleware }: MetricsRouterProps) {
+    this.authorizationMiddleware = authorizationMiddleware
+  }
+
+  collateUserFeedback: RequestHandler<
+    never,
+    unknown,
+    FeedbackDto,
+    unknown,
+    { userWithSiteSessionData: UserWithSiteSessionData }
+  > = (req, res) => {
+    const { userType } = req.body
+    mailer.sendMail(
+      "admin@isomer.gov.sg",
+      "[METRICS] User feedback",
+      getFeedbackHtml(req.body)
+    )
+    statsService.trackNpsRating(req.body.rating, { userType })
+    res.status(200).send("OK")
+  }
+
+  getRouter() {
+    const router = express.Router({ mergeParams: true })
+
+    router.post("/feedback", this.collateUserFeedback)
+    return router
+  }
+}

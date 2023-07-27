07fbae472068f7efde8c169b3a59c8e643388a8d
Harish V <harish@open.gov.sg>
2023-07-27 15:39:30 +0800

    Add EnvType

--- .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
+++ .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+ENV_TYPE=$(/opt/elasticbeanstalk/bin/get-config environment -k SSM_PREFIX)
+
 echo "Set AWS region"
 sudo aws configure set default.region ap-southeast-1
 
 echo "Fetching CLIENT_ID from SSM"
-CLIENT_ID=$(sudo aws ssm get-parameter --name STAGING_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
+CLIENT_ID=$(sudo aws ssm get-parameter --name "${ENV_TYPE}_CLIENT_ID" --with-decryption --query "Parameter.Value" --output text)
 echo "CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
 echo "Saved CLIENT_ID"
 
 echo "Fetching AWS_REGION from SSM"
-AWS_REGION=$(sudo aws ssm get-parameter --name STAGING_AWS_REGION --with-decryption --query "Parameter.Value" --output text)
+AWS_REGION=$(sudo aws ssm get-parameter --name "${ENV_TYPE}_AWS_REGION" --with-decryption --query "Parameter.Value" --output text)
 echo "AWS_REGION=$AWS_REGION" | sudo tee -a /opt/elasticbeanstalk/deployment/env
 echo "Saved AWS_REGION"

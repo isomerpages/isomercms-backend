b22762161da463d0fb57f59ef4dfc85308fb0bc3
Harish V <harish@open.gov.sg>
2023-07-27 14:24:27 +0800

    Fix echo

--- .platform/hooks/predeploy/01_fetch_ssm_parameters.sh
+++ .platform/hooks/predeploy/01_fetch_ssm_parameters.sh
@@ -5,5 +5,5 @@ sudo aws configure set default.region ap-southeast-1
 
 echo "Fetching CLIENT_ID from SSM"
 CLIENT_ID=$(sudo aws ssm get-parameter --name STAGING_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
-echo "export CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
-echo "Exported CLIENT_ID"
+echo "CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
+echo "Saved CLIENT_ID"

848e46a909a9811e03d90a7a795a93731199ad8f
Harish V <harish@open.gov.sg>
2023-07-27 14:46:32 +0800

    Fetch AWS region

--- .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
+++ .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
@@ -7,3 +7,8 @@ echo "Fetching CLIENT_ID from SSM"
 CLIENT_ID=$(sudo aws ssm get-parameter --name STAGING_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
 echo "CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
 echo "Saved CLIENT_ID"
+
+echo "Fetching AWS_REGION from SSM"
+AWS_REGION=$(sudo aws ssm get-parameter --name STAGING_AWS_REGION --with-decryption --query "Parameter.Value" --output text)
+echo "AWS_REGION=$AWS_REGION" | sudo tee -a /opt/elasticbeanstalk/deployment/env
+echo "Saved AWS_REGION"

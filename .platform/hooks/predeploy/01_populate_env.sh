e0990e2882358ec84db9014945b506e86651acdb
Harish V <harish@open.gov.sg>
2023-07-27 13:36:14 +0800

    edit the logs

--- .platform/hooks/predeploy/01_populate_env.sh
+++ .platform/hooks/predeploy/01_populate_env.sh
@@ -7,8 +7,10 @@ aws configure set default.region ap-southeast-1
 
 echo "Fetching CLIENT_ID from SSM"
 CLIENT_ID=$(aws ssm get-parameter --name STAGING_CLIENT_ID --with-decryption --query "Parameter.Value" --output text)
-echo "export CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
+sudo tee -a /opt/elasticbeanstalk/deployment/env
+echo "Exported CLIENT_ID"
 
 echo "Fetching CLIENT_SECRET from SSM"
 CLIENT_SECRET=$(aws ssm get-parameter --name STAGING_CLIENT_SECRET --with-decryption --query "Parameter.Value" --output text)
-echo "export CLIENT_SECRET=$CLIENT_SECRET" | sudo tee -a /opt/elasticbeanstalk/deployment/env
+sudo tee -a /opt/elasticbeanstalk/deployment/env
+echo "Exported CLIENT_ID"

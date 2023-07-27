001525f538261e9fd23350e0340bb3f94c3a8584
Harish V <harish@open.gov.sg>
2023-07-27 15:41:54 +0800

    Modularise script

--- .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
+++ .platform/hooks/postdeploy/01_fetch_ssm_parameters.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 ENV_TYPE=$(/opt/elasticbeanstalk/bin/get-config environment -k SSM_PREFIX)
+ENV_VARS=("CLIENT_ID" "AWS_REGION") # Add any additional env vars to this array
 
 echo "Set AWS region"
-sudo aws configure set default.region ap-southeast-1
+aws configure set default.region ap-southeast-1
 
-echo "Fetching CLIENT_ID from SSM"
-CLIENT_ID=$(sudo aws ssm get-parameter --name "${ENV_TYPE}_CLIENT_ID" --with-decryption --query "Parameter.Value" --output text)
-echo "CLIENT_ID=$CLIENT_ID" | sudo tee -a /opt/elasticbeanstalk/deployment/env
-echo "Saved CLIENT_ID"
-
-echo "Fetching AWS_REGION from SSM"
-AWS_REGION=$(sudo aws ssm get-parameter --name "${ENV_TYPE}_AWS_REGION" --with-decryption --query "Parameter.Value" --output text)
-echo "AWS_REGION=$AWS_REGION" | sudo tee -a /opt/elasticbeanstalk/deployment/env
-echo "Saved AWS_REGION"
+for ENV_VAR in "${ENV_VARS[@]}"; do
+  echo "Fetching ${ENV_VAR} from SSM"
+  VALUE=$(aws ssm get-parameter --name "${ENV_TYPE}_${ENV_VAR}" --with-decryption --query "Parameter.Value" --output text)
+  echo "${ENV_VAR}=${VALUE}" >> /opt/elasticbeanstalk/deployment/env
+  echo "Saved ${ENV_VAR}"
+done

f9fb3b337cd02d66c2c05eb50f6e88b389196d9b
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-07-31 21:54:03 +0800

    Use neverthrow the proper way

--- src/services/db/GitFileSystemService.ts
+++ src/services/db/GitFileSystemService.ts
@@ -1,7 +1,15 @@
 import fs from "fs"
 
-import { errAsync, okAsync, ResultAsync } from "neverthrow"
-import { SimpleGit } from "simple-git"
+import {
+  combine,
+  err,
+  errAsync,
+  ok,
+  okAsync,
+  Result,
+  ResultAsync,
+} from "neverthrow"
+import { GitError, SimpleGit } from "simple-git"
 
 import { config } from "@config/config"
 
@@ -26,244 +34,275 @@ export default class GitFileSystemService {
     this.git = git
   }
 
-  private async isGitInitialized(
-    repoName: string
-  ): Promise<ResultAsync<boolean, GitFileSystemError>> {
-    try {
-      const isGitRepo = await this.git
-        .cwd(`${EFS_VOL_PATH}/${repoName}`)
-        .checkIsRepo()
+  isGitInitialized(repoName: string): ResultAsync<boolean, GitFileSystemError> {
+    return ResultAsync.fromPromise(
+      this.git.cwd(`${EFS_VOL_PATH}/${repoName}`).checkIsRepo(),
+      (error) => {
+        if (error instanceof GitError) {
+          return new GitFileSystemError(error.message)
+        }
 
-      return okAsync(isGitRepo)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+        return new GitFileSystemError("An unknown error occurred")
+      }
+    )
   }
 
-  private async isOriginRemoteCorrect(
+  isOriginRemoteCorrect(
     repoName: string
-  ): Promise<ResultAsync<boolean, GitFileSystemError>> {
-    try {
-      const originUrl = `git@github.com:${ISOMER_GITHUB_ORG_NAME}/${repoName}.git`
-      const remoteUrl = await this.git
-        .cwd(`${EFS_VOL_PATH}/${repoName}`)
-        .remote(["get-url", "origin"])
+  ): ResultAsync<boolean, GitFileSystemError> {
+    const originUrl = `git@github.com:${ISOMER_GITHUB_ORG_NAME}/${repoName}.git`
 
-      return okAsync(!remoteUrl || remoteUrl.trim() !== originUrl)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+    return ResultAsync.fromPromise(
+      this.git.cwd(`${EFS_VOL_PATH}/${repoName}`).remote(["get-url", "origin"]),
+      (error) => {
+        if (error instanceof GitError) {
+          return new GitFileSystemError(error.message)
+        }
+
+        return new GitFileSystemError("An unknown error occurred")
+      }
+    ).map((remoteUrl) => !!remoteUrl && remoteUrl.trim() === originUrl)
   }
 
   // Determine if the folder is a valid Git repository
-  async isValidGitRepo(
-    repoName: string
-  ): Promise<ResultAsync<boolean, GitFileSystemError>> {
-    try {
-      const isFolderExisting = fs.existsSync(`${EFS_VOL_PATH}/${repoName}`)
-      if (!isFolderExisting) {
-        return okAsync(false)
+  isValidGitRepo(repoName: string): ResultAsync<boolean, GitFileSystemError> {
+    const safeExistsSync = Result.fromThrowable(fs.existsSync, (error) => {
+      if (error instanceof GitError || error instanceof Error) {
+        return new GitFileSystemError(error.message)
       }
 
-      const isGitInitialized = await this.isGitInitialized(repoName)
-      if (isGitInitialized.isErr() || !isGitInitialized.value) {
-        return okAsync(false)
-      }
+      return new GitFileSystemError("An unknown error occurred")
+    })
 
-      const isOriginRemoteCorrect = await this.isOriginRemoteCorrect(repoName)
-      return okAsync(
-        isOriginRemoteCorrect.isOk() && isOriginRemoteCorrect.value
-      )
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+    return safeExistsSync(`${EFS_VOL_PATH}/${repoName}`)
+      .andThen((isFolderExisting) => {
+        if (!isFolderExisting) {
+          return err(false)
+        }
+        return ok(true)
+      })
+      .asyncAndThen(() => this.isGitInitialized(repoName))
+      .andThen((isGitInitialized) => {
+        if (!isGitInitialized) {
+          return err(false)
+        }
+        return ok(true)
+      })
+      .andThen(() => this.isOriginRemoteCorrect(repoName))
+      .andThen((isOriginRemoteCorrect) => {
+        if (!isOriginRemoteCorrect) {
+          return err(false)
+        }
+        return ok(true)
+      })
+      .orElse((error) => {
+        if (typeof error === "boolean") {
+          return okAsync(false)
+        }
+        return errAsync(error)
+      })
   }
 
   // Obtain the Git blob hash of a file or directory
-  async getGitBlobHash(
+  getGitBlobHash(
     repoName: string,
     filePath: string
-  ): Promise<ResultAsync<string, GitFileSystemError>> {
-    try {
-      const hash = await this.git
+  ): ResultAsync<string, GitFileSystemError> {
+    return ResultAsync.fromPromise(
+      this.git
         .cwd(`${EFS_VOL_PATH}/${repoName}`)
         .checkout(BRANCH_REF)
-        .revparse([`HEAD:${filePath}`])
-      return okAsync(hash)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+        .revparse([`HEAD:${filePath}`]),
+      (error) => {
+        if (error instanceof GitError) {
+          return new GitFileSystemError(error.message)
+        }
+
+        return new GitFileSystemError("An unknown error occurred")
+      }
+    )
   }
 
   // Clone repository from upstream Git hosting provider
-  async clone(
-    repoName: string
-  ): Promise<ResultAsync<string, GitFileSystemError>> {
+  clone(repoName: string): ResultAsync<string, GitFileSystemError> {
     const originUrl = `git@github.com:${ISOMER_GITHUB_ORG_NAME}/${repoName}.git`
-
-    try {
-      if (fs.existsSync(`${EFS_VOL_PATH}/${repoName}`)) {
-        const isGitInitialized = await this.isGitInitialized(repoName)
-        if (isGitInitialized.isErr() || !isGitInitialized.value) {
-          return errAsync(
-            new GitFileSystemError(
-              `An existing folder "${repoName}" exists but is not a Git repo`
-            )
-          )
-        }
-
-        const isOriginRemoteCorrect = await this.isOriginRemoteCorrect(repoName)
-        if (isOriginRemoteCorrect.isErr() || !isOriginRemoteCorrect.value) {
-          return errAsync(
-            new GitFileSystemError(
-              `An existing folder "${repoName}" exists but is not the correct Git repo`
-            )
-          )
-        }
-
-        return okAsync(`${EFS_VOL_PATH}/${repoName}`)
+    const safeExistsSync = Result.fromThrowable(fs.existsSync, (error) => {
+      if (error instanceof GitError || error instanceof Error) {
+        return new GitFileSystemError(error.message)
       }
 
-      await this.git
-        .clone(originUrl, `${EFS_VOL_PATH}/${repoName}`)
-        .cwd(`${EFS_VOL_PATH}/${repoName}`)
-        .checkout(BRANCH_REF)
-      return okAsync(`${EFS_VOL_PATH}/${repoName}`)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+      return new GitFileSystemError("An unknown error occurred")
+    })
+
+    return safeExistsSync(`${EFS_VOL_PATH}/${repoName}`).asyncAndThen(
+      (isFolderExisting) => {
+        if (!isFolderExisting) {
+          return ResultAsync.fromPromise(
+            this.git
+              .clone(originUrl, `${EFS_VOL_PATH}/${repoName}`)
+              .cwd(`${EFS_VOL_PATH}/${repoName}`)
+              .checkout(BRANCH_REF),
+            (error) => {
+              if (error instanceof GitError) {
+                return new GitFileSystemError(error.message)
+              }
+
+              return new GitFileSystemError("An unknown error occurred")
+            }
+          ).map(() => `${EFS_VOL_PATH}/${repoName}`)
+        }
+
+        return this.isGitInitialized(repoName)
+          .andThen((isGitInitialized) => {
+            if (!isGitInitialized) {
+              return errAsync(
+                new GitFileSystemError(
+                  `An existing folder "${repoName}" exists but is not a Git repo`
+                )
+              )
+            }
+            return okAsync(true)
+          })
+          .andThen(() => this.isOriginRemoteCorrect(repoName))
+          .andThen((isOriginRemoteCorrect) => {
+            if (!isOriginRemoteCorrect) {
+              return errAsync(
+                new GitFileSystemError(
+                  `An existing folder "${repoName}" exists but is not the correct Git repo`
+                )
+              )
+            }
+            return okAsync(`${EFS_VOL_PATH}/${repoName}`)
+          })
+      }
+    )
   }
 
   // Pull the latest changes from upstream Git hosting provider
   // TODO: Pulling is a very expensive operation, should find a way to optimise
-  async pull(
-    repoName: string
-  ): Promise<ResultAsync<string, GitFileSystemError>> {
-    const isValid = await this.isValidGitRepo(repoName)
+  pull(repoName: string): ResultAsync<string, GitFileSystemError> {
+    return this.isValidGitRepo(repoName).andThen((isValid) => {
+      if (!isValid) {
+        return errAsync(
+          new GitFileSystemError(`Folder "${repoName}" is not a valid Git repo`)
+        )
+      }
 
-    if (isValid.isOk() && !isValid.value) {
-      return errAsync(
-        new GitFileSystemError(`Folder "${repoName}" is not a valid Git repo`)
-      )
-    }
-    if (isValid.isErr()) {
-      return errAsync(isValid.error)
-    }
+      return ResultAsync.fromPromise(
+        this.git.cwd(`${EFS_VOL_PATH}/${repoName}`).checkout(BRANCH_REF).pull(),
+        (error) => {
+          if (error instanceof GitError) {
+            return new GitFileSystemError(error.message)
+          }
 
-    try {
-      await this.git.cwd(`${EFS_VOL_PATH}/${repoName}`).pull()
-      return okAsync(`${EFS_VOL_PATH}/${repoName}`).checkout(BRANCH_REF)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+          return new GitFileSystemError("An unknown error occurred")
+        }
+      ).map(() => `${EFS_VOL_PATH}/${repoName}`)
+    })
   }
 
   // TODO: Creates either directory or file
   // ResourceDirectoryService used this to create a directory + file at the same time
-  async create() {}
+  create() {}
 
   // Read the contents of a file
-  async read(
+  read(
     repoName: string,
     filePath: string
-  ): Promise<ResultAsync<GitFile, GitFileSystemError>> {
-    // Ensure that the repository is up-to-date first
-    const isUpdated = await this.pull(repoName)
+  ): ResultAsync<GitFile, GitFileSystemError> {
+    return this.pull(repoName).andThen(() =>
+      combine([
+        ResultAsync.fromPromise(
+          fs.promises.readFile(
+            `${EFS_VOL_PATH}/${repoName}/${filePath}`,
+            "utf-8"
+          ),
+          (error) => {
+            if (error instanceof Error) {
+              return new GitFileSystemError(error.message)
+            }
 
-    if (isUpdated.isErr()) {
-      return errAsync(isUpdated.error)
-    }
-
-    // Read the file and get the hash concurrently
-    try {
-      const [contents, sha] = await Promise.all([
-        fs.promises.readFile(
-          `${EFS_VOL_PATH}/${repoName}/${filePath}`,
-          "utf-8"
+            return new GitFileSystemError("An unknown error occurred")
+          }
         ),
         this.getGitBlobHash(repoName, filePath),
-      ])
-
-      if (sha.isErr()) {
-        return errAsync(sha.error)
-      }
-
-      const result: GitFile = {
-        contents,
-        sha: sha.value,
-      }
-
-      return okAsync(result)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+      ]).map((contentsAndHash) => {
+        const [contents, sha] = contentsAndHash
+        const result: GitFile = {
+          contents,
+          sha,
+        }
+        return result
+      })
+    )
   }
 
   // Read the contents of a directory
-  async listDirectoryContents(
+  listDirectoryContents(
     repoName: string,
     directoryPath: string
-  ): Promise<ResultAsync<GitDirectoryItem[], GitFileSystemError>> {
-    // Check that the directory path provided exists and is a directory
-    try {
-      const stats = await fs.promises.stat(
-        `${EFS_VOL_PATH}/${repoName}/${directoryPath}`
-      )
+  ): ResultAsync<GitDirectoryItem[], GitFileSystemError> {
+    return ResultAsync.fromPromise(
+      fs.promises.stat(`${EFS_VOL_PATH}/${repoName}/${directoryPath}`),
+      (error) => {
+        if (error instanceof Error) {
+          return new GitFileSystemError(error.message)
+        }
 
-      if (!stats.isDirectory()) {
-        return errAsync(
-          new GitFileSystemError(
-            `Path "${directoryPath}" is not a directory in repo "${repoName}"`
-          )
-        )
+        return new GitFileSystemError("An unknown error occurred")
       }
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
+    )
+      .andThen((stats) => {
+        if (!stats.isDirectory()) {
+          return errAsync(
+            new GitFileSystemError(
+              `Path "${directoryPath}" is not a directory in repo "${repoName}"`
+            )
+          )
+        }
+        return okAsync(true)
+      })
+      .andThen(() =>
+        ResultAsync.fromPromise(
+          fs.promises.readdir(`${EFS_VOL_PATH}/${repoName}/${directoryPath}`, {
+            withFileTypes: true,
+          }),
+          (error) => {
+            if (error instanceof Error) {
+              return new GitFileSystemError(error.message)
+            }
 
-    // Read the directory contents
-    try {
-      const directoryContents = await fs.promises.readdir(
-        `${EFS_VOL_PATH}/${repoName}/${directoryPath}`,
-        { withFileTypes: true }
+            return new GitFileSystemError("An unknown error occurred")
+          }
+        )
       )
-
-      const result: GitDirectoryItem[] = await Promise.all(
-        directoryContents.map(async (directoryItem) => {
+      .andThen((directoryContents) => {
+        const resultAsyncs = directoryContents.map((directoryItem) => {
           const isDirectory = directoryItem.isDirectory()
           const { name } = directoryItem
           const path = directoryPath === "" ? name : `${directoryPath}/${name}`
           const type = isDirectory ? "dir" : "file"
-          const fileHash = await this.getGitBlobHash(repoName, path)
-          const sha = fileHash.isOk() ? fileHash.value : ""
 
-          return {
-            name,
-            type,
-            sha,
-            path,
-          }
+          return this.getGitBlobHash(repoName, path)
+            .orElse(() => okAsync(""))
+            .andThen((sha) => {
+              const result: GitDirectoryItem = {
+                name,
+                type,
+                sha,
+                path,
+              }
+
+              return okAsync(result)
+            })
         })
-      )
 
-      // Note: The sha is empty if the file is not tracked by Git
-      const gitTrackedResults = result.filter(
-        (directoryItem) => directoryItem.sha !== ""
+        return combine(resultAsyncs)
+      })
+      .andThen((directoryItems) =>
+        // Note: The sha is empty if the file is not tracked by Git
+        okAsync(directoryItems.filter((item) => item.sha !== ""))
       )
-
-      return okAsync(gitTrackedResults)
-    } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : "Unknown error"
-      return errAsync(new GitFileSystemError(message))
-    }
   }
 
   // TODO: Update the contents of a file

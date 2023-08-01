337652579b420bda7fc02e64a73cc0762461dadf
Harish V <harish@open.gov.sg>
2023-08-01 18:28:33 +0800

    run as simple cloning app

--- src/server.js
+++ src/server.js
@@ -109,26 +109,26 @@ const express = require("express")
 const helmet = require("helmet")
 const createError = require("http-errors")
 
-const SESSION_SECRET = config.get("auth.sessionSecret")
+// const SESSION_SECRET = config.get("auth.sessionSecret")
 
-const SequelizeStore = SequelizeStoreFactory(session.Store)
-const sessionMiddleware = session({
-  store: new SequelizeStore({
-    db: sequelize,
-    tableName: "sessions",
-    checkExpirationInterval: 15 * 60 * 1000, // Checks expired sessions every 15 minutes
-  }),
-  resave: false, // can set to false since touch is implemented by our store
-  saveUninitialized: false, // do not save new sessions that have not been modified
-  cookie: {
-    httpOnly: true,
-    sameSite: "strict",
-    secure: isSecure,
-    maxAge: AUTH_TOKEN_EXPIRY_MS,
-  },
-  secret: SESSION_SECRET,
-  name: "isomer",
-})
+// const SequelizeStore = SequelizeStoreFactory(session.Store)
+// const sessionMiddleware = session({
+//   store: new SequelizeStore({
+//     db: sequelize,
+//     tableName: "sessions",
+//     checkExpirationInterval: 15 * 60 * 1000, // Checks expired sessions every 15 minutes
+//   }),
+//   resave: false, // can set to false since touch is implemented by our store
+//   saveUninitialized: false, // do not save new sessions that have not been modified
+//   cookie: {
+//     httpOnly: true,
+//     sameSite: "strict",
+//     secure: isSecure,
+//     maxAge: AUTH_TOKEN_EXPIRY_MS,
+//   },
+//   secret: SESSION_SECRET,
+//   name: "isomer",
+// })
 
 // Env vars
 const FRONTEND_URL = config.get("app.frontendUrl")
@@ -137,14 +137,14 @@ const FRONTEND_URL = config.get("app.frontendUrl")
 // Import routes
 const simpleGit = require("simple-git")
 
-const { errorHandler } = require("@middleware/errorHandler")
+// const { errorHandler } = require("@middleware/errorHandler")
 
-const { FormsgRouter } = require("@routes/formsgSiteCreation")
-const { FormsgSiteLaunchRouter } = require("@routes/formsgSiteLaunch")
-const { AuthRouter } = require("@routes/v2/auth")
+// const { FormsgRouter } = require("@routes/formsgSiteCreation")
+// const { FormsgSiteLaunchRouter } = require("@routes/formsgSiteLaunch")
+// const { AuthRouter } = require("@routes/v2/auth")
 
-const { GitHubService } = require("@services/db/GitHubService")
-const { AuthService } = require("@services/utilServices/AuthService")
+// const { GitHubService } = require("@services/db/GitHubService")
+// const { AuthService } = require("@services/utilServices/AuthService")
 
 async function cloneRepo(repoPath, localPath) {
   const git = simpleGit()
@@ -153,259 +153,268 @@ async function cloneRepo(repoPath, localPath) {
     await git.clone(repoPath, localPath)
     console.log(`Repository cloned from ${repoPath} to ${localPath}`)
   } catch (err) {
-    console.error("Failed to clone repository:", err)
+    console.error("Failed to clone repository:")
+    console.log(err)
   }
 }
-
-cloneRepo("git@github.com:harishv7/ggs-mvp-test.git", "/efs/repos")
-
-const authService = new AuthService({ usersService })
-const gitHubService = new GitHubService({
-  axiosInstance: isomerRepoAxiosInstance,
-})
-const configYmlService = new ConfigYmlService({ gitHubService })
-const footerYmlService = new FooterYmlService({ gitHubService })
-const collectionYmlService = new CollectionYmlService({ gitHubService })
-const baseDirectoryService = new BaseDirectoryService({ gitHubService })
-
-const contactUsService = new ContactUsPageService({
-  gitHubService,
-  footerYmlService,
-})
-const collectionPageService = new CollectionPageService({
-  gitHubService,
-  collectionYmlService,
-})
-const subCollectionPageService = new SubcollectionPageService({
-  gitHubService,
-  collectionYmlService,
-})
-const homepageService = new HomepagePageService({ gitHubService })
-const resourcePageService = new ResourcePageService({ gitHubService })
-const unlinkedPageService = new UnlinkedPageService({ gitHubService })
-const resourceRoomDirectoryService = new ResourceRoomDirectoryService({
-  baseDirectoryService,
-  configYmlService,
-  gitHubService,
-})
-const pageService = new PageService({
-  collectionPageService,
-  contactUsService,
-  subCollectionPageService,
-  homepageService,
-  resourcePageService,
-  unlinkedPageService,
-  resourceRoomDirectoryService,
-})
-const reviewRequestService = new ReviewRequestService(
-  gitHubService,
-  User,
-  ReviewRequest,
-  Reviewer,
-  ReviewMeta,
-  ReviewRequestView,
-  pageService,
-  new ConfigService()
-)
-const cacheRefreshInterval = 1000 * 60 * 5 // 5 minutes
-const sitesCacheService = new SitesCacheService(cacheRefreshInterval)
-
-const sitesService = new SitesService({
-  siteRepository: Site,
-  gitHubService,
-  configYmlService,
-  usersService,
-  isomerAdminsService,
-  reviewRequestService,
-  sitesCacheService,
-})
-const reposService = new ReposService({ repository: Repo })
-const deploymentsService = new DeploymentsService({
-  deploymentsRepository: Deployment,
-})
-const launchClient = new LaunchClient()
-const launchesService = new LaunchesService({
-  launchesRepository: Launch,
-  repoRepository: Repo,
-  deploymentRepository: Deployment,
-  redirectionsRepository: Redirection,
-  siteRepository: Site,
-  launchClient,
-})
-const queueService = new QueueService()
-const stepFunctionsService = new StepFunctionsService(
-  config.get("aws.stepFunctions.stepFunctionsArn")
-)
-const dynamoDBService = new DynamoDBService({
-  dynamoDBClient: new DynamoDBDocClient(),
-})
-
-const identityAuthService = getIdentityAuthService(gitHubService)
-const collaboratorsService = new CollaboratorsService({
-  siteRepository: Site,
-  siteMemberRepository: SiteMember,
-  sitesService,
-  usersService,
-  whitelist: Whitelist,
-})
-
-const infraService = new InfraService({
-  sitesService,
-  reposService,
-  deploymentsService,
-  launchesService,
-  queueService,
-  collaboratorsService,
-  stepFunctionsService,
-  dynamoDBService,
-  usersService,
-})
-// poller site launch updates
-infraService.pollMessages()
-
-const authenticationMiddleware = getAuthenticationMiddleware()
-const authorizationMiddleware = getAuthorizationMiddleware({
-  identityAuthService,
-  usersService,
-  isomerAdminsService,
-  collaboratorsService,
-})
-const notificationOnEditHandler = new NotificationOnEditHandler({
-  reviewRequestService,
-  sitesService,
-  collaboratorsService,
-  notificationsService,
-})
-
-const reviewRouter = new ReviewsRouter(
-  reviewRequestService,
-  usersService,
-  sitesService,
-  collaboratorsService,
-  notificationsService,
-  gitHubService
-)
-const authenticatedSubrouterV1 = getAuthenticatedSubrouterV1({
-  authenticationMiddleware,
-  statsMiddleware,
-  usersService,
-  apiLogger,
-})
-const authenticatedSitesSubrouterV1 = getAuthenticatedSitesSubrouterV1({
-  authenticationMiddleware,
-  authorizationMiddleware,
-  apiLogger,
-})
-
-const authenticatedSubrouterV2 = getAuthenticatedSubrouter({
-  authenticationMiddleware,
-  sitesService,
-  usersService,
-  reposService,
-  statsMiddleware,
-  deploymentsService,
-  apiLogger,
-  collaboratorsService,
-  authorizationMiddleware,
-  reviewRouter,
-  notificationsService,
-  infraService,
-})
-
-const authenticatedSitesSubrouterV2 = getAuthenticatedSitesSubrouter({
-  authorizationMiddleware,
-  authenticationMiddleware,
-  gitHubService,
-  configYmlService,
-  apiLogger,
-  notificationOnEditHandler,
-  sitesService,
-  deploymentsService,
-})
-const authV2Router = new AuthRouter({
-  authenticationMiddleware,
-  authService,
-  apiLogger,
-  rateLimiter,
-  statsMiddleware,
-})
-const formsgRouter = new FormsgRouter({ usersService, infraService })
-const formsgSiteLaunchRouter = new FormsgSiteLaunchRouter({
-  usersService,
-  infraService,
-})
 
 const app = express()
-
-if (isSecure) {
-  // Our server only receives requests from the alb reverse proxy, so we need to use the client IP provided in X-Forwarded-For
-  // This is trusted because our security groups block all other access to the server
-  app.set("trust proxy", true)
-}
-app.use(helmet())
-
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-  })
-)
-app.use(express.json({ limit: "7mb" }))
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
-app.use(nocache())
-
-app.use(sessionMiddleware)
-
-// Health endpoint
-app.use("/v2/ping", (req, res, next) => res.status(200).send("Ok"))
-
-// Routes layer setup
-// To avoid refactoring auth router v1 to use dependency injection
-app.use("/v1/auth", authV2Router.getRouter())
-// Endpoints which have siteName, used to inject site access token
-app.use("/v1/sites/:siteName", authenticatedSitesSubrouterV1)
-// Endpoints which require login, but not site access token
-app.use("/v1", authenticatedSubrouterV1)
-
-app.use("/v2/auth", authV2Router.getRouter())
-// Endpoints which have require login, but not site access token
-app.use("/v2", authenticatedSubrouterV2)
-// Endpoints which modify the github repo, used to inject site access token
-app.use("/v2/sites/:siteName", authenticatedSitesSubrouterV2)
-
-// FormSG Backend handler routes
-app.use("/formsg", formsgRouter.getRouter())
-app.use("/formsg", formsgSiteLaunchRouter.getRouter())
-
-// catch unknown routes
-app.use((req, res, next) => {
-  if (!req.route) {
-    return res.status(404).send("Unknown route requested")
-  }
-  return next()
+app.get("/", (req, res) => {
+  res.send("Hello World!")
 })
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404))
+app.listen(8081, () => {
+  console.log(`Example app listening on port ${8081}`)
+  cloneRepo("git@github.com:harishv7/ggs-mvp-test.git", "/efs/repos")
 })
 
-// error handler
-app.use(errorHandler)
+// const authService = new AuthService({ usersService })
+// const gitHubService = new GitHubService({
+//   axiosInstance: isomerRepoAxiosInstance,
+// })
+// const configYmlService = new ConfigYmlService({ gitHubService })
+// const footerYmlService = new FooterYmlService({ gitHubService })
+// const collectionYmlService = new CollectionYmlService({ gitHubService })
+// const baseDirectoryService = new BaseDirectoryService({ gitHubService })
 
-logger.info("Connecting to Sequelize")
-sequelize
-  .authenticate()
-  .then(() => {
-    logger.info("Connection has been established successfully.")
-    bootstrap(app)
-  })
-  .catch((err) => {
-    logger.error(`Unable to connect to the database: ${err}`)
+// const contactUsService = new ContactUsPageService({
+//   gitHubService,
+//   footerYmlService,
+// })
+// const collectionPageService = new CollectionPageService({
+//   gitHubService,
+//   collectionYmlService,
+// })
+// const subCollectionPageService = new SubcollectionPageService({
+//   gitHubService,
+//   collectionYmlService,
+// })
+// const homepageService = new HomepagePageService({ gitHubService })
+// const resourcePageService = new ResourcePageService({ gitHubService })
+// const unlinkedPageService = new UnlinkedPageService({ gitHubService })
+// const resourceRoomDirectoryService = new ResourceRoomDirectoryService({
+//   baseDirectoryService,
+//   configYmlService,
+//   gitHubService,
+// })
+// const pageService = new PageService({
+//   collectionPageService,
+//   contactUsService,
+//   subCollectionPageService,
+//   homepageService,
+//   resourcePageService,
+//   unlinkedPageService,
+//   resourceRoomDirectoryService,
+// })
+// const reviewRequestService = new ReviewRequestService(
+//   gitHubService,
+//   User,
+//   ReviewRequest,
+//   Reviewer,
+//   ReviewMeta,
+//   ReviewRequestView,
+//   pageService,
+//   new ConfigService()
+// )
+// const cacheRefreshInterval = 1000 * 60 * 5 // 5 minutes
+// const sitesCacheService = new SitesCacheService(cacheRefreshInterval)
 
-    // If we cannot connect to the db, report an error using status code
-    // And gracefully shut down the application since we can't serve client
-    process.exitCode = 1
-  })
+// const sitesService = new SitesService({
+//   siteRepository: Site,
+//   gitHubService,
+//   configYmlService,
+//   usersService,
+//   isomerAdminsService,
+//   reviewRequestService,
+//   sitesCacheService,
+// })
+// const reposService = new ReposService({ repository: Repo })
+// const deploymentsService = new DeploymentsService({
+//   deploymentsRepository: Deployment,
+// })
+// const launchClient = new LaunchClient()
+// const launchesService = new LaunchesService({
+//   launchesRepository: Launch,
+//   repoRepository: Repo,
+//   deploymentRepository: Deployment,
+//   redirectionsRepository: Redirection,
+//   siteRepository: Site,
+//   launchClient,
+// })
+// const queueService = new QueueService()
+// const stepFunctionsService = new StepFunctionsService(
+//   config.get("aws.stepFunctions.stepFunctionsArn")
+// )
+// const dynamoDBService = new DynamoDBService({
+//   dynamoDBClient: new DynamoDBDocClient(),
+// })
+
+// const identityAuthService = getIdentityAuthService(gitHubService)
+// const collaboratorsService = new CollaboratorsService({
+//   siteRepository: Site,
+//   siteMemberRepository: SiteMember,
+//   sitesService,
+//   usersService,
+//   whitelist: Whitelist,
+// })
+
+// const infraService = new InfraService({
+//   sitesService,
+//   reposService,
+//   deploymentsService,
+//   launchesService,
+//   queueService,
+//   collaboratorsService,
+//   stepFunctionsService,
+//   dynamoDBService,
+//   usersService,
+// })
+// // poller site launch updates
+// infraService.pollMessages()
+
+// const authenticationMiddleware = getAuthenticationMiddleware()
+// const authorizationMiddleware = getAuthorizationMiddleware({
+//   identityAuthService,
+//   usersService,
+//   isomerAdminsService,
+//   collaboratorsService,
+// })
+// const notificationOnEditHandler = new NotificationOnEditHandler({
+//   reviewRequestService,
+//   sitesService,
+//   collaboratorsService,
+//   notificationsService,
+// })
+
+// const reviewRouter = new ReviewsRouter(
+//   reviewRequestService,
+//   usersService,
+//   sitesService,
+//   collaboratorsService,
+//   notificationsService,
+//   gitHubService
+// )
+// const authenticatedSubrouterV1 = getAuthenticatedSubrouterV1({
+//   authenticationMiddleware,
+//   statsMiddleware,
+//   usersService,
+//   apiLogger,
+// })
+// const authenticatedSitesSubrouterV1 = getAuthenticatedSitesSubrouterV1({
+//   authenticationMiddleware,
+//   authorizationMiddleware,
+//   apiLogger,
+// })
+
+// const authenticatedSubrouterV2 = getAuthenticatedSubrouter({
+//   authenticationMiddleware,
+//   sitesService,
+//   usersService,
+//   reposService,
+//   statsMiddleware,
+//   deploymentsService,
+//   apiLogger,
+//   collaboratorsService,
+//   authorizationMiddleware,
+//   reviewRouter,
+//   notificationsService,
+//   infraService,
+// })
+
+// const authenticatedSitesSubrouterV2 = getAuthenticatedSitesSubrouter({
+//   authorizationMiddleware,
+//   authenticationMiddleware,
+//   gitHubService,
+//   configYmlService,
+//   apiLogger,
+//   notificationOnEditHandler,
+//   sitesService,
+//   deploymentsService,
+// })
+// const authV2Router = new AuthRouter({
+//   authenticationMiddleware,
+//   authService,
+//   apiLogger,
+//   rateLimiter,
+//   statsMiddleware,
+// })
+// const formsgRouter = new FormsgRouter({ usersService, infraService })
+// const formsgSiteLaunchRouter = new FormsgSiteLaunchRouter({
+//   usersService,
+//   infraService,
+// })
+
+// const app = express()
+
+// if (isSecure) {
+//   // Our server only receives requests from the alb reverse proxy, so we need to use the client IP provided in X-Forwarded-For
+//   // This is trusted because our security groups block all other access to the server
+//   app.set("trust proxy", true)
+// }
+// app.use(helmet())
+
+// app.use(
+//   cors({
+//     origin: FRONTEND_URL,
+//     credentials: true,
+//   })
+// )
+// app.use(express.json({ limit: "7mb" }))
+// app.use(express.urlencoded({ extended: false }))
+// app.use(cookieParser())
+// app.use(express.static(path.join(__dirname, "public")))
+// app.use(nocache())
+
+// app.use(sessionMiddleware)
+
+// // Health endpoint
+// app.use("/v2/ping", (req, res, next) => res.status(200).send("Ok"))
+
+// // Routes layer setup
+// // To avoid refactoring auth router v1 to use dependency injection
+// app.use("/v1/auth", authV2Router.getRouter())
+// // Endpoints which have siteName, used to inject site access token
+// app.use("/v1/sites/:siteName", authenticatedSitesSubrouterV1)
+// // Endpoints which require login, but not site access token
+// app.use("/v1", authenticatedSubrouterV1)
+
+// app.use("/v2/auth", authV2Router.getRouter())
+// // Endpoints which have require login, but not site access token
+// app.use("/v2", authenticatedSubrouterV2)
+// // Endpoints which modify the github repo, used to inject site access token
+// app.use("/v2/sites/:siteName", authenticatedSitesSubrouterV2)
+
+// // FormSG Backend handler routes
+// app.use("/formsg", formsgRouter.getRouter())
+// app.use("/formsg", formsgSiteLaunchRouter.getRouter())
+
+// // catch unknown routes
+// app.use((req, res, next) => {
+//   if (!req.route) {
+//     return res.status(404).send("Unknown route requested")
+//   }
+//   return next()
+// })
+
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   next(createError(404))
+// })
+
+// // error handler
+// app.use(errorHandler)
+
+// logger.info("Connecting to Sequelize")
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     logger.info("Connection has been established successfully.")
+//     bootstrap(app)
+//   })
+//   .catch((err) => {
+//     logger.error(`Unable to connect to the database: ${err}`)
+
+//     // If we cannot connect to the db, report an error using status code
+//     // And gracefully shut down the application since we can't serve client
+//     process.exitCode = 1
+//   })

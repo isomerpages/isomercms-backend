5151b499690cf8e2741f079a5862178a54a0a9fc
Harish V <harish@open.gov.sg>
2023-08-02 16:02:13 +0800

    add check to bashrc

--- .platform/hooks/predeploy/03_add_keys_to_ssh_agent.sh
+++ .platform/hooks/predeploy/03_add_keys_to_ssh_agent.sh
@@ -7,4 +7,12 @@ ssh-add $SSH_PRIVATE_KEY_PATH
 echo "SSH Key added to ssh-agent"
 
 # for troubleshooting if required
-ssh-add -l > /tmp/ssh-agent-identities.txt 
+ssh-add -l > /tmp/ssh-agent-identities.txt
+
+# Add the commands to the end of .bashrc
+echo "" >> /home/ec2-user/.bashrc
+echo "# SSH agent settings" >> /home/ec2-user/.bashrc
+echo "if [ -z \"\$SSH_AUTH_SOCK\" ] ; then" >> /home/ec2-user/.bashrc
+echo "  eval \`ssh-agent -s\`" >> /home/ec2-user/.bashrc
+echo "  ssh-add $SSH_PRIVATE_KEY_PATH" >> /home/ec2-user/.bashrc
+echo "fi" >> /home/ec2-user/.bashrc

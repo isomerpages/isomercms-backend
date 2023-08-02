56799120d4e245ccb7328dc1a31254373d5622ce
Harish V <harish@open.gov.sg>
2023-08-02 16:13:11 +0800

    add to bashrc of root user

--- .platform/hooks/predeploy/03_add_keys_to_ssh_agent.sh
+++ .platform/hooks/predeploy/03_add_keys_to_ssh_agent.sh
@@ -9,10 +9,18 @@ echo "SSH Key added to ssh-agent"
 # for troubleshooting if required
 ssh-add -l > /tmp/ssh-agent-identities.txt
 
-# Add the commands to the end of .bashrc
+# Add the commands to the end of ec2-user .bashrc
 echo "" >> /home/ec2-user/.bashrc
 echo "# SSH agent settings" >> /home/ec2-user/.bashrc
 echo "if [ -z \"\$SSH_AUTH_SOCK\" ] ; then" >> /home/ec2-user/.bashrc
 echo "  eval \`ssh-agent -s\`" >> /home/ec2-user/.bashrc
 echo "  ssh-add $SSH_PRIVATE_KEY_PATH" >> /home/ec2-user/.bashrc
 echo "fi" >> /home/ec2-user/.bashrc
+
+# Add the commands to the end of root .bashrc
+echo "" >> /root/.bashrc
+echo "# SSH agent settings" >> /root/.bashrc
+echo "if [ -z \"\$SSH_AUTH_SOCK\" ] ; then" >> /root/.bashrc
+echo "  eval \`ssh-agent -s\`" >> /root/.bashrc
+echo "  ssh-add $SSH_PRIVATE_KEY_PATH" >> /root/.bashrc
+echo "fi" >> /root/.bashrc

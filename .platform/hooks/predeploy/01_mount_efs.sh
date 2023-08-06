0c031c456bf0d373262c741dc862946ddd4bc37f
Harish V <harish@open.gov.sg>
2023-08-06 16:45:57 +0800

    feat: add comment from AWS

--- .platform/hooks/predeploy/01_mount_efs.sh
+++ .platform/hooks/predeploy/01_mount_efs.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Note the following has been adapted from https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/services-efs.html
+
+###################################################################################################
+#### Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+####
+#### Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+#### except in compliance with the License. A copy of the License is located at
+####
+####     http://aws.amazon.com/apache2.0/
+####
+#### or in the "license" file accompanying this file. This file is distributed on an "AS IS"
+#### BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#### License for the specific language governing permissions and limitations under the License.
+###################################################################################################
+
+###################################################################################################
+#### This configuration file mounts an Amazon EFS file system to a specified directory.
+#### Both the directory and the EFS file system ID to be mounted is retrieved from AWS SSM.
+####
+#### If your environment and file system are in a custom VPC, you must configure the VPC to allow
+#### DNS resolution and DNS host names. See this topic in the VPC User Guide for more information:
+####    http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/vpc-dns.html
+###################################################################################################
+
 # Install AWS CLI if it's not installed yet
 which aws >/dev/null 2>&1
 if [ $? -ne 0 ]; then

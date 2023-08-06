21c1ddd79764f1275d2e422ca37a7e9fa17d7cc3
Harish V <harish@open.gov.sg>
2023-08-06 17:31:10 +0800

    feat: add check for errors

--- .platform/hooks/predeploy/05_set_git_profile.sh
+++ .platform/hooks/predeploy/05_set_git_profile.sh
@@ -13,6 +13,12 @@ aws configure set default.region ap-southeast-1
 GIT_USER_NAME=$(aws ssm get-parameter --name $GIT_USER_NAME_PARAM_NAME --with-decryption --query "Parameter.Value" --output text)
 GIT_USER_EMAIL=$(aws ssm get-parameter --name $GIT_USER_EMAIL_PARAM_NAME --with-decryption --query "Parameter.Value" --output text)
 
+# Check if required variables are not empty
+if [[ -z $GIT_USER_NAME || -z $GIT_USER_EMAIL ]]; then
+    echo "Error: Failed to fetch Git user information from AWS SSM." > /tmp/setup-git-profile.txt
+    exit 1
+fi
+
 # Write the configuration to .gitconfig
 echo "[user]" >> /home/webapp/.gitconfig
 echo "  name = $GIT_USER_NAME" >> /home/webapp/.gitconfig

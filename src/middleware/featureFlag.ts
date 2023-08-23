0c33a47dacc9d123fa9b6ae1986e9b9270433f8e
Harish V <harish@open.gov.sg>
2023-08-24 00:42:12 +0800

    feat: update middleware

--- src/middleware/featureFlag.ts
+++ src/middleware/featureFlag.ts
@@ -3,46 +3,6 @@ import config from "@root/config/config"
 import { RequestHandlerWithGrowthbook } from "@root/types"
 import { getNewGrowthbookInstance } from "@root/utils/growthbook-utils"
 
-export class FeatureFlagMiddleware {
-  //   private readonly featureFlagService: FeatureFlagService
-
-  //   constructor({
-  //     featureFlagService,
-  //   }: {
-  //     featureFlagService: FeatureFlagService
-  //   }) {
-  //     this.featureFlagService = featureFlagService
-  //   }
-
-  loadFeatures: RequestHandlerWithGrowthbook<
-    never,
-    unknown,
-    unknown,
-    never,
-    { userWithSiteSessionData: UserWithSiteSessionData }
-  > = async (req, res, next) => {
-    req.growthbook = getNewGrowthbookInstance(
-      config.get("growthbook.clientKey")
-    )
-    console.log("Created growth book instance: ", req.growthbook.getFeatures())
-
-    // Clean up at the end of the request
-    res.on("close", () => {
-      console.log("Destroying growthbook")
-      req.growthbook.destroy()
-    })
-
-    // Wait for features to load (will be cached in-memory for future requests)
-    req.growthbook
-      .loadFeatures()
-      .then(() => next())
-      .catch((e: unknown) => {
-        console.error("Failed to load features from GrowthBook", e)
-        next()
-      })
-  }
-}
-
 export const featureFlagMiddleware: RequestHandlerWithGrowthbook<
   never,
   unknown,
@@ -54,20 +14,13 @@ export const featureFlagMiddleware: RequestHandlerWithGrowthbook<
 
   // Clean up at the end of the request
   res.on("close", () => {
-    console.log("Destroying growthbook")
     req.growthbook.destroy()
   })
 
   // Wait for features to load (will be cached in-memory for future requests)
   req.growthbook
     .loadFeatures()
-    .then(() => {
-      console.log(
-        "Created growth book instance: ",
-        req.growthbook.getFeatures()
-      )
-      next()
-    })
+    .then(() => next())
     .catch((e: unknown) => {
       console.error("Failed to load features from GrowthBook", e)
       next()

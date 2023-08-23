a12da4eeb1492bf4fe57d7a7327244e95a2bad25
Harish V <harish@open.gov.sg>
2023-08-24 00:40:58 +0800

    feat: add extended type for request handler

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,4 +1,10 @@
-import { RequestHandler as ExpressHandler } from "express"
+import { GrowthBook } from "@growthbook/growthbook"
+import {
+  RequestHandler as ExpressHandler,
+  Request as ExpressRequest,
+  Response,
+  NextFunction,
+} from "express"
 
 export type RequestHandler<
   P = unknown,
@@ -7,3 +13,24 @@ export type RequestHandler<
   ReqQuery = unknown,
   Locals extends Record<string, unknown> = Record<string, unknown>
 > = ExpressHandler<P, ResBody, ReqBody, ReqQuery, Locals>
+
+interface GrowthBookRequest extends ExpressRequest {
+  growthbook: GrowthBook
+}
+
+export type RequestHandlerWithGrowthbook<
+  P = unknown,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = unknown,
+  Locals extends Record<string, unknown> = Record<string, unknown>
+> = (
+  req: GrowthBookRequest & {
+    params: P
+    body: ReqBody
+    query: ReqQuery
+    locals: Locals
+  },
+  res: Response<ResBody, Locals>,
+  next: NextFunction
+) => any

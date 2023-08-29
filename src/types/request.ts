796bf69c6550ae58eac329a2f9393022b173b46e
Harish V <harish@open.gov.sg>
2023-08-29 23:22:45 +0800

    feat: add comment explaining new handler type

--- src/types/request.ts
+++ src/types/request.ts
@@ -18,6 +18,15 @@ interface GrowthBookRequest extends ExpressRequest {
   growthbook: GrowthBook
 }
 
+/**
+ * Type for middleware functions with Growthbook integration.
+ *
+ * @template P Type for URL parameters
+ * @template ResBody Type for HTTP response body
+ * @template ReqBody Type for HTTP request body
+ * @template ReqQuery Type for HTTP request query parameters
+ * @template Locals Type for local variables
+ */
 export type RequestHandlerWithGrowthbook<
   P = unknown,
   ResBody = unknown,

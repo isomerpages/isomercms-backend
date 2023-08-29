4711c42f3ae95d450a5753e968e1d6bcfa24972b
Harish V <harish@open.gov.sg>
2023-08-29 13:06:48 +0800

    feat: add type safety for growthbook property in req

--- src/types/express/request.d.ts
+++ src/types/express/request.d.ts
@@ -1,12 +1,14 @@
 import { GrowthBook } from "@growthbook/growthbook"
 
+import { FeatureFlags } from "@root/types/featureFlags"
+
 // to make the file a module and avoid the TypeScript error
 export {}
 
 declare global {
   namespace Express {
     export interface Request {
-      growthbook: GrowthBook
+      growthbook: GrowthBook<FeatureFlags>
     }
   }
 }

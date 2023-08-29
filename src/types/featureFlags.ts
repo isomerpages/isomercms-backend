d1e1bbdf4973a9e41eb249fafd2040f71c15ced9
Harish V <harish@open.gov.sg>
2023-08-29 14:07:41 +0800

    feat: add key for ggs whitelist

--- src/types/featureFlags.ts
+++ src/types/featureFlags.ts
@@ -1,15 +1,7 @@
 // Use for type safety with GrowthBook
 // Add BE feature flags here to mirror that on GrowthBook
 export interface FeatureFlags {
-  samplekey: string
-}
-
-// List of attributes we set in GrowthBook Instance in auth middleware
-export type GrowthBookAttributes = {
-  isomerUserId: string
-  email: string
-  githubId?: string
-  siteName?: string
+  ggs_whitelisted_repos: { repos: string[] }
 }
 
 // List of attributes we set in GrowthBook Instance in auth middleware

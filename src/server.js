62500ea32385be7ffc30bf2c897d620f13338fef
seaerchin <jiachin@open.gov.sg>
2023-10-03 14:17:47 +0800

    refactor(formsg): shift cloning to separate router

--- src/server.js
+++ src/server.js
@@ -147,6 +147,7 @@ const FRONTEND_URL = config.get("app.frontendUrl")
 // Import routes
 const { errorHandler } = require("@middleware/errorHandler")
 
+const { FormsgSiteCloneRouter } = require("@routes/formsgSiteClone")
 const { FormsgRouter } = require("@routes/formsgSiteCreation")
 const { FormsgSiteLaunchRouter } = require("@routes/formsgSiteLaunch")
 const { AuthRouter } = require("@routes/v2/auth")
@@ -360,6 +361,9 @@ const formsgSiteLaunchRouter = new FormsgSiteLaunchRouter({
   usersService,
   infraService,
 })
+const formsgSiteCloneRouter = new FormsgSiteCloneRouter({
+  gitFileSystemService,
+})
 
 const app = express()
 
@@ -407,6 +411,7 @@ app.use("/v2/sites/:siteName", authenticatedSitesSubrouterV2)
 // FormSG Backend handler routes
 app.use("/formsg", formsgRouter.getRouter())
 app.use("/formsg", formsgSiteLaunchRouter.getRouter())
+app.use("/formsg", formsgSiteCloneRouter.getRouter())
 
 // catch unknown routes
 app.use((req, res, next) => {

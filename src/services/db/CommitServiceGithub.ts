8f2dc5bbe8bbf711df00cb1da39ea5a688bff3d0
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-10-12 13:10:17 +0800

    feat(commitService): add commit service

---
+++ src/services/db/CommitServiceGithub.ts
@@ -0,0 +1,78 @@
+import { GrowthBook } from "@growthbook/growthbook"
+import { AxiosCacheInstance } from "axios-cache-interceptor"
+
+import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
+import { FEATURE_FLAGS } from "@root/constants"
+import { FeatureFlags } from "@root/types/featureFlags"
+import { isReduceBuildTimesWhitelistedRepo } from "@root/utils/growthbook-utils"
+
+import { isFileAsset } from "./CommitServiceGitFile"
+import GitFileSystemService from "./GitFileSystemService"
+import GitHubService, { STAGING_LITE_BRANCH } from "./GitHubService"
+
+/**
+ * Responsibilities of this class
+ * 1. Creates all commits to staging
+ * 2. Creates non-asset related commits to staging-lite
+ */
+export default class CommitServiceGitHub extends GitHubService {
+  private readonly STAGING_BRANCH = "staging"
+
+  private readonly STAGING_LITE_BRANCH = "staging-lite"
+
+  private readonly gitFileSystemService: GitFileSystemService
+
+  constructor(
+    gitFileSystemService: GitFileSystemService,
+    axiosInstance: AxiosCacheInstance
+  ) {
+    super({ axiosInstance })
+    this.gitFileSystemService = gitFileSystemService
+  }
+
+  async create(
+    sessionData: UserWithSiteSessionData,
+    {
+      content,
+      fileName,
+      directoryName,
+      isMedia = false,
+    }: {
+      content: string
+      fileName: string
+      directoryName: string
+      isMedia?: boolean
+    }
+  ) {
+    const createPromises = [
+      super.create(sessionData, {
+        content,
+        fileName,
+        directoryName,
+        isMedia,
+      }),
+    ]
+    const shouldStagingLiteUpdate =
+      isFileAsset(directoryName) &&
+      isReduceBuildTimesWhitelistedRepo(sessionData.growthbook)
+
+    // todo check if directory name is works here
+    if (shouldStagingLiteUpdate) {
+      createPromises.push(
+        super.create(sessionData, {
+          content,
+          directoryName,
+          fileName,
+          isMedia,
+          branchName: STAGING_LITE_BRANCH,
+        })
+      )
+    }
+
+    // We still await and throw error if the commit to staging-lite fails,
+    // but the caller only gets the `resultToStaging` commit returned
+    const [resultToStaging, _] = await Promise.all(createPromises)
+
+    return resultToStaging
+  }
+}

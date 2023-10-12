b9ad2ef6e9ec24e8a09097eb21931730f352d7ae
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-10-12 13:09:40 +0800

    feat(feature flag): add feature flag for quickie

--- src/utils/growthbook-utils.ts
+++ src/utils/growthbook-utils.ts
@@ -1,6 +1,7 @@
 import { GrowthBook, setPolyfills } from "@growthbook/growthbook"
 
 import config from "@root/config/config"
+import { FEATURE_FLAGS } from "@root/constants/featureFlags"
 import { FeatureFlags } from "@root/types/featureFlags"
 
 const GROWTHBOOK_API_HOST = "https://cdn.growthbook.io"
@@ -19,4 +20,17 @@ export const setBrowserPolyfills = () => {
     // Optional, can make feature rollouts faster
     EventSource: require("eventsource"),
   })
+}
+
+export const isReduceBuildTimesWhitelistedRepo = (
+  growthbook: GrowthBook<FeatureFlags> | undefined
+): boolean => {
+  if (!growthbook) return false
+
+  const isWhitelistedRedBuildTimesRepo = growthbook.getFeatureValue(
+    FEATURE_FLAGS.REDUCE_BUILD_TIMES_WHITELISTED_REPOS,
+    false
+  )
+
+  return isWhitelistedRedBuildTimesRepo
 }

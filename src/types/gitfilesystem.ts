eedda92daf5829c723a4421d3343fd7c806c552f
seaerchin <jiachin@open.gov.sg>
2023-10-12 12:20:47 +0800

    fix(gitfilesystem): revert types

--- src/types/gitfilesystem.ts
+++ src/types/gitfilesystem.ts
@@ -1,21 +1,15 @@
 export type GitFile = {
   content: string
   sha: string
-  name: string
-  type: "file"
-  path: string
-  size: number
 }
 
-export type GitDirectory = {
+export type GitDirectoryItem = {
   name: string
-  type: "dir"
+  type: "file" | "dir"
   sha: string
   path: string
   size: number
 }
-
-export type GitDirectoryItem = GitDirectory | GitFile
 
 export type GitCommitResult = {
   newSha: string

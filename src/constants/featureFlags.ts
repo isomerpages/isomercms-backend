b56fd1b6870c5b6858d73955680c60772df8ba2f
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-10-18 11:29:34 +0800

    fix(feature flags): incorrect feature flag

--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -1,4 +1,4 @@
 export const FEATURE_FLAGS = {
   GGS_WHITELISTED_REPOS: "ggs_whitelisted_repos",
-  REDUCE_BUILD_TIMES_WHITELISTED_REPOS: "reduce_build_times_whitelisted_repos",
+  IS_QUICKIE_WHITELISTED: "is_quickie_whitelisted",
 } as const

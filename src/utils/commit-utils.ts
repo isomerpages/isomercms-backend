b9a64a84eaa7829a6bc5a8a4b6be48e025c44ff8
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-10-20 14:07:37 +0800

    fix(commitService): logic for top level dir

--- src/utils/commit-utils.ts
+++ src/utils/commit-utils.ts
@@ -1,3 +1,22 @@
-export default function isFileAsset(path: string) {
-  return path.includes("images") || path.includes("files")
+import path from "path"
+
+export default function isFileAsset({
+  directoryName,
+  fileName,
+}: {
+  directoryName?: string | undefined
+  fileName?: string | undefined
+}) {
+  if (!fileName && !directoryName) return false
+
+  let filePath = ""
+  if (directoryName && fileName) {
+    filePath = path.join(directoryName, fileName)
+  } else if (fileName) {
+    filePath = fileName
+  } else if (directoryName) {
+    filePath = directoryName
+  }
+
+  return filePath.includes("images") || filePath.includes("files")
 }

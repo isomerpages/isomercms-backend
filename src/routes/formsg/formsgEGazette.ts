1703e61c388c07d0b7c56f20d622762f06bc09a0
Harish V <harish@open.gov.sg>
2023-11-04 18:11:12 +0800

    add processing logic for form submission

--- src/routes/formsg/formsgEGazette.ts
+++ src/routes/formsg/formsgEGazette.ts
@@ -7,26 +7,71 @@ import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
 import { BadRequestError } from "@root/errors/BadRequestError"
 import InitializationError from "@root/errors/InitializationError"
 import logger from "@root/logger/logger"
+import { attachFormSGHandler } from "@root/middleware"
+import S3Service from "@root/services/egazette/S3Service"
 import SearchService from "@root/services/egazette/SearchService"
 import { RequestHandler } from "@root/types"
 import { getField } from "@root/utils/formsg-utils"
 
 const EGAZETTE_FORM_KEY = config.get("formSg.eGazetteFormKey")
-const REQUESTER_EMAIL_FIELD = "Government E-mail"
-const SITE_NAME_FIELD = "Site Name"
-const REPO_NAME_FIELD = "Repository Name"
-const OWNER_NAME_FIELD = "Site Owner E-mail"
-const LOGIN_TYPE_FIELD = "Login Type"
+const EGAZETTE_S3_BUCKET = config.get("egazette.s3Bucket")
+
+const S3_URL = `https://[bucket-name].s3.[region].amazonaws.com/[object-key]
+`
+
+const formStructure = {
+  publisherEmail: "Email",
+  gazetteDetails: {
+    title: "Gazette title",
+    notificationNumber: "Gazette notification number",
+    category: "Category",
+    subCategoryGovGazette: "Sub-category (Government Gazette)",
+    subCategorySupplements: "Sub-category (Supplements)",
+    subCategoryLegislativeGazettes: "Sub-category (Legislative Gazettes)",
+    document: "Gazette document",
+  },
+}
 
 interface FormsgEGazetteRouterProps {
   searchService: SearchService
+  s3Service: S3Service
 }
+
+export type SearchRecord = {
+  objectID: string
+  title: string
+  category: string
+  subCategory: string
+  notificationNum: string
+  publishTimestamp: number
+}
+
+function toTimestamp(strDate: string) {
+  const datum = Date.parse(strDate)
+  return datum / 1000
+}
+
+function getS3ObjectUrl(
+  bucketName: string,
+  region: string,
+  objectKey: string
+): string {
+  const encodedObjectKey = encodeURIComponent(objectKey)
+  return `https://${bucketName}.s3.${region}.amazonaws.com/${encodedObjectKey}`
+}
+
+type DecryptedPayload = {
+  createdAt: string
+} & DecryptedContentAndAttachments
 
 export class FormsgEGazetteRouter {
   private readonly searchService: FormsgEGazetteRouterProps["searchService"]
 
-  constructor({ searchService }: FormsgEGazetteRouterProps) {
+  private readonly s3Service: FormsgEGazetteRouterProps["s3Service"]
+
+  constructor({ searchService, s3Service }: FormsgEGazetteRouterProps) {
     this.searchService = searchService
+    this.s3Service = s3Service
   }
 
   formsgEGazettePublish: RequestHandler<
@@ -34,9 +79,155 @@ export class FormsgEGazetteRouter {
     Record<string, never>,
     { data: { submissionId: string } },
     never,
-    { submission: DecryptedContentAndAttachments }
+    { submission: DecryptedPayload }
   > = async (req, res) => {
     logger.info("Received egazette publish request")
+
+    const { responses } = res.locals.submission.content
+
+    // Retrieve form details
+
+    // Gazette Notification Number
+    const gazetteNotificationNum = getField(
+      responses,
+      formStructure.gazetteDetails.notificationNumber
+    )
+    if (!gazetteNotificationNum) {
+      logger.error(
+        "Gazette notification number is missing in form submission. Skip processing..."
+      )
+    }
+
+    // Publisher Email
+    const publisherEmail = getField(responses, formStructure.publisherEmail)
+    if (!publisherEmail) {
+      logger.error(
+        "Missing publisher email in form submission. Skip processing..."
+      )
+    }
+
+    // Gazette Title
+    const gazetteTitle = getField(responses, formStructure.gazetteDetails.title)
+    if (!gazetteTitle) {
+      logger.error("Missing title for the gazette. Skipping...")
+    }
+
+    // Publish Time
+    const publishTime = res.locals.submission.createdAt
+    if (!publishTime) {
+      logger.error("No publish time found. Skipping...")
+    }
+
+    // Gazette Category
+    const gazetteCategory = getField(
+      responses,
+      formStructure.gazetteDetails.category
+    )
+    if (!gazetteCategory) {
+      logger.error("Missing category for the gazette. Skipping...")
+    }
+
+    // Gazette Sub-Category
+    let gazetteSubCategory = null
+    switch (gazetteCategory) {
+      case "Government Gazette":
+        gazetteSubCategory = getField(
+          responses,
+          formStructure.gazetteDetails.subCategoryGovGazette
+        )
+        break
+      case "Supplements":
+        gazetteSubCategory = getField(
+          responses,
+          formStructure.gazetteDetails.subCategorySupplements
+        )
+        break
+      case "Legislative Gazettes":
+        gazetteSubCategory = getField(
+          responses,
+          formStructure.gazetteDetails.subCategoryLegislativeGazettes
+        )
+        break
+      default:
+        break
+    }
+
+    if (
+      (gazetteCategory === "Government Gazette" ||
+        gazetteCategory === "Supplements" ||
+        gazetteCategory === "Legislative Gazettes") &&
+      !gazetteSubCategory
+    ) {
+      logger.error(`Missing sub-category for ${gazetteCategory}. Skipping...`)
+    }
+
+    console.log(`Aggregated details from subsmission`, {
+      publisherEmail,
+      publishTime,
+      gazetteTitle,
+      gazetteCategory,
+      gazetteSubCategory,
+      gazetteNotificationNum,
+    })
+
+    const objectKey = gazetteSubCategory
+      ? `${gazetteCategory}/${gazetteSubCategory}/${gazetteNotificationNum}.pdf`
+      : `${gazetteCategory}/${gazetteNotificationNum}.pdf`
+    const { attachments } = res.locals.submission
+
+    // Should only have one attachment
+    if (Object.keys(attachments).length !== 1) {
+      // TODO: Add custom error type
+      throw new Error("More than 1 attachment found!")
+    }
+
+    for (const [key, value] of Object.entries(attachments)) {
+      if (value) {
+        try {
+          const uploadResponse = await this.s3Service.uploadBlob(
+            EGAZETTE_S3_BUCKET,
+            objectKey,
+            value.content
+          )
+          console.log(`Received upload response from S3Service`, uploadResponse)
+        } catch (err) {
+          logger.error(
+            `Uploading gazette to S3 Bucket failed with error: ${JSON.stringify(
+              err
+            )}`
+          )
+          console.log(`S3 err`, err)
+        }
+      }
+    }
+    console.log(`raw data`, attachments)
+
+    // Add to search index
+    // NOTE: Using `!` here to force unwrap as validation has been done above
+    const newSearchRecord = {
+      category: gazetteCategory!,
+      subCategory: gazetteSubCategory || "",
+      notificationNum: gazetteNotificationNum!,
+      title: gazetteTitle!,
+      publishDate: publishTime!,
+      publishTimestamp: toTimestamp(publishTime), // Dates must be Unix timestamps for searchability
+      fileUrl: getS3ObjectUrl(
+        EGAZETTE_S3_BUCKET,
+        config.get("aws.region"),
+        objectKey
+      ),
+      objectID: gazetteNotificationNum!,
+    }
+
+    console.log(`Adding record to search index`, newSearchRecord)
+
+    // publish to index
+    try {
+      await this.searchService.addToIndex(newSearchRecord)
+    } catch (e) {
+      console.log(e)
+    }
+
     return res.sendStatus(200)
   }
 
@@ -49,9 +240,10 @@ export class FormsgEGazetteRouter {
     }
     router.post(
       "/publish-gazette",
-      // attachFormSGHandler(GGS_REPAIR_FORM_KEY),
+      attachFormSGHandler(EGAZETTE_FORM_KEY),
       this.formsgEGazettePublish
     )
+
     return router
   }
 }

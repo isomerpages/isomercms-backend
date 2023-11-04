f6d9a3765e3e6c6d9a5864ea0e278f9aa852f876
Harish V <harish@open.gov.sg>
2023-11-04 17:09:25 +0800

    minor updates to search svc

--- src/services/egazette/SearchService.ts
+++ src/services/egazette/SearchService.ts
@@ -1,16 +1,55 @@
-import algoliasearch, { SearchClient } from "algoliasearch"
+import algoliasearch, { SearchClient, SearchIndex } from "algoliasearch"
 
 import logger from "@root/logger/logger"
+// TODO: Update import
+import { SearchRecord } from "@root/routes/formsg/formsgEGazette"
 
 class SearchService {
   private searchClient: SearchClient
 
+  private searchIndex: SearchIndex
+
   constructor(client: SearchClient) {
     this.searchClient = client
+    this.searchIndex = this.searchClient.initIndex("ogp-egazettes")
+    this.setUpIndex()
   }
 
-  searchGazettes = async () => {
+  private setUpIndex = async () => {
+    try {
+      await this.searchIndex.setSettings({
+        // NOTE: This is in order of priority
+        searchableAttributes: [
+          "title",
+          "category",
+          "subCategory",
+          "publishDate,notificationNum",
+        ],
+      })
+    } catch (e) {
+      logger.error(
+        `Error while setting search index settings: ${JSON.stringify(e)}`
+      )
+    }
+  }
+
+  searchGazettes = async (query: string) => {
     logger.info("Search for gazettes")
+    try {
+      const hits = await this.searchIndex.search(query)
+      logger.info(`Received hits`)
+      logger.info(hits)
+    } catch (e) {
+      logger.error(`Error while searching: ${JSON.stringify(e)}`)
+    }
+  }
+
+  addToIndex = async (record: SearchRecord) => {
+    try {
+      await this.searchIndex.saveObject(record)
+    } catch (e) {
+      logger.error(`Error while adding to index: ${JSON.stringify(e)}`)
+    }
   }
 }
 

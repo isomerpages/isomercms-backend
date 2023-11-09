f9b20cc2145c1cef357551540813fa0dba3566f1
Kishore <42832651+kishore03109@users.noreply.github.com>
2023-11-09 09:05:43 +0800

    fix(quickie): short circuit when push to stg fail

--- src/services/db/GithubCommitService.ts
+++ src/services/db/GithubCommitService.ts
@@ -3,7 +3,6 @@ import { AxiosCacheInstance } from "axios-cache-interceptor"
 import GithubSessionData from "@root/classes/GithubSessionData"
 import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
 import { STAGING_BRANCH, STAGING_LITE_BRANCH } from "@root/constants"
-import { ConflictError } from "@root/errors/ConflictError"
 import { GitCommitResult } from "@root/types/gitfilesystem"
 import isFileAsset from "@root/utils/commit-utils"
 import { isReduceBuildTimesWhitelistedRepo } from "@root/utils/growthbook-utils"
@@ -35,35 +34,29 @@ export default class GitHubCommitService extends GitHubService {
   ): Promise<{
     sha: string
   }> {
-    const createPromises = [
-      super.create(sessionData, {
-        content,
-        fileName,
-        directoryName,
-        isMedia,
-        branchName: STAGING_BRANCH,
-      }),
-    ]
+    const resultToStaging = await super.create(sessionData, {
+      content,
+      fileName,
+      directoryName,
+      isMedia,
+      branchName: STAGING_BRANCH,
+    })
+
     const shouldStagingLiteUpdate =
       !isFileAsset({ directoryName, fileName }) &&
       isReduceBuildTimesWhitelistedRepo(sessionData.growthbook)
 
     if (shouldStagingLiteUpdate) {
-      createPromises.push(
-        super.create(sessionData, {
-          content,
-          directoryName,
-          fileName,
-          isMedia,
-          branchName: STAGING_LITE_BRANCH,
-        })
-      )
+      // We still await and throw error if the commit to staging-lite fails,
+      // but the caller only gets the `resultToStaging` commit returned
+      await super.create(sessionData, {
+        content,
+        directoryName,
+        fileName,
+        isMedia,
+        branchName: STAGING_LITE_BRANCH,
+      })
     }
-
-    // We still await and throw error if the commit to staging-lite fails,
-    // but the caller only gets the `resultToStaging` commit returned
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [resultToStaging, _] = await Promise.all(createPromises)
 
     return resultToStaging
   }
@@ -82,34 +75,29 @@ export default class GitHubCommitService extends GitHubService {
       directoryName: string | undefined
     }
   ): Promise<GitCommitResult> {
-    const updatePromises = [
-      super.update(sessionData, {
-        fileContent,
-        sha,
-        fileName,
-        directoryName,
-        branchName: STAGING_BRANCH,
-      }),
-    ]
+    const resultToStaging = await super.update(sessionData, {
+      fileContent,
+      sha,
+      fileName,
+      directoryName,
+      branchName: STAGING_BRANCH,
+    })
+
     const shouldStagingLiteUpdate =
       !isFileAsset({ directoryName, fileName }) &&
       isReduceBuildTimesWhitelistedRepo(sessionData.growthbook)
 
     if (shouldStagingLiteUpdate) {
-      updatePromises.push(
-        super.update(sessionData, {
-          fileContent,
-          sha,
-          fileName,
-          directoryName,
-          branchName: STAGING_LITE_BRANCH,
-        })
-      )
+      // We still await and throw error if the commit to staging-lite fails,
+      // but the caller only gets the `resultToStaging` commit returned
+      await super.update(sessionData, {
+        fileContent,
+        sha,
+        fileName,
+        directoryName,
+        branchName: STAGING_LITE_BRANCH,
+      })
     }
-
-    // We still await and throw error if the commit to staging-lite fails,
-    // but the caller only gets the `resultToStaging` commit returned
-    const [resultToStaging, _] = await Promise.all(updatePromises)
 
     return resultToStaging
   }
@@ -128,57 +116,24 @@ export default class GitHubCommitService extends GitHubService {
       isStaging?: boolean
     }
   ): Promise<void> {
-    // GitHub flow
-    const gitTree = await this.getTree(
-      sessionData,
+    await super.deleteDirectory(sessionData, {
+      directoryName,
+      message,
       githubSessionData,
-      {
-        isRecursive: true,
-      },
-      isStaging
-    )
-
-    // Retrieve removed items and set their sha to null
-    const newGitTree = gitTree
-      .filter(
-        (item) =>
-          item.path.startsWith(`${directoryName}/`) && item.type !== "tree"
-      )
-      .map((item) => ({
-        ...item,
-        sha: null,
-      }))
-
-    const newCommitSha = await this.updateTree(
-      sessionData,
-      githubSessionData,
-      {
-        gitTree: newGitTree,
-        message,
-      },
-      !!isStaging
-    )
-
-    const deletePromises = [
-      this.updateRepoState(sessionData, {
-        commitSha: newCommitSha,
-      }),
-    ]
+      isStaging,
+    })
 
     if (
       isReduceBuildTimesWhitelistedRepo(sessionData.growthbook) &&
       !isFileAsset({ directoryName })
     ) {
-      deletePromises.push(
-        this.deleteDirectory(sessionData, {
-          directoryName,
-          message,
-          githubSessionData,
-          isStaging: false,
-        })
-      )
+      await this.deleteDirectory(sessionData, {
+        directoryName,
+        message,
+        githubSessionData,
+        isStaging: false,
+      })
     }
-    await Promise.all(deletePromises)
   }
 
   async delete(
@@ -193,28 +148,23 @@ export default class GitHubCommitService extends GitHubService {
       directoryName: string
     }
   ): Promise<void> {
-    const deletePromises = [
-      super.delete(sessionData, {
-        sha,
-        fileName,
-        directoryName,
-      }),
-    ]
+    await super.delete(sessionData, {
+      sha,
+      fileName,
+      directoryName,
+    })
+
     const shouldStagingLiteUpdate =
       !isFileAsset({ directoryName, fileName }) &&
       isReduceBuildTimesWhitelistedRepo(sessionData.growthbook)
     if (shouldStagingLiteUpdate) {
-      deletePromises.push(
-        super.delete(sessionData, {
-          sha,
-          fileName,
-          directoryName,
-          branchName: STAGING_LITE_BRANCH,
-        })
-      )
+      await super.delete(sessionData, {
+        sha,
+        fileName,
+        directoryName,
+        branchName: STAGING_LITE_BRANCH,
+      })
     }
-
-    await Promise.all(deletePromises)
   }
 
   async renameSinglePath(

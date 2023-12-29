7e25a320b8ba859d6f8444dd87b625ed75bccb4d
Qilu Xie <qilu@open.gov.sg>
2023-12-29 17:59:28 +0800

    fixed import for test files

--- src/integration/NotificationOnEditHandler.spec.ts
+++ src/integration/NotificationOnEditHandler.spec.ts
@@ -30,8 +30,8 @@ import {
   MOCK_PULL_REQUEST_FILECHANGEINFO_ONE,
   MOCK_PULL_REQUEST_FILECHANGEINFO_TWO,
 } from "@root/fixtures/review"
-import { BaseDirectoryService } from "@root/services/directoryServices/BaseDirectoryService"
-import { ResourceRoomDirectoryService } from "@root/services/directoryServices/ResourceRoomDirectoryService"
+import BaseDirectoryService from "@root/services/directoryServices/BaseDirectoryService"
+import ResourceRoomDirectoryService from "@root/services/directoryServices/ResourceRoomDirectoryService"
 import { CollectionPageService } from "@root/services/fileServices/MdPageServices/CollectionPageService"
 import { ContactUsPageService } from "@root/services/fileServices/MdPageServices/ContactUsPageService"
 import { HomepagePageService } from "@root/services/fileServices/MdPageServices/HomepagePageService"

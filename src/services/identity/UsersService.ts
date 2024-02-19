686a61e66efae3202e1bfac9ce5ebce6c24f591d
seaerchin <jiachin@open.gov.sg>
2024-02-19 17:59:49 +0800

    fix(vapt): delete vapt related stuff for ez merge

--- src/services/identity/UsersService.ts
+++ src/services/identity/UsersService.ts
@@ -162,13 +162,6 @@ class UsersService {
       })
       user.lastLoggedIn = new Date()
 
-      if (
-        parsedEmail.endsWith("@cure53.de") ||
-        parsedEmail.endsWith("@volt.cure53.de")
-      ) {
-        user.contactNumber = "12345678"
-      }
-
       return user.save({ transaction })
     })
   }

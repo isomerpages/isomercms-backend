54fa178a1a756d098dd49ecd88b57c8ec44f617c
Alexander Lee <alexander@open.gov.sg>
2024-02-21 18:54:03 +0800

    chore: use url-template for all endpoints with user determined params

--- src/services/db/review.ts
+++ src/services/db/review.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from "axios"
 import _ from "lodash"
+import urlTemplate from "url-template"
 
 import { config } from "@config/config"
 
@@ -19,12 +20,15 @@ export const getCommitDiff = async (
   siteName: string,
   base = "master",
   head = "staging"
-) =>
-  axiosInstance
-    .get<{ files: RawFileChangeInfo[]; commits: Commit[] }>(
-      `${siteName}/compare/${base}...${head}`
-    )
+) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/compare/{base}...{head}`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, base, head })
+  return axiosInstance
+    .get<{ files: RawFileChangeInfo[]; commits: Commit[] }>(endpoint)
     .then(({ data }) => data)
+}
 
 export const createPullRequest = (
   siteName: string,
@@ -32,56 +36,85 @@ export const createPullRequest = (
   description?: string,
   base = "master",
   head = "staging"
-) =>
-  axiosInstance
+) => {
+  const endpointTemplate = urlTemplate.parse(`{siteName}/pulls`)
+  const endpoint = endpointTemplate.expand({ siteName })
+  return axiosInstance
     .post<{ number: number }>(
-      `${siteName}/pulls`,
+      endpoint,
       // NOTE: only create body if a valid description is given
       { title, base, head, ...(description && { body: description }) }
     )
     .then(({ data }) => data.number)
+}
 
-export const getPullRequest = (siteName: string, pullRequestNumber: number) =>
-  axiosInstance
-    .get<RawPullRequest>(`${siteName}/pulls/${pullRequestNumber}`)
-    .then(({ data }) => data)
+export const getPullRequest = (siteName: string, pullRequestNumber: number) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/pulls/{pullRequestNumber}`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, pullRequestNumber })
+  return axiosInstance.get<RawPullRequest>(endpoint).then(({ data }) => data)
+}
 
 export const updatePullRequest = (
   siteName: string,
   pullRequestNumber: number,
   title: string,
   description?: string
-) =>
-  axiosInstance.patch<void>(
-    `${siteName}/pulls/${pullRequestNumber}`,
+) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/pulls/{pullRequestNumber}`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, pullRequestNumber })
+  return axiosInstance.patch<void>(
+    endpoint,
     // NOTE: only create body if a valid description is given
     { title, ...(description !== undefined && { body: description }) }
   )
+}
 
 export const closeReviewRequest = (
   siteName: string,
   pullRequestNumber: number
-) =>
-  axiosInstance.patch<void>(
-    `${siteName}/pulls/${pullRequestNumber}`,
+) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/pulls/{pullRequestNumber}`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, pullRequestNumber })
+  return axiosInstance.patch<void>(
+    endpoint,
     // NOTE: only create body if a valid description is given
     { state: "closed" }
   )
+}
 
 export const mergePullRequest = (
   repoNameInGithub: string,
   pullRequestNumber: number
-) =>
-  axiosInstance.put<void>(
-    `${repoNameInGithub}/pulls/${pullRequestNumber}/merge`
+) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{repoNameInGithub}/pulls/{pullRequestNumber}/merge`
   )
+  const endpoint = endpointTemplate.expand({
+    repoNameInGithub,
+    pullRequestNumber,
+  })
+  return axiosInstance.put<void>(endpoint)
+}
 
 export const approvePullRequest = (
   repoNameInGithub: string,
   pullRequestNumber: number
-) =>
-  axiosInstance.post<void>(
-    `${repoNameInGithub}/pulls/${pullRequestNumber}/reviews`,
+) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{repoNameInGithub}/pulls/{pullRequestNumber}/reviews`
+  )
+  const endpoint = endpointTemplate.expand({
+    repoNameInGithub,
+    pullRequestNumber,
+  })
+  return axiosInstance.post<void>(
+    endpoint,
     {
       event: "APPROVE",
     },
@@ -99,13 +132,18 @@ export const approvePullRequest = (
       },
     }
   )
+}
 
 export const getComments = async (
   siteName: string,
   pullRequestNumber: number
 ) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/issues/{pullRequestNumber}/comments`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, pullRequestNumber })
   const rawComments = await axiosInstance
-    .get<RawComment[]>(`${siteName}/issues/${pullRequestNumber}/comments`)
+    .get<RawComment[]>(endpoint)
     .then(({ data }) => data)
   return _.compact(
     rawComments.map((rawComment) => {
@@ -128,23 +166,26 @@ export const createComment = async (
   userId: string,
   message: string
 ) => {
+  const endpointTemplate = urlTemplate.parse(
+    `{siteName}/pulls/{pullRequestNumber}/comments`
+  )
+  const endpoint = endpointTemplate.expand({ siteName, pullRequestNumber })
   const stringifiedMessage = JSON.stringify({
     userId,
     message,
   })
-  return axiosInstance.post<void>(
-    `${siteName}/issues/${pullRequestNumber}/comments`,
-    { body: stringifiedMessage }
-  )
+  return axiosInstance.post<void>(endpoint, { body: stringifiedMessage })
 }
 
 export const getBlob = async (
   repo: string,
   path: string,
   ref: string
-): Promise<string> =>
-  axiosInstance
-    .get<string>(`${repo}/contents/${path}?ref=${ref}`, {
+): Promise<string> => {
+  const endpointTemplate = urlTemplate.parse(`{repo}/contents/{path}?ref={ref}`)
+  const endpoint = endpointTemplate.expand({ repo, path, ref })
+  return axiosInstance
+    .get<string>(endpoint, {
       headers: {
         Accept: "application/vnd.github.raw",
       },
@@ -158,3 +199,4 @@ export const getBlob = async (
       throw err
     })
     .then(({ data }) => data)
+}

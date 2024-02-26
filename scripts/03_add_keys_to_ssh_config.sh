1a4d561dfdeeab73f851dc0c4b313a2f35c26194
dcshzj <27919917+dcshzj@users.noreply.github.com>
2024-02-26 16:24:00 +0800

    fix(docker): ensure that the app runs with webapp

--- scripts/03_add_keys_to_ssh_config.sh
+++ scripts/03_add_keys_to_ssh_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # add github as host and ssh key to ssh config
-cat << EOF > /root/.ssh/config
+cat << EOF > /home/webapp/.ssh/config
 Host github.com
-  IdentityFile /root/.ssh/github
+  IdentityFile /home/webapp/.ssh/github
   User git
 EOF

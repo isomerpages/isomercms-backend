899b8107929e475c53ab536791ce2e79890a76a8
seaerchin <jiachin@open.gov.sg>
2024-04-04 13:24:07 +0800

    feat(support): connect db

--- support/index.ts
+++ support/index.ts
@@ -2,7 +2,7 @@ import "module-alias/register"
 
 import express from "express"
 
-import { infraService } from "@common/index"
+import { infraService, sequelize } from "@common/index"
 import { useSharedMiddleware } from "@common/middleware"
 import { config } from "@root/config/config"
 import logger from "@root/logger/logger"
@@ -18,11 +18,23 @@ infraService.pollMessages()
 
 useSharedMiddleware(app)
 
-// TODO: prefix under infra
-// FormSG Backend handler routes
 app.use("/", v2Router)
 app.use("/v2/ping", (req, res) => res.status(200).send("Ok"))
 
-app.listen(PORT, () => {
-  logger.info(`Infra container started on port ${PORT}`)
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    logger.info(
+      "Connection to db has been established successfully on support service."
+    )
+    app.listen(PORT, () => {
+      logger.info(`Infra container started on port ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    logger.error(`Unable to connect to the database: ${err}`)
+
+    // If we cannot connect to the db, report an error using status code
+    // And gracefully shut down the application since we can't serve client
+    process.exit(1)
+  })

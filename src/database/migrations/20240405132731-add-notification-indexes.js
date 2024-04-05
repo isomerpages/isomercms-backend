ecffb0422ce8cb04ac743ff8f626166e7c41e374
Alexander Lee <alexander@open.gov.sg>
2024-04-05 17:11:39 +0800

    fix: await addindex separately

--- src/database/migrations/20240405132731-add-notification-indexes.js
+++ src/database/migrations/20240405132731-add-notification-indexes.js
@@ -1,44 +1,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    queryInterface.sequelize.transaction((t) =>
-      Promise.all([
-        queryInterface.addIndex(
-          "notifications", // name of Source model
-          ["site_id", "user_id"],
-          {
-            transaction: t,
-          }
-        ),
-        queryInterface.addIndex(
-          "notifications", // name of Source model
-          ["user_id"],
-          {
-            transaction: t,
-          }
-        ),
-      ])
+    await queryInterface.addIndex(
+      "notifications", // name of Source model
+      ["site_id", "user_id"]
+    )
+    await queryInterface.addIndex(
+      "notifications", // name of Source model
+      ["user_id"]
     )
   },
 
   async down(queryInterface, Sequelize) {
-    queryInterface.sequelize.transaction((t) =>
-      Promise.all([
-        queryInterface.removeIndex(
-          "notifications", // name of Source model
-          ["site_id", "user_id"],
-          {
-            transaction: t,
-          }
-        ),
-        queryInterface.removeIndex(
-          "notifications", // name of Source model
-          ["user_id"],
-          {
-            transaction: t,
-          }
-        ),
-      ])
+    await queryInterface.removeIndex(
+      "notifications", // name of Source model
+      ["site_id", "user_id"]
+    )
+    await queryInterface.removeIndex(
+      "notifications", // name of Source model
+      ["user_id"]
     )
   },
 }

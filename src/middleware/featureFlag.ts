fe0b3ad9be11bc97401b562780058552a336bdc1
Timothee Groleau <timothee@open.gov.sg>
2024-04-25 09:50:22 +0800

    chore(growthbook): handle refreshes at module level (#1228)
    * chore(growthbook): handle refreshes at module level
    
    * chore: update growthbook
    
    * feat: use the new GrowthBook instantiation style

--- src/middleware/featureFlag.ts
+++ src/middleware/featureFlag.ts
@@ -1,8 +1,22 @@
 import UserWithSiteSessionData from "@root/classes/UserWithSiteSessionData"
-import config from "@root/config/config"
+import { config } from "@root/config/config"
 import logger from "@root/logger/logger"
 import { RequestHandlerWithGrowthbook } from "@root/types"
 import { getNewGrowthbookInstance } from "@root/utils/growthbook-utils"
+
+// Keep one GrowthBook instance at module level
+// The instance will handle internal cache refreshes via a SSE connection
+const gb = getNewGrowthbookInstance({
+  clientKey: config.get("growthbook.clientKey"),
+  subscribeToChanges: true,
+})
+
+gb.loadFeatures().catch((e: unknown) => {
+  logger.error({
+    error: e,
+    message: "Failed to load features from GrowthBook at startup",
+  })
+})
 
 // eslint-disable-next-line import/prefer-default-export
 export const featureFlagMiddleware: RequestHandlerWithGrowthbook<
@@ -12,7 +26,10 @@ export const featureFlagMiddleware: RequestHandlerWithGrowthbook<
   never,
   { userWithSiteSessionData: UserWithSiteSessionData }
 > = async (req, res, next) => {
-  req.growthbook = getNewGrowthbookInstance(config.get("growthbook.clientKey"))
+  req.growthbook = getNewGrowthbookInstance({
+    clientKey: config.get("growthbook.clientKey"),
+    subscribeToChanges: false, // request-level instances should have immutable flag values
+  })
 
   // Clean up at the end of the request
   res.on("close", () => {
@@ -21,12 +38,12 @@ export const featureFlagMiddleware: RequestHandlerWithGrowthbook<
 
   // Wait for features to load (will be cached in-memory for future requests)
   req.growthbook
-    .loadFeatures({ autoRefresh: true })
-    .then(() => next())
+    .loadFeatures()
     .catch((e: unknown) => {
-      logger.error(
-        `Failed to load features from GrowthBook: ${JSON.stringify(e)}`
-      )
-      next()
+      logger.error({
+        error: e,
+        message: "Failed to load features from GrowthBook",
+      })
     })
+    .then(() => next())
 }

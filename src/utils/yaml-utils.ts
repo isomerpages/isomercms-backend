ada178a3a33050d3058860dd89f259b165424871
Alexander Lee <alexander@open.gov.sg>
2024-04-27 16:20:52 +0800

    chore: add util method to trim and replace unicode line breaks

--- src/utils/yaml-utils.ts
+++ src/utils/yaml-utils.ts
@@ -2,6 +2,26 @@ import yaml from "yaml"
 
 import { sanitizer } from "@services/utilServices/Sanitizer"
 
+const LINE_BREAK_REGEX = "[\r\n\x0B\x0C\u0085\u2028\u2029]"
+const GLOBAL_LINE_BREAK_SEARCH = new RegExp(LINE_BREAK_REGEX, "gi")
+
+export const recursiveTrimAndReplaceLineBreaks = (obj: any): any => {
+  if (typeof obj === "string") {
+    return obj.replaceAll(GLOBAL_LINE_BREAK_SEARCH, " ").trim()
+  }
+  if (Array.isArray(obj)) {
+    return obj.map((item) => recursiveTrimAndReplaceLineBreaks(item))
+  }
+  if (typeof obj === "object" && obj !== null) {
+    const newObj: any = {}
+    Object.keys(obj).forEach((key) => {
+      newObj[key] = recursiveTrimAndReplaceLineBreaks(obj[key])
+    })
+    return newObj
+  }
+  return obj
+}
+
 // Note: `yaml.parse()` and `yaml.stringify()` should not be used anywhere
 // else in the codebase.
 export const sanitizedYamlParse = (
@@ -17,7 +37,13 @@ export const sanitizedYamlParse = (
       : value
   )
 
-export const sanitizedYamlStringify = (prestringifiedContent: object): string =>
-  yaml.stringify(prestringifiedContent, (key, value) =>
+export const sanitizedYamlStringify = (
+  prestringifiedContent: object
+): string => {
+  const formattedPrestringifiedContent = recursiveTrimAndReplaceLineBreaks(
+    prestringifiedContent
+  )
+  return yaml.stringify(formattedPrestringifiedContent, (key, value) =>
     typeof value === "string" && !!value ? sanitizer.sanitize(value) : value
   )
+}

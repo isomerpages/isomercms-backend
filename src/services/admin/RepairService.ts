442aa5224e420acaa0d131a8dd00ef0d44dd88f3
seaerchin <jiachin@open.gov.sg>
2024-05-28 03:28:36 +0800

    feat: endpoints
    add endpoints for cloning and locking

--- src/services/admin/RepairService.ts
+++ src/services/admin/RepairService.ts
@@ -1,8 +1,8 @@
 import path from "path"
 
-import { ResultAsync, errAsync, fromPromise, okAsync } from "neverthrow"
+import { fromPromise, ResultAsync } from "neverthrow"
 
-import { lock, unlock } from "@utils/mutex-utils"
+import { lock } from "@utils/mutex-utils"
 
 import { EFS_VOL_PATH_STAGING_LITE } from "@root/constants"
 import GitFileSystemError from "@root/errors/GitFileSystemError"
@@ -13,44 +13,42 @@ import GitFileSystemService from "../db/GitFileSystemService"
 
 const LOCK_TIME_SECONDS = 15 * 60 // 15 minutes
 
+// TODO: Add class constructor and update these to be deps
 const gitFileSystemService: GitFileSystemService = (null as unknown) as GitFileSystemService
 const reposService: ReposService = (null as unknown) as ReposService
 
-export const lockRepo = (repoName: string) =>
+export const lockRepo = (
+  repoName: string,
+  lockDurationSeconds: number = LOCK_TIME_SECONDS
+) =>
   ResultAsync.fromPromise(
-    lock(repoName, LOCK_TIME_SECONDS),
+    lock(repoName, lockDurationSeconds),
     (err) => new LockedError(`Unable to lock repo ${repoName}, ${err}`)
   ).map(() => repoName)
 
-export const unlockRepo = (repoName: string) => {}
-
-export const cloneRepo = (repoName: string, lockRepo = true) => {
-  if (lockRepo) {
-    lock(repoName)
-  }
-
+export const cloneRepo = (repoName: string) => {
   const repoUrl = `git@github.com:isomerpages/${repoName}.git`
 
-  gitFileSystemService
-    // Repo does not exist in EFS, clone it
-    .cloneBranch(repoName, true)
-    .andThen(() =>
-      // repo exists in efs, but we need to pull for staging and reset staging lite
-      gitFileSystemService
-        .pull(repoName, "staging")
-        .andThen(() =>
-          fromPromise(
-            reposService.setUpStagingLite(
-              path.join(EFS_VOL_PATH_STAGING_LITE, repoName),
-              repoUrl
-            ),
-            (error) =>
-              new GitFileSystemError(
-                `Error setting up staging lite for repo ${repoName}: ${error}`
-              )
+  return (
+    gitFileSystemService
+      .cloneBranch(repoName, true)
+      // Repo does not exist in EFS, clone it
+      .andThen(() =>
+        // repo exists in efs, but we need to pull for staging and reset staging lite
+        gitFileSystemService
+          .pull(repoName, "staging")
+          .andThen(() =>
+            fromPromise(
+              reposService.setUpStagingLite(
+                path.join(EFS_VOL_PATH_STAGING_LITE, repoName),
+                repoUrl
+              ),
+              (error) =>
+                new GitFileSystemError(
+                  `Error setting up staging lite for repo ${repoName}: ${error}`
+                )
+            )
           )
-        )
-    )
-    // TODO: update this to actually send a message to slack
-    .andThen(() => sendSlackMessage(repoName))
+      )
+  )
 }
--- support/routes/v2/isobot/index.ts
+++ support/routes/v2/isobot/index.ts
@@ -1,15 +1,16 @@
 import {
   App,
-  AuthorizationError,
   ExpressReceiver,
   Middleware,
   SlackCommandMiddlewareArgs,
 } from "@slack/bolt"
 import { RequestHandler } from "express"
+import { okAsync } from "neverthrow"
 
 import { Whitelist } from "@database/models"
 import config from "@root/config/config"
 import logger from "@root/logger/logger"
+import { cloneRepo, lockRepo } from "@root/services/admin/RepairService"
 import WhitelistService from "@root/services/identity/WhitelistService"
 
 import BotService from "./ops/botService"
@@ -25,14 +26,109 @@ export const isobotRouter = botReceiver.router
 
 // TODO: add slack ids of isomer user
 const ISOMER_USERS_ID = ["U01HTSFC0RY"]
+const BOT_AUDIT_CHANNEL_ID = "C075Z617GCQ"
 const bot = new App({
   token,
   receiver: botReceiver,
 })
 
-// TODO: add in validation for user once downstream is merged
-bot.command("/whitelist", async ({ payload, respond, ack }) => {
-  await ack()
+const cloneRepoCommmand = bot.command(
+  "/clone",
+  async ({ command, ack, respond, payload, client }) => {
+    await ack()
+
+    const HELP_TEXT =
+      "Usage: `/clone <github_repo_name>`. Take note that this locks the repo for 15 minutes by default. To bypass this behaviour, add `-n` at the end of the command"
+    const tokens = command.text.split(" ")
+
+    const hasUnrecognisedLastToken = tokens.length === 2 && tokens[1] !== "-n"
+    if (tokens.length < 1 || hasUnrecognisedLastToken) {
+      return respond(HELP_TEXT)
+    }
+
+    // NOTE: Invariant maintained:
+    // 1. we always need 0 < tokens.length < 3
+    // 2. token at index 0 is always the github repository
+    // 3. token at index 1 is always the "-n" option
+    const isHelp = tokens[0].toLowerCase() === "help"
+    if (isHelp) {
+      return respond(HELP_TEXT)
+    }
+
+    const repo = tokens[0]
+    const shouldLock = tokens.length === 2 && tokens[1] === "-n"
+    await client.chat.postMessage({
+      channel: BOT_AUDIT_CHANNEL_ID,
+      text: `${payload.user_id} attempting to clone repo: ${repo} to EFS. Should lock: ${shouldLock}`,
+    })
+
+    const base = shouldLock ? lockRepo(tokens[0]) : okAsync("")
+    return base
+      .andThen(cloneRepo)
+      .map(() => respond(`${repo} was successfully cloned to efs!`))
+      .mapErr((e) => respond(`${e} occurred while cloning repo to efs`))
+  }
+)
+
+const lockRepoCommand = bot.command(
+  "/lock",
+  async ({ command, ack, respond, payload, client }) => {
+    await ack()
+
+    const HELP_TEXT =
+      "Usage: `/lock <github_repo_name> -d <duration_in_minutes>`. Take note that this locks the repo for 15 minutes by default if `-d` is not specified"
+    const tokens = command.text.split(" ")
+    // NOTE: Invariant maintained:
+    // 1. tokens.length === 1 || tokens.length === 3
+    // 2. token at index 0 is always the github repository
+    // 3. if tokens.length === 3, then the last element must not be `NaN`
+    const isShortCommand = tokens.length === 1
+    const isLongCommand =
+      tokens.length === 3 &&
+      !Number.isNaN(parseInt(tokens[2], 10)) &&
+      tokens[1] === "-d"
+    if (!isShortCommand || !isLongCommand) {
+      return respond(HELP_TEXT)
+    }
+
+    const repo = tokens[0]
+    const lockTimeMinutes = isLongCommand ? parseInt(tokens[2], 10) : 15
+    const lockTimeSeconds = lockTimeMinutes * 60
+    await client.chat.postMessage({
+      channel: BOT_AUDIT_CHANNEL_ID,
+      text: `${payload.user_id} attempting to lock repo: ${repo} for ${lockTimeMinutes}`,
+    })
+
+    return lockRepo(repo, lockTimeSeconds)
+      .map((repo) => {
+        respond(
+          `${repo} was successfully locked for ${lockTimeMinutes} minutes!`
+        )
+      })
+      .mapErr((e) => respond(`${e} occurred while attempting to lock repo`))
+  }
+)
+
+const handleWhitelistEmails: RequestHandler<
+  {},
+  { message: string },
+  SlackPayload,
+  {},
+  {}
+> = async (req, res) => {
+  console.log(req.headers)
+  const slackTimestamp = req.headers["x-slack-request-timestamp"] as string
+  const slackSig = req.headers["x-slack-signature"] as string
+
+  if (!slackTimestamp || !slackSig)
+    return res.send({ message: "Missing header/signature" })
+
+  const isVerifiedMessage = botService.verifySignature(
+    slackSig,
+    slackTimestamp,
+    req.body
+  )
+  if (!isVerifiedMessage) return res.send({ message: "Invalid signature" })
 
   try {
     await botService.whitelistEmails(payload.text)
@@ -69,18 +165,24 @@ const addUserContext: Middleware<SlackCommandMiddlewareArgs> = async ({
   await next()
 }
 
-// NOTE: ALWAYS call this after `addUserContext`
 const validateIsomerUser: Middleware<SlackCommandMiddlewareArgs> = async ({
+  payload,
   client,
-  context,
   next,
 }) => {
   // NOTE: Not calling `client.get` again - repeated work and also
   // we only have a 3s window to ACK slack (haven't ack yet)
-  const user = context.user as Awaited<ReturnType<typeof client.users.info>>
-
-  if (!user || !ISOMER_USERS_ID.some((userId) => userId === user.user?.id)) {
-    throw new Error("Only Isomer members are allowed to use this command!")
+  if (!ISOMER_USERS_ID.some((userId) => userId === payload.user_id)) {
+    await client.chat.postEphemeral({
+      channel: payload.channel,
+      user: payload.user_id,
+      text: `Sorry @${payload.user_id}, only Isomer members are allowed to use this command!`,
+    })
+    await client.chat.postMessage({
+      channel: BOT_AUDIT_CHANNEL_ID,
+      text: `Attempted access by @${payload.user_id}`,
+    })
+    throw new Error("Non-isomer member")
   }
 
   next()
